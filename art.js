ef127a6bfdab2e04d58305145e5a4413c094fbba
Eric Lewiston <eric.lewiston.el@gmail.com>
2021-08-17 12:51:09 -0500

    added gernerated iframes based on arts.js arrray

--- art.js
+++ art.js
@@ -1,1 +1,25 @@
 //list of links for art
+let base_url = "https://preview.p5js.org/lewi0622/embed/"
+
+let arts = [
+    "yvw22BQc-",
+    "Em9UDZvvp",
+    "ZuBk6qIJh",
+    "c2i--pjEk"
+]
+
+window.onload = init;
+
+function init(){
+    arts.forEach(element => {
+    var ifrm = document.createElement("iframe");
+    document.getElementById("iframe_parent").appendChild(ifrm);
+
+    ifrm.setAttribute("src", base_url.concat(element));
+    ifrm.style.width = "400px";
+    ifrm.style.height = "400px";
+    ifrm.style.overflow = "hidden";
+    ifrm.setAttribute("scrolling", "no");
+    ifrm.setAttribute("frameborder", "0");
+    });
+}
