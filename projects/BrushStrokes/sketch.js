38e533972c9f9c90c539d560b4fb93c10bf2e4bd
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-01-20 15:17:20 -0600

    added BrushStrokes, added Sun_Burst to WIP, where files go to die. added fun new color palette that needs a name

---
+++ projects/BrushStrokes/sketch.js
@@ -0,0 +1,53 @@
+function setup() {
+  common_setup();
+  
+}
+//***************************************************
+function draw() {
+  //bleed
+  bleed_border = apply_bleed();
+
+  //apply background
+  bg(true);
+
+  //actual drawing stuff
+  push();
+  strokeWeight(1*global_scale);
+  steps = random(200,300);
+  noFill();
+  center_rotate(random([0,90,180,270]));
+
+  for(let z=0; z<2; z++){
+    push();
+    translate(0, random(canvas_y*.25, canvas_y*.5));
+    dir = random([-1,1])
+    noise_start = random(100);
+    lines = random(150, 200);
+    c = random(palette);
+    reduce_array(palette, c);
+    stroke(c);
+    for(let j=0; j<lines; j++){
+      push();
+      translate(0, random(canvas_y*.35));
+      beginShape();
+      for(let i=0; i<steps + 10*global_scale; i++){
+        push();
+        vertex(canvas_x/steps*i, noise(noise_start + i/75)*global_scale*100*dir);
+        pop();
+      }
+      endShape();
+      pop();
+    }
+    pop();
+    center_rotate(random([0,90,180,270]));
+
+  }
+  pop();
+  //cleanup
+  apply_cutlines();
+  save_drawing();
+}
+//***************************************************
+//custom funcs
+
+
---
+++ projects/WIP/Sun_Burst/sketch.js
@@ -0,0 +1,76 @@
+function reset_values(){
+  //reset project values here for redrawing 
+  line_length = random([25, 40])*global_scale;
+}
+
+//***************************************************
+function setup() {
+  common_setup();
+}
+//***************************************************
+function draw() {  
+  //bleed
+  bleed_border = apply_bleed();
+
+  //apply background
+  bg_c = bg(true);
+
+  //actual drawing stuff
+  push();
+  
+  translate(canvas_x/2, canvas_y/2);
+  rotate(random(0, 360));
+  rad = 50*global_scale;
+  steps = 60;
+  step_size = 360/steps;
+  terminators = 1;
+
+  noFill();
+  strokeWeight(5*global_scale);
+  stroke(random(palette));
+  circle(0,0, rad*3);
+  circle(0,0, rad*4);
+  circle(0,0, rad*5);
+
+  for(let i=0; i<steps; i++){
+    push();
+    noFill();
+    strokeWeight(random(4,4)*global_scale);
+    stroke(random(palette));
+    beginShape();
+    vertex(rad*cos(i*step_size), rad*sin(i*step_size));
+    x_len = random([rad*1.5, rad*2, rad*2.5]);
+    vertex(x_len*cos(i*step_size),x_len*sin(i*step_size));
+    if(i*step_size<180){
+      y_len = canvas_y;
+    }
+    else{
+      y_len = -canvas_y;
+    }
+    vertex(0, y_len) 
+    endShape();
+    strokeWeight(1*global_scale);
+    pop();
+  }
+  fill(bg_c);
+  circle(0,0, rad*2);
+  //center
+
+  //draw line striaght out, then bent
+
+  //rotate and repeat
+
+
+  pop();
+  
+  //cutlines
+  apply_cutlines();
+  
+  save_drawing();
+}
+//***************************************************
+//custom funcs
+
+
+
+
