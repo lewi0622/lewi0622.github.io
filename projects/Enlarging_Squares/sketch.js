1865106ae06a8075a54c35608b972f02271a4f77
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-04-16 15:36:42 -0500

    added symmetries, and more variable speed

--- projects/Enlarging_Squares/sketch.js
+++ projects/Enlarging_Squares/sketch.js
@@ -3,41 +3,46 @@ fr = 30;
 
 xoff = 0;
 inc = 0.01*60/fr;
-square_inc = 5*60/fr;
 
 function setup() {
   common_setup(gif);
   frameRate(fr);
   squares = [];
   palette = shuffle(palette);
-  background(random(palette));
+  bg_c = color(random(palette));
   noStroke();
-  c_id = 0;
-  square_rate = 10; //10 frames
-  // createLoop({duration:10, gif:{fileName:"instanceMode.gif"}})
+  square_rate = 5; //10 frames
+  square_inc = 3*global_scale;
+  symmetries = floor(random(5,11));
+  // createLoop({duration:5, gif:{fileName:"instanceMode.gif"}})
 }
 //***************************************************
 function draw() {
   clear();
   //bleed
   bleed_border = apply_bleed();
-
   //actual drawing stuff
   push();
+
+  background(bg_c);
+
+  center_rotate(45)
 
   //add new squares
   if(frameCount%square_rate==0){
     newSquares(squares);
-  }
 
+  }
+  
   squares.forEach(sq => {
     fill(sq.color);
-    c_id++;
+    square_inc = map(noise(xoff), 0,1, 2,12)*global_scale;
     size_inc = map(noise(xoff), 0,1, 0,square_inc);
-    if(size_inc > 0){
-      sq.radius = lerp(sq.radius, sq.size, 0.001*size_inc);
+    sq.radius = lerp(sq.radius, sq.size, 0.001*size_inc);
+    for(let i=0; i<symmetries; i++){
+      center_rotate(360/symmetries);
+      square(sq.x-sq.size/2, sq.y-sq.size/2, sq.size, sq.radius, sq.radius, sq.radius, sq.radius);
     }
-    square(sq.x-sq.size/2, sq.y-sq.size/2, sq.size, sq.radius, sq.radius, sq.radius, sq.radius);
     sq.size += size_inc;
   });
 
@@ -51,34 +56,11 @@ function draw() {
 //***************************************************
 //custom funcs
 function newSquares(arr){
-  // c = color(palette[c_id%palette.length])
   c = color(random(palette))
   c.setAlpha(100);
-  c_id ++;
   arr.push({
     x:canvas_x/4,
     y:canvas_y/4,
-    size:1*global_scale,
-    radius: 0,
-    color: c
-  })
-  arr.push({
-    x:canvas_x*3/4,
-    y:canvas_y/4,
-    size:1*global_scale,
-    radius: 0,
-    color: c
-  })
-  arr.push({
-    x:canvas_x*3/4,
-    y:canvas_y*3/4,
-    size:1*global_scale,
-    radius: 0,
-    color: c
-  })
-  arr.push({
-    x:canvas_x/4,
-    y:canvas_y*3/4,
     size:1*global_scale,
     radius: 0,
     color: c
@@ -88,9 +70,9 @@ function newSquares(arr){
 function cullSquares(arr){
   for(let i=0; i<arr.length; i++){
     if(arr[i].size > canvas_x/2 || arr[i].size > canvas_y/2){
-      arr[i].color.setAlpha(lerp(arr[i].color.levels[3], 0, 0.07))
+      arr[i].color.setAlpha(lerp(arr[i].color.levels[3], 0, 0.3))
     }
-    if(arr[i].color.levels[3] <= 0){
+    if(arr[i].color.levels[3] <= 10){
       arr.splice(i,1);
     }
   }
