033d3b858b6101e60c7fc2964780631356bd2501
elewiston <elewiston@danfoss.com>
2022-05-03 10:26:49 -0500

    moved to center, and framed

--- projects/Rainbow_Heiroglyphs/sketch.js
+++ projects/Rainbow_Heiroglyphs/sketch.js
@@ -3,13 +3,16 @@ fr = 60;
 
 xoff = 0;
 xinc = 0.005*60/fr;
+offset = 50;
 
 function setup() {
   common_setup(gif);
   frameRate(fr);
   noFill();
-  // draw_line=true;
+  step = canvas_x/2/random(2,8);
   colorMode(HSB);
+
+  num_frames = 359*floor(random(1,6));
 }
 //***************************************************
 function draw() {
@@ -20,27 +23,35 @@ function draw() {
   //actual drawing stuff
   push();
 
-  strokeWeight(1*global_scale);
+  strokeWeight(5*global_scale);
 
   pts = 4;
   shape_rad = 150*global_scale;
-  spacing = shape_rad;
-  zoff = 0;
-  zinc = 0.01;
-  for(let i=spacing; i<canvas_x; i+=spacing){
-    for(let j=spacing; j<canvas_y; j+=spacing){
-      push();
-      translate(i, j);
-      beginShape();
-      for(let z=0; z<pts; z++){
-        curveVertex(map(noise(z+i-j+zoff+xoff), 0,1, -shape_rad, shape_rad), map(noise(z+i+j+zoff+xoff), 0,1, -shape_rad, shape_rad));
-      }
-      endShape();
 
-      zoff += zinc;
-      pop();
+  translate(canvas_x/2, canvas_y/2);
+
+  y_size = canvas_y/4;
+  beginShape();
+  for(let i=-canvas_x/4; i<canvas_x/4; i+=step){
+    if(i==-canvas_x/4){
+      curveVertex(i, map(noise(xoff+i), 0,1, -y_size, y_size));
+      curveVertex(i, map(noise(xoff+i), 0,1, -y_size, y_size));
+    }
+    else if(i+step>=canvas_x/4){
+      curveVertex(canvas_x/4, map(noise(xoff+i), 0,1, -y_size, y_size));
+      curveVertex(canvas_x/4, map(noise(xoff+i), 0,1, -y_size, y_size));
+    }
+    else{
+      curveVertex(i+sin(xoff*10)*10*global_scale, map(noise(xoff+i), 0,1, -y_size, y_size));
     }
   }
+  endShape();
+
+  push();
+  strokeWeight(7*global_scale)
+  stroke("BLACK")
+  rect(-canvas_x/4, -y_size, canvas_x/2, y_size*2)
+  pop();
 
   xoff+=xinc;
   pop();
@@ -48,9 +59,9 @@ function draw() {
   //cleanup
   apply_cutlines();
 
-  // if(frameCount ==359){
-  //   noLoop();
-  // }
+  if(frameCount ==num_frames){
+    noLoop();
+  }
 
 }
 //***************************************************
