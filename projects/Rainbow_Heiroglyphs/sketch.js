4d0fe347d19a1a8c5645192864decaa7050be550
elewiston <elewiston@danfoss.com>
2022-05-05 08:54:58 -0500

    blocked overflow, added second curve

--- projects/Rainbow_Heiroglyphs/sketch.js
+++ projects/Rainbow_Heiroglyphs/sketch.js
@@ -1,18 +1,23 @@
+gif = true;
+fr = 30;
+
+xoff = 0;
+xinc = 0.001*60/fr;
+offset = 50;
+
 function setup() {
-  gif = true;
-  fr = 60;
-  
-  xoff = 0;
-  xinc = 0.005*60/fr;
-  offset = 50;
   common_setup(gif);
   frameRate(fr);
   noFill();
   steps = floor(random(3,8));
+  steps = 3;
   step_size = canvas_x/2/steps;
   colorMode(HSB);
+  bg_c = "WHITE"
+  background(bg_c)
 
-  num_frames = 359*floor(random(1,6));
+  num_frames = 359;
+  console.log(num_frames)
 }
 //***************************************************
 function draw() {
@@ -26,27 +31,35 @@ function draw() {
   strokeWeight(5*global_scale);
   translate(canvas_x/4, canvas_y/2);
 
-  y_size = canvas_y/4;
-  beginShape();
-  for(let i=0; i<steps; i++){
-    if(i==0){
-      curveVertex(i*step_size, map(noise(xoff+i), 0,1, -y_size, y_size));
-      curveVertex(i*step_size, map(noise(xoff+i), 0,1, -y_size, y_size));
+  y_size = canvas_y;
+  for(let j=1; j<=2; j++){
+    beginShape();
+    for(let i=0; i<steps; i++){
+      if(i==0){
+        curveVertex(i*j*step_size, map(noise(xoff), 0,1, -y_size, y_size));
+        curveVertex(i*j*step_size, map(noise(xoff), 0,1, -y_size, y_size));
+      }
+      else if(i+1>=steps){
+        curveVertex(canvas_x/2, map(noise(xoff+i*j), 0,1, -y_size, y_size));
+        curveVertex(canvas_x/2, map(noise(xoff+i*j), 0,1, -y_size, y_size));
+      }
+      else{
+        curveVertex(i*j*step_size, map(noise(xoff+i*j*noise(i*j)), 0,1, -y_size, y_size));
+      }
     }
-    else if(i+1>=steps){
-      curveVertex(canvas_x/2, map(noise(xoff+i), 0,1, -y_size, y_size));
-      curveVertex(canvas_x/2, map(noise(xoff+i), 0,1, -y_size, y_size));
-    }
-    else{
-      curveVertex(i*step_size+sin(xoff*10)*10*global_scale, map(noise(xoff+i), 0,1, -y_size, y_size));
-    }
+    endShape();
   }
-  endShape();
 
   push();
+  fill("WHITE")
+  noStroke();
+  rect(-canvas_x/4, -canvas_y/4, canvas_x, -canvas_y/4)
+  rect(-canvas_x/4, canvas_y/4, canvas_x, canvas_y/4)
+
+  noFill();
   strokeWeight(7*global_scale)
   stroke("BLACK")
-  rect(0, -y_size, canvas_x/2, y_size*2)
+  rect(0, -canvas_y/4, canvas_x/2, canvas_y/2)
   pop();
 
   xoff+=xinc;
@@ -62,5 +75,3 @@ function draw() {
 }
 //***************************************************
 //custom funcs
-
-
