7d04eb45c517f2d0cbfa9e958d96cc6bf96a5391
lewi0622 <eric.lewiston.el@gmail.com>
2022-05-06 21:38:31 -0500

    moved find Min and find Max index

--- projects/Common/utils.js
+++ projects/Common/utils.js
@@ -715,3 +715,37 @@ function find_cnv_mult(){
   }
 
 }
+
+function indexOfMin(arr) {
+  if (arr.length === 0) {
+      return -1;
+  }
+
+  var min = arr[0];
+  var minIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < min) {
+          minIndex = i;
+          min = arr[i];
+      }
+  }
+  return minIndex;
+}
+
+function indexOfMax(arr) {
+  if (arr.length === 0) {
+      return -1;
+  }
+
+  var max = arr[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+          maxIndex = i;
+          max = arr[i];
+      }
+  }
+  return maxIndex;
+}
