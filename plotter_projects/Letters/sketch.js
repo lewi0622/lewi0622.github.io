e6b22f5bf734e7c0fdf5ce5476aeda921758159b
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-05-24 20:39:12 -0500

    changed to use thin stroke and outline letters, not plottable right now

--- plotter_projects/Letters/sketch.js
+++ plotter_projects/Letters/sketch.js
@@ -9,64 +9,43 @@ capture = false;
 capture_time = 10;
 function setup() {
   common_setup(gif, SVG);
+  letters = Array.from("EABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  letter_idx = 0;
 }
 //***************************************************
 function draw() {
+  clear();
   capture_start(capture);
   //bleed
   bleed_border = apply_bleed();
+  // background("WHITE")
 
   //actual drawing stuff
   push();
 
-  strokeWeight(1*global_scale);
+  strokeWeight(1);
 
   noFill();
   stroke('BLACK')
 
-  pts = 10;
-  shape_rad = 40*global_scale;
-  spacing = shape_rad * 2;
-  zoff = 0;
-  inc = 0.01;
-  translate(canvas_x/4, canvas_y/2);
+  translate(150*global_scale, 250*global_scale);
 
-  letters = Array.from("DAD");
-  letters.forEach((l,idx) => {
-    points = font.textToPoints(l, idx*40*global_scale,0, 100*global_scale, {
-      sampleFactor: 1,
-      simplifyThreshold: 1
-    });
-  
-    curveTightness(0);
-    beginShape();
-    curveVertex(points[0].x, points[0].y);
-    points.forEach(p => {
-      curveVertex(p.x, p.y);
-    });
-    endShape();
+  letter = letters[letter_idx%letters.length];
+  points = font.textToPoints(letter, 0,0, 200*global_scale, {
+    sampleFactor: 0.1,
+    simplifyThreshold: 0
   });
 
+  points.forEach(p => {
+    push();
+    translate(p.x, p.y);
+    // line(0,0, 10*global_scale, 10*global_scale);
+    circle(0,0, 200*global_scale);
+    // square (0,0, 100*global_scale);
+    pop();
+  });
 
-  
-
-  // for(let i=spacing; i<canvas_x; i+=spacing){
-  //   for(let j=spacing; j<canvas_y; j+=spacing){
-  //     push();
-  //     curveTightness(random(-5,5));
-  //     translate(i, j);
-  //     beginShape();
-  //     for(let z=0; z<pts; z++){
-  //       // curveVertex(random(-shape_rad,shape_rad), random(-shape_rad,shape_rad));
-  //       curveVertex(map(noise(z+i-j+zoff), 0,1, -shape_rad, shape_rad), map(noise(z+i+j+zoff), 0,1, -shape_rad, shape_rad));
-  //     }
-  //     endShape();
-
-  //     zoff += inc;
-  //     pop();
-  //   }
-  // }
-
+  letter_idx++;
 
   pop();
 
