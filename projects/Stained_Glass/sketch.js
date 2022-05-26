16da49f63ff8684e5669559c7c7a184cf4bb602f
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-05-26 17:59:52 -0500

    added project variation of Kaleidoscope

---
+++ projects/Stained_Glass/sketch.js
@@ -0,0 +1,118 @@
+gif = false;
+fr = 1;
+
+capture = false;
+capture_time = 10;
+function setup() {
+  suggested_palette = random([SAGEANDCITRUS, COTTONCANDY, SUPPERWARE]);
+  common_setup(gif);
+
+  working_palette = JSON.parse(JSON.stringify(palette))
+  bg_c = random(working_palette);
+
+  noStroke();
+
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+
+  clear();
+  
+  //bleed
+  bleed_border = apply_bleed();
+
+  //apply background
+  background(bg_c);
+
+  //actual drawing stuff
+  push();
+
+  //create series of points
+  pt_size = floor(random(10,30)*global_scale);
+  pts = [];
+  num_pts = floor(random(6,60));
+  for(let i=0; i<num_pts; i++){
+    new_pt = gen_pt(pts, pt_size*2);
+    pts.push(new_pt);
+  }
+
+  furthest_pts(pts);
+
+  pts.forEach(pt => {
+    draw_indices(pts, pt);
+  })
+
+  pop();
+  //cutlines
+  apply_cutlines();
+
+  capture_frame(capture, num_frames);
+}
+//***************************************************
+//custom funcs
+
+function gen_pt(arr, min_dist){
+  //checks new point to see if it's greater than the min_dist
+  new_pt = {
+    x:random(-canvas_x, canvas_x*2),
+    y:random(-canvas_y, canvas_y*2),
+    idxs:[]
+  };
+  
+  arr.every(pt => {
+    if(dist(pt.x, pt.y, new_pt.x, new_pt.y)<min_dist){
+      new_pt = gen_pt(arr, min_dist);
+      return false;
+    }
+    else{
+      return true;
+    }
+  })
+
+  return new_pt;
+}
+
+function furthest_pts(arr){
+  //draw lines between the three furthest points
+  arr.forEach((pt, current_idx) => {
+    distances=[];
+    idxs = [];
+    //get distances between all points and pt
+    arr.forEach((p, sub_idx) => {
+      if(sub_idx == current_idx){
+        distances.push(canvas_x*2)
+      }
+      else{
+        distances.push(dist(p.x, p.y, pt.x, pt.y));
+      }
+    })
+
+    //find three smallest distances between points
+    discard = floor(random(2,num_pts/2));
+    for(let i=0; i<discard; i++){
+      index = indexOfMin(distances);
+      distances[index] = canvas_x*2;
+      if(i+2>=discard){
+        idxs.push(index);
+      }
+    }
+    pt.idxs = idxs;
+  })
+}
+
+function draw_indices(pts, pt){
+  //draw triangle between pt and two furthest points
+  c = random(working_palette)
+  c[3] = floor(random(100,220));
+  fill(c);
+  drawingContext.filter= 'brightness(125%) blur(1px)'
+  triangle(pts[pt.idxs[0]].x, pts[pt.idxs[0]].y, pts[pt.idxs[1]].x, pts[pt.idxs[1]].y, pt.x, pt.y);
+
+  noFill();
+  stroke("BLACK")
+  strokeJoin(ROUND)
+  strokeWeight(8)
+  drawingContext.filter = 'none'
+  triangle(pts[pt.idxs[0]].x, pts[pt.idxs[0]].y, pts[pt.idxs[1]].x, pts[pt.idxs[1]].y, pt.x, pt.y);
+}
