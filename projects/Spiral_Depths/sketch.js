7e623c5d5198d8aca4f2acca98e4c6877c3e154d
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-08-05 11:14:12 -0500

    moved background/foregroudn into separate funcs, and added explicit variable declarations

--- projects/Spiral_Depths/sketch.js
+++ projects/Spiral_Depths/sketch.js
@@ -1,9 +1,9 @@
-gif = false;
-sixteen_by_nine = false;
-fr = 1;
+const gif = false;
+const sixteen_by_nine = false;
+const fr = 1;
 
-capture = false;
-capture_time = 10;
+const capture = false;
+const capture_time = 10;
 function setup() {
   suggested_palette = random([BEACHDAY, GAMEDAY, NURSERY]);
   common_setup(gif);
@@ -12,34 +12,44 @@ function setup() {
 function draw() {
   capture_start(capture);
   //bleed
-  bleed_border = apply_bleed();
+  const bleed_border = apply_bleed();
 
-  working_palette = JSON.parse(JSON.stringify(palette));
+  //actual drawing stuff
+  push();
+  center_rotate(random(360));
+
+  createBackground();
+
+  createSpiral();
+
+  pop();
+  //cutlines
+  apply_cutlines(bleed_border);
+  capture_frame(capture, num_frames);
+}
+//***************************************************
+//custom funcs
+const createBackground = function(){
+  push();
+  let working_palette = JSON.parse(JSON.stringify(palette));
   strokeCap(random([PROJECT,ROUND]))
 
   //apply background
-  bg_c = random(working_palette)
+  const bg_c = random(working_palette)
   background(bg_c)
   reduce_array(working_palette, bg_c)
 
-  //actual drawing stuff
-
-  //background stuff
-  center_rotate(random(360));
-  push();
-
-
   translate(canvas_x/2, canvas_y/2);
   noStroke();
-  c1 = random(working_palette);
+  let c1 = random(working_palette);
   reduce_array(working_palette, c1);
 
-  c2 = random(working_palette);
-  opacity = 2;
+  let c2 = random(working_palette);
+  const opacity = 2;
   c1[3] = opacity;
   c2[3] = opacity;
-  ang = 360/floor(random(4,19));
-  steps = 5000;
+  const ang = 360/floor(random(4,19));
+  const steps = 5000;
   for(let i=0; i<steps; i++){
     rotate(ang);
     c = lerpColor(color(c1), color(c2), i/steps);
@@ -53,40 +63,41 @@ function draw() {
 
   filter("blur", 5*global_scale);
   pop();
+}
 
-  // Spiral shapes
+const createSpiral = function(){
   push();
   noStroke();
-  working_palette = JSON.parse(JSON.stringify(palette));
-  steps = 1000;    
-  c1 = random(working_palette);
+  let working_palette = JSON.parse(JSON.stringify(palette));
+  const steps = 1000;    
+  let c1 = random(working_palette);
   reduce_array(working_palette,c1);
-  c2 = random(working_palette);
-  opacity = 200;
+  let c2 = random(working_palette);
+  const opacity = 200;
   c1[3] = opacity;
   c2[3] = opacity;
   drawingContext.shadowColor = color(0,0,0,100);
   translate(canvas_x/2, canvas_y/2);
-  phi = random(1,255);
-  dist_scale = random(1.25,2.5);
+  const phi = random(1,255);
+  const dist_scale = random(1.25,2.5);
   //50/50 circle or square.
-  circ = random()>0.5;
-  dir = random([1,-1]);
+  const circ = random()>0.5;
+  const dir = random([1,-1]);
   for(let i=0; i<steps; i++){
-    f = i/steps;
-    c = lerpColor(color(c1), color(c2), f*2);
+    const f = i/steps;
+    const c = lerpColor(color(c1), color(c2), f*2);
     fill(c);
-    radius = sqrt(0.5);
-    angle = i * phi * dir;
-    smaller_cnv = canvas_x;
+    const radius = sqrt(0.5);
+    const angle = i * phi * dir;
+    let smaller_cnv = canvas_x;
     if(canvas_y<canvas_x){
       smaller_cnv = canvas_y;
     }
-    dist = f * radius * smaller_cnv*dist_scale;
-    x = cos(angle)*dist;
-    y = sin(angle)*dist;
+    const distance = f * radius * smaller_cnv*dist_scale;
+    const x = cos(angle)*distance;
+    const y = sin(angle)*distance;
 
-    size = map(f, 0,1,0,100)*global_scale;
+    const size = map(f, 0,1,0,100)*global_scale;
     drawingContext.shadowBlur = size/floor(random(2,6));
     if(circ){
       circle(x,y, size);
@@ -96,14 +107,4 @@ function draw() {
     }
   }
   pop();
-
-  //cutlines
-  apply_cutlines();
-  capture_frame(capture, num_frames);
 }
-//***************************************************
-//custom funcs
-
-
-
-
