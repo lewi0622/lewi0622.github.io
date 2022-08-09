bd5f0d6d7a2414399585dddf1aad7ac2f5e82a28
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-08-09 14:29:40 -0500

    added variation in phase speed, amplitude, and overall number of points

--- projects/Kingly/sketch.js
+++ projects/Kingly/sketch.js
@@ -3,14 +3,16 @@
 const gif = true;
 const fr = 30;
 const capture = false;
-const capture_time = 10;
+const capture_time = 15;
 const sixteen_by_nine = false;
 let suggested_palette;
 
 //project variables
 let xoff = 0;
 const inc = 1*60/fr;
-let bg_c, crown_c, hl_c, upper_mult, lower_mult;
+let yoff = 0;
+const y_inc =0.001*60/fr;
+let bg_c, crown_c, hl_c, num_points, rot;
 
 function setup() {
   suggested_palette = random([SAGEANDCITRUS, COTTONCANDY, SUPPERWARE]);
@@ -32,30 +34,32 @@ function draw() {
     crown_c = random(working_palette);
     reduce_array(working_palette, crown_c);
     hl_c = random(working_palette);
-
-    upper_mult = random(0.1,5);
-    lower_mult = random(0.1,5);
+    num_points = random([10,15,20, 25]);
+    rot = random([0, 180]);
   }
+  center_rotate(rot);
   background(bg_c)
 
   //actual drawing stuff
   push();
   noStroke();
   fill(crown_c);
-  const point_height = canvas_y * 2/3;
+  const max_point_height = canvas_y * 2/3;
+  const point_height = max_point_height*map(noise(xoff/100), 0,1, 0.5,1);
   const point_width = 15*global_scale;
-  const num_points = 15;
   const radius = point_width/2;
-  translate((canvas_x-(num_points*point_width))/2, canvas_y-(canvas_y-point_height*1.1)/2);
+  translate((canvas_x-(num_points*point_width))/2, canvas_y-(canvas_y-max_point_height*1.1)/2);
   const upper_circle_coords = []
   const lower_circle_coords = [];
   const phase_mult = noise(xoff/1000)*20;
+  const upper_mult = noise(yoff)*5000;
+  const lower_mult = noise(yoff+1000)*5000;
   //crown
   beginShape();
   for(let i=0; i<=num_points; i++){
     // const height_variation = map(noise(i/10+xoff), 0,1, -0.02, 0.5)*point_height;
-    const height_variation = map(sin(i*phase_mult+xoff*upper_mult), -1,1, 0,1)*point_height;
-    const lower_height_variation = map(cos(i*phase_mult+xoff*lower_mult), -1,1, -0.02,1)*point_height;
+    const height_variation = map(sin(i*phase_mult+upper_mult), -1,1, 0,1)*point_height;
+    const lower_height_variation = map(cos(i*phase_mult+lower_mult), -1,1, -0.02,1)*point_height;
     //lower left tri vertex
     vertex(i*point_width, -lower_height_variation);
 
@@ -67,7 +71,6 @@ function draw() {
 
       //point vertex
       vertex(i*point_width + point_width/2, -point_height+height_variation);
-      // circle(i*point_width + point_width/2, -point_height+height_variation - radius/2, radius);
     }
   }
   vertex(num_points*point_width, point_width);
@@ -89,21 +92,10 @@ function draw() {
   //outline
   stroke(crown_c);
   strokeWeight(5*global_scale);
-  rect(0,point_width, point_width*num_points, -point_height*1.1-point_width-radius);
-
-  pop();
-
-  push();
-  //arrows
-  // const arrow_inner_width = point_width;
-  // const arrow_outer_width = arrow_inner_width*1.25;
-  // const arrow_length = 3*arrow_outer_width;
-  // translate(arrow_outer_width, canvas_y/2);
-  // beginShape();
-  // endShape();
-
+  rect(0,point_width, point_width*num_points, -max_point_height*1.1-point_width-radius);
 
   xoff += inc;
+  yoff += y_inc;
   pop();
   //cutlines
   apply_cutlines(bleed_border);
@@ -111,7 +103,3 @@ function draw() {
 }
 //***************************************************
 //custom funcs
-
-
-
-
