48f4c85b46621cf5e88b4407eb8a4148890e4c16
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-08-15 08:57:14 -0500

    tweaked params, added random chance instead of noise, more rows, horizontal chance, etc.

--- projects/Color_Stacks/sketch.js
+++ projects/Color_Stacks/sketch.js
@@ -45,12 +45,20 @@ function draw() {
     working_palette[idx] = color(c);
   });
 
+  //rotation chance
+  if(random()>0.5) center_rotate(90);
+
   //project variables
   const offset_x = canvas_x/8;
   const offset_y = canvas_y/8;
-  const grid_num_col = floor(random(4, 16));
-  const grid_size_col = (canvas_x-offset_x)/grid_num_col; 
-  const layers = 10;
+
+  //find the smaller offset and use that for both edges
+  let smaller_offset = offset_x;
+  if(offset_y<offset_x) smaller_offset=offset_y;
+
+  const grid_num_col = floor(random(4, 40));
+  let grid_size_col = (canvas_x-smaller_offset)/grid_num_col; 
+  const layers = floor(random(4,11));
 
   noStroke();
 
@@ -58,9 +66,9 @@ function draw() {
   push();
   for(let z=0; z<layers; z++){
     for(let j=0; j<grid_num_col; j++){
-      const grid_num_row = floor(random(5, 40));
-      const grid_size_row = (canvas_y-offset_y)/grid_num_row;
-  
+      const grid_num_row = floor(random(4, 40));
+      let grid_size_row = (canvas_y-smaller_offset)/grid_num_row;
+
       const init_c = random(working_palette);
       let second_c = init_c;
       while(second_c == init_c){
@@ -75,18 +83,22 @@ function draw() {
       third_c.setAlpha(100);
 
       const crossover = random(0.25, 0.75);
+      // const crossover = map(noise((j+z)/10), 0,1, 0.25, 0.75);
       const smoothness = floor(random(20)+1);
+      const choose_noise = random()>0.25;
       for(let i=0; i<grid_num_row; i++){
         push();
         let c;
         if((i+1)/grid_num_row>crossover){
-          c = lerpColor(second_c, third_c, map(noise((i*(j+1))/smoothness), 0,1, 0,grid_num_row)/grid_num_row);
+          if(choose_noise) c = lerpColor(second_c, third_c, map(noise((i*(j+1))/smoothness), 0,1, 0,grid_num_row)/grid_num_row);
+          else c = lerpColor(second_c, third_c, random(0,grid_num_row/2)/grid_num_row);
         }
         else{
-          c = lerpColor(init_c, second_c, map(noise((i*(j+1))/smoothness), 0,1, 0,grid_num_row)/grid_num_row);
+          if(choose_noise) c = lerpColor(init_c, second_c, map(noise((i*(j+1))/smoothness), 0,1, 0,grid_num_row)/grid_num_row);
+          else c = lerpColor(init_c, second_c, random(0,grid_num_row/2)/grid_num_row);
         }
         fill(c);
-        translate(offset_x/2, offset_y/2);
+        translate(smaller_offset/2, smaller_offset/2);
         translate(j*grid_size_col, i*grid_size_row);
         rect(0,0, grid_size_col, grid_size_row);
         pop();
