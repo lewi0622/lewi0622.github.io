4a45d8d8cf372d86cfb673c3277a9938451ae04c
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-10-18 07:02:55 -0500

    added one tile design

---
+++ projects/Alhambra/Square_Stars/sketch.js
@@ -0,0 +1,82 @@
+'use strict';
+//setup variables
+const gif = false;
+const fr = 1;
+const capture = false;
+const capture_time = 10;
+const sixteen_by_nine = false;
+let suggested_palette;
+
+function setup() {
+  suggested_palette = random([SAGEANDCITRUS, COTTONCANDY, BIRDSOFPARADISE]);
+  common_setup(gif);
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+  //bleed
+  const bleed_border = apply_bleed();
+
+  //projct variables
+
+  let working_palette = JSON.parse(JSON.stringify(palette));
+  working_palette = shuffle(working_palette, true);
+  //apply background
+  let bg_c = color("WHITE");
+  background(bg_c)
+
+  //actual drawing stuff
+  push();
+  noStroke();
+  center_rotate(random([0,90]));
+  translate(random(-canvas_x/16,-canvas_x/8), random(-canvas_y/16,-canvas_y/8));
+  rectMode(CENTER);
+  const spacing = random(4,15)*global_scale;
+  const sq_size = random(1,5)*spacing;
+  const star_size = random(3,20)*global_scale;
+
+  const y_offset = sqrt(2*pow(sq_size,2)) + star_size + spacing;
+  const x_offset = (sqrt(2*pow(sq_size,2)) + star_size + spacing)/2;
+  const x_steps = floor(canvas_x*1.5/x_offset);
+  const y_steps = floor(canvas_y*1.5/y_offset);
+  let c_id = 0;
+
+  for(let i=0; i<x_steps; i++){
+    push();
+    if(i%2==0){
+      translate(0,y_offset/2);
+    }
+    const c = working_palette[c_id%(working_palette.length)];
+    fill(c);
+    for(let j=0; j<y_steps; j++){
+      push();
+      translate(i*x_offset, j*y_offset);
+      
+      push();
+      rotate(45);
+      square(0,0,sq_size);
+      pop();
+      
+      push();
+      translate(0, y_offset/2)
+      square(0,0, star_size);
+      rotate(45);
+      square(0,0, star_size);
+      pop();
+
+      pop();
+    }
+    c_id++;
+    pop();
+  }
+  pop();
+  //cutlines
+  apply_cutlines(bleed_border);
+  capture_frame(capture);
+}
+//***************************************************
+//custom funcs
+
+
+
+
---
+++ projects/Alhambra/Tile1/sketch.js
@@ -0,0 +1,92 @@
+'use strict';
+//setup variables
+const gif = false;
+const fr = 1;
+const capture = false;
+const capture_time = 10;
+const sixteen_by_nine = false;
+let suggested_palette;
+
+function setup() {
+  suggested_palette = random([SAGEANDCITRUS, COTTONCANDY, SUPPERWARE]);
+  common_setup(gif);
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+  //bleed
+  const bleed_border = apply_bleed();
+
+  //projct variables
+
+  let working_palette = JSON.parse(JSON.stringify(palette));
+  working_palette = shuffle(working_palette, true);
+  //apply background
+  let bg_c = color("WHITE");
+  background(bg_c);
+  rectMode(CENTER);
+
+  //actual drawing stuff
+  push();
+  noStroke();
+  const tile_size = canvas_x;
+
+  const sq_size = tile_size/10;
+  const weight = tile_size/20;
+  push();
+  translate(canvas_x/2, canvas_y/2);
+  fill(working_palette[1]);
+  square(0,0, tile_size);
+
+  //center Star
+  fill(working_palette[0]);
+  rect(0,0, sq_size*2.5, sq_size);
+  rect(0,0, sq_size, sq_size*2.5)
+  pop();
+
+  rectMode(CORNER);
+  for(let i=0; i<4; i++){
+    push();
+    center_rotate(i*90);
+    //corner
+    fill(working_palette[0]);
+    square(0,0,sq_size*2);
+
+    //mid
+    fill(working_palette[2]);
+    square(sq_size*2, 0, sq_size*2);
+    square(0, sq_size*2, sq_size*2);
+
+    //center
+    fill(working_palette[3%working_palette.length]);
+    square(sq_size*4, 0, sq_size*2);
+    square(0, sq_size*4, sq_size*2);
+    pop();
+  }
+  for(let i=0; i<1; i++){
+    fill("TAN");
+    stroke("BROWN");
+    beginShape();
+    vertex(tile_size/4, tile_size/8+weight/2);
+    vertex(tile_size/2-weight, tile_size/8+weight/2);
+    vertex(tile_size/2+sq_size/2, tile_size/2-sq_size/2);
+    vertex(tile_size*7/8-weight/2, tile_size/2+weight);
+    vertex(tile_size*7/8+weight/2, tile_size/2+weight);
+    vertex(tile_size*7/8+weight/2, tile_size/2+weight/2);
+    vertex(tile_size/2+sq_size/2+weight, 0);
+    endShape();
+
+    pop();
+  }
+
+  pop();
+  //cutlines
+  apply_cutlines(bleed_border);
+  capture_frame(capture);
+}
+//***************************************************
+//custom funcs
+
+
+
+
