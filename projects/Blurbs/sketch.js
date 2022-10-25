03a8df12d5b795d79f8309ff04c4e3f83d65d467
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-10-25 08:30:51 -0500

    added parameters to blurbs, did not parameterize fully, mainly to test functionality when live

--- projects/Blurbs/sketch.js
+++ projects/Blurbs/sketch.js
@@ -7,6 +7,14 @@ const capture_time = 10;
 const sixteen_by_nine = false;
 let suggested_palette;
 
+let gui_params = [];
+
+function gui_values(){
+
+}
+gui_params.push(parameterize("Weight", 10*global_scale, 0, 100*global_scale, 0.1*global_scale));
+gui_params.push(parameterize("Line_length", 60*global_scale, 10*global_scale, 100*global_scale, 0.1*global_scale));
+
 function setup() {
   suggested_palette = random([SAGEANDCITRUS, COTTONCANDY, SUPPERWARE]);
   common_setup(gif);
@@ -18,9 +26,8 @@ function draw() {
   const bleed_border = apply_bleed();
 
   //projct variables
-  const line_length = 60*global_scale;
-  const tile_width = canvas_x / line_length;
-  const tile_height = canvas_y / line_length;
+  const tile_width = canvas_x / Line_length;
+  const tile_height = canvas_y / Line_length;
   let funcs, colors, iterations, x_offset_min, x_offset_max, y_offset_min, y_offset_max;
 
   let working_palette = JSON.parse(JSON.stringify(palette));
@@ -36,10 +43,10 @@ function draw() {
   center_rotate(random([0,90,180,270]));
 
   //line width
-  strokeWeight(10*global_scale);
+  strokeWeight(Weight);
 
   //tile lines
-  tile(tile_width, tile_height, line_length, funcs=[draw_diag, draw_cardinal], 
+  tile(tile_width, tile_height, Line_length, funcs=[draw_diag, draw_cardinal], 
     colors=working_palette, iterations=1, 
     x_offset_min=0, x_offset_max=0,
     y_offset_min=0, y_offset_max=0);
