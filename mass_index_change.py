0d2110b0faa7ae74d2b0bf5dacf2b0dfb1c193d7
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-10-26 15:38:20 -0500

    added comments for clarity and color picker cdn links, ran to propagate to all indexes

--- mass_index_change.py
+++ mass_index_change.py
@@ -22,13 +22,20 @@ for f in to_replace:
     
     file_contents = '''<!DOCTYPE html><html lang="en"><head>
     <script src="'''+rel_path.as_posix()+'''/p5.js"></script>
+    <!-- Custom scripts -->
     <script src="'''+rel_path.as_posix()+'''/palettes.js"></script>
     <script src="'''+rel_path.as_posix()+'''/utils.js"></script>
     <script src="'''+rel_path.as_posix()+'''/shapes.js"></script>
+    <!-- Frame capture lib for gifs/vids -->
     <script src="'''+rel_path.as_posix()+'''/CCapture.all.min.js"></script>
+    <!-- p5.gui lib -->
     <script src="'''+rel_path.as_posix()+'''/p5.gui.js"></script>
     <script src="'''+rel_path.as_posix()+'''/quicksettings.js"></script>   
     <script src="https://unpkg.com/p5.js-svg@1.3.1"></script>
+    <!-- Color Picker lib -->
+    <link rel="stylesheet" href="https://unpkg.com/alwan/dist/css/alwan.min.css">
+    <script src="https://unpkg.com/alwan/dist/js/alwan.min.js"></script>
+    <!-- Global Stylesheet -->
     <link rel="stylesheet" type="text/css" href="'''+rel_path.as_posix()+'''/style.css">
     <meta charset="utf-8">
 
