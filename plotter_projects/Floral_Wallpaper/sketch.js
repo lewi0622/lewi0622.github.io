ad354e2946236a23a39503c3a28153d03c134289
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-10-28 21:49:28 -0500

    added parameters, made grid regular.

--- plotter_projects/Floral_Wallpaper/sketch.js
+++ plotter_projects/Floral_Wallpaper/sketch.js
@@ -9,7 +9,15 @@ let suggested_palette;
 
 
 function gui_values(){
-
+  parameterize("dec", random(2,3),2,3,0.1,true);
+  parameterize("points", floor(random(10,30)), 3, 100, 1, false);
+  parameterize("scale_factor", random(10,15), 1, 30, 0.25, false);
+  parameterize("noise_off", 50, 0, 100, 0.1, false);
+  parameterize("starter_radius", canvas_x/global_scale/random(4.5,6), canvas_x/global_scale/10, canvas_x/global_scale/3, 1 ,true);
+  parameterize("min_radius", 1, 0.1,5,0.1,true);
+  parameterize("spacing", 60, 60, 400, true);
+  parameterize("rows", 1, 1, 5, 1, false);
+  parameterize("cols", 1, 1, 5, 1, false);
 }
 
 function setup() {
@@ -27,36 +35,23 @@ function draw() {
 
   refresh_working_palette();
 
-  let tree_c = random(working_palette);
-  fill(tree_c);
-  reduce_array(working_palette,tree_c);
-
   strokeWeight(random(0.25,0.75)*global_scale);
-  stroke(random(working_palette));
-
-  const min_radius = 1*global_scale;
-  const noise_off = 50;
-
-  const dec = random(2,3)*global_scale;
-  const points = floor(random(10,30));
-  const scale_factor = random(10,15); 
-  const starter_radius = canvas_x/random(4.5,6);
-  const rows = 3;
-  const cols = 3;
+  let padding = starter_radius;
+  translate(padding, padding);
   for(let j=0; j<rows; j++){
     for(let z=0; z<cols; z++){
-      stroke(random(255));
+
       let radius = starter_radius;
       push();
-      translate(z*canvas_x/2,j*canvas_y/2);
-      if(j==1)translate(canvas_x/4,0);
+      stroke(0);
+      translate(z*spacing,j*spacing);
+      // if(j%2==1)translate(canvas_x/4,0);
       rotate(random(360));
 
       let noise_base = 0;
       let noise_inc = random(0.1,0.15);
       let dir = random([-1,1]);
       while(radius>min_radius){
-        beginShape();
         let noise_scale = radius/starter_radius*global_scale*scale_factor;
         let pts = [];
         for(let i=0; i<points; i++){
@@ -69,7 +64,6 @@ function draw() {
         pts = arrayRotate(pts, floor(random(pts.length)));
         beginShape();
         for(let i=0; i<points+4; i++){
-          print("i ",i, " elem ", i%pts.length);
           curveVertex(pts[i%pts.length].x, pts[i%pts.length].y)
         }
         endShape();
