eed2dc04028a310d8c4afc21e5f1dd356a93ff0c
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-11-01 11:32:23 -0500

    added comments, added commented out sections that can be run to scale to different sizes

--- Run_Vpype.py
+++ Run_Vpype.py
@@ -15,8 +15,34 @@ file_parts = os.path.splitext(input_file)
 output_file = file_parts[0] + "_PROCESSED" + file_parts[1]
 
 #options and args
-prefix = r"vpype read --no-crop --attr stroke "
-args = r" occult layout -l letter crop 0 0 11in 8.5in linemerge linesort reloop linesimplify write --color-mode layer "
+# crop 0 0 8in 10in can be placed after letter if need be
 
+#read command
+prefix = r"vpype read"
+
+# read flag: doesn't crop incoming picture
+prefix += r" --no-crop "
+
+args = r""
+
+# sclaing argument, uncomment which one is the preferred size
+#standard mat opening
+args += r" scaleto 8in 10in"
+#slightly larger than mat opening, so edges aren't visible
+# args += r" scaleto 8.5in 10.5in"
+#full sized
+# args += r" scaleto 8.5in 11in"
+
+#occult function uses most recently drawn closed shapes to erase lines that are below the shape
+args += r" occult "
+
+#layout as letter centers graphics within a letter size, but does not scale it
+args += r"layout -l letter "
+
+#linemerge connects start/ends of shapes. linesort sorts for plotting pendown time. 
+# reloop randomizes starts ends of closed shapes to reduce alignment of up/down marks. linesimplify changes curves to polygons for better plotter control
+#still need to check on color-mode-layer
+args += r" linemerge linesort reloop linesimplify write --color-mode layer "
+print(args)
 full_cmd = prefix + '"' + input_file + '"' + args + '"' + output_file + '"'
 subprocess.run(full_cmd)
