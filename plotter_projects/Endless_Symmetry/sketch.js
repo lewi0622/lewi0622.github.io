84f8cd421afb998e32f345feac8825623c321e25
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-11-07 12:21:32 -0600

    added parameters to Endless Symmetry, including xoff parameter to scrub instead of animating

--- plotter_projects/Endless_Symmetry/sketch.js
+++ plotter_projects/Endless_Symmetry/sketch.js
@@ -1,28 +1,22 @@
 'use strict';
 //setup variables
-const gif = true;
+const gif = false;
 const fr = 30;
 
 const noise_off = 20;
-let xoff = 0;
-const inc = 0.1*60/fr;
 
 const capture = false;
 const capture_time = 5
 
-let sym_angs, line_segs;
-
-
 function gui_values(){
-
+  parameterize("sym_angs", 4, 3, 20, 1, false);
+  parameterize("line_segs", floor(random(5,20)), 3, 30, 1, false);
+  parameterize("xoff", 0, 0, 100, 0.1, false);
+  parameterize("tightness", 0, -5, 5, 0.1, false);
 }
 
 function setup() {
   common_setup(gif, SVG);
-
-  //apply background
-  sym_angs = random([4,6,8,10,12,14,16]);
-  line_segs = floor(random(5,20));
 }
 //***************************************************
 function draw() {
@@ -39,7 +33,7 @@ function draw() {
       translate(canvas_x/2, canvas_y/2);
       noFill();
       strokeWeight(1.5*global_scale);
-      curveTightness(map(noise(xoff/100), 0, 1, -5, 5));
+      curveTightness(tightness);
       beginShape();
       for(let j=0; j<line_segs; j++){
         const dampening = map(noise(j), 0, 1, 10, 100);
@@ -55,8 +49,6 @@ function draw() {
     center_rotate(360/sym_angs);
 
   }
-
-  xoff+= inc;
 
   pop();
   //cutlines
