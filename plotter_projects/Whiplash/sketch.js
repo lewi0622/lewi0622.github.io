202a4aa7314391d37ccae6b80c4483a27490074a
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-11-07 12:24:00 -0600

    added parameters to Whiplash, changed from gif, uses coarse/fine parameters to animate

--- plotter_projects/Whiplash/sketch.js
+++ plotter_projects/Whiplash/sketch.js
@@ -1,18 +1,19 @@
 'use strict';
 //setup variables
-const gif = true;
+const gif = false;
 const fr = 30;
 const capture = false;
 const capture_time = 8;
 
 //project variables
-const noiseMax = 2;
 const phase_off = 20;
-let i_mult;
+// let i_mult;
 
 
 function gui_values(){
-
+  parameterize("i_mult_coarse", random([0, 1.5, 2.5,  5, 10, 20, 30, 40, 120]), 0, 120, 1, false);
+  parameterize("i_mult_fine", 0, 0, 1, 0.0001, false);
+  parameterize("noise_maximum", 2, 0, 5, 0.1, false);
 }
 
 function setup() {
@@ -25,11 +26,6 @@ function setup() {
   noFill();
   strokeWeight(1.5)
   angleMode(DEGREES)
-  //cool stuff at 60
-  i_mult = random([0, 1.5, 2.5,  5, 10, 20, 30, 40, 60])
-  if(i_mult != 0){
-    i_mult -= 0.1;
-  }
 }
 //***************************************************
 function draw() {
@@ -43,21 +39,24 @@ function draw() {
 
   //actual drawing stuff
   push();
+
+  let working_i_mult = i_mult_coarse + i_mult_fine;
+
   translate(canvas_x/2, canvas_y/2);
   const line_size = 300*global_scale;
   const step_size = 3;
   beginShape();
   for(let i=0; i<720; i+=step_size){
-    let xoff = map(cos(i*i_mult), -1,1, 0, noiseMax);
-    let yoff = map(sin(i*i_mult), -1,1, 0, noiseMax);
+    let xoff = map(cos(i*working_i_mult), -1,1, 0, noise_maximum);
+    let yoff = map(sin(i*working_i_mult), -1,1, 0, noise_maximum);
     const x = map(noise(xoff, yoff), 0,1, -line_size,line_size)
     
-    xoff = map(cos((i+phase_off)*i_mult), -1,1, 0, noiseMax);  
-    yoff = map(sin((i+phase_off)*i_mult), -1,1, 0, noiseMax);
+    xoff = map(cos((i+phase_off)*working_i_mult), -1,1, 0, noise_maximum);  
+    yoff = map(sin((i+phase_off)*working_i_mult), -1,1, 0, noise_maximum);
     const y = map(noise(xoff, yoff), 0,1, -line_size,line_size)
     curveVertex(x, y)
     
-    i_mult += 0.000003
+    working_i_mult += 0.000003
 
     }
   endShape();
