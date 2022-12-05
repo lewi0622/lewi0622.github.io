63b9f456d3dfaf2d80e4d8a88663b873e30d9148
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-12-05 11:31:07 -0600

    made buttons plaid, added suggested color palletes

--- projects/Circle_Grids/sketch.js
+++ projects/Circle_Grids/sketch.js
@@ -6,14 +6,20 @@ const fr = 1;
 const capture = false;
 const capture_time = 10;
 const sixteen_by_nine = false;
-suggested_palettes = [SAGEANDCITRUS, COTTONCANDY, SUPPERWARE]
+suggested_palettes = [OASIS,SUPPERWARE,TOYBLOCKS]
 
 function gui_values(){
-
+  parameterize("num_circles", round(random(2,15)), 1, 50, 1, false);
+  parameterize("buffer", 110, 0, 400, 1, true);
+  parameterize("blur_size", 4, 0, 100, 1, true);
+  parameterize("blur_offset_x", random(-5,5), -20, 20, 1, true);
+  parameterize("blur_offset_y", random(-5,5), -20, 20, 1, true);
 }
 
 function setup() {
   common_setup(gif);
+  //if not square, the cirlces have weird protrusions
+  strokeCap(SQUARE);
 }
 //***************************************************
 function draw() {
@@ -30,20 +36,23 @@ function draw() {
 
   //actual drawing stuff
   push();
-  center_rotate(360)
-  let num_circles=round(random(1,10));
   for(let i=0; i<num_circles; i++){
     push();
-    translate(random(110*global_scale,290*global_scale), random(110*global_scale,290*global_scale));
+    translate(random(buffer, canvas_x-buffer), random(buffer, canvas_y-buffer));
     let rad=round(random((1-i/num_circles)*100, 200-50*(1-i/num_circles)));
     rad *= global_scale;
     strokeWeight(random(5,15)*global_scale);
+
     const circ_c = random(working_palette);
-    fill("BLACK");
-    circle(random(5,20)*global_scale, random(5,20)*global_scale, rad);
+    drawingContext.shadowColor = "black";
+    drawingContext.shadowBlur = blur_size;
+    drawingContext.shadowOffsetX = blur_offset_x;
+    drawingContext.shadowOffsetY = blur_offset_y;
     stroke(circ_c);
     circle(0,0, rad);
     drawingContext.clip();
+    //change shadow color to clear so no artifacting shows up on circle
+    drawingContext.shadowColor = color(0,0,0,0);
     image(criss_cross(rad, circ_c),-rad/2,-rad/2);
     pop();
   }
@@ -60,20 +69,29 @@ function criss_cross(rad, c){
   pg.translate(rad/2, rad/2);
   pg.rotate(random(360));
   pg.translate(-rad/2, -rad/2);
-  pg.strokeWeight(random(0.1, 10)*global_scale);
+  pg.strokeWeight(random(2, 8)*global_scale);
   let grid_c = c;
   while(grid_c == c) grid_c = random(working_palette);
+  grid_c = color(grid_c);
+  grid_c.setAlpha(150);
   pg.stroke(grid_c);
   pg.background(c);
   let line_nums = ceil(rad/10);
+  pg.noFill();
   for(let i=0; i<line_nums; i++){
-    for(let j=0; j<line_nums; j++){
-      pg.push();
-      pg.noFill();
-      pg.translate(i*10*global_scale, j*10*global_scale);
-      pg.rect(0,0, 100*global_scale, 10*global_scale);
-      pg.pop();
-    }
+    pg.push();
+    pg.translate(i*10*global_scale, 0);
+    pg.rect(0,0, 10*global_scale, rad);
+    pg.pop();
   }
+  grid_c.setAlpha(100);
+  pg.stroke(grid_c);
+  for(let i=0; i<line_nums; i++){
+    pg.push();
+    pg.translate(0, i*10*global_scale);
+    pg.rect(0,0, rad, 10*global_scale);
+    pg.pop();
+  }
+
   return pg;
 }
