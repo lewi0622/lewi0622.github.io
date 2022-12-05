c3780a5afa35ec062f2bfe3916637ab6a0b414c1
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-12-05 08:36:50 -0600

    added parameters to Scrambled Matrix

--- projects/Scrambled_Matrix/sketch.js
+++ projects/Scrambled_Matrix/sketch.js
@@ -10,7 +10,8 @@ suggested_palettes = [COTTONCANDY, SOUTHWEST, MUTEDEARTH]
 
 
 function gui_values(){
-
+  parameterize("step_size", 20, 1, 50, 1, true);
+  parameterize("square_size", 30, 1, 60, 1, true);
 }
 
 function setup() {
@@ -24,10 +25,6 @@ function draw() {
   const bleed_border = apply_bleed();
 
   refresh_working_palette();
-  if(gif){
-    //randomize noise seed
-    noiseSeed(random(10000))
-  }
 
   //apply background
   let bg_c = random(working_palette)
@@ -38,8 +35,8 @@ function draw() {
   push();
   center_rotate(random([0,90,180,270]));
 
-  const step=20*global_scale;
-  const square_size = 30*global_scale;
+  // const step=20*global_scale;
+  // const square_size = 30*global_scale;
   const x_off = canvas_x/8 + square_size/2;
   const y_off = canvas_y/8 + square_size/2;
 
@@ -64,7 +61,7 @@ function draw() {
       strokeWeight(random(1,5)*global_scale);
 
       push();
-      translate(i*step+x_off, j*step+y_off);
+      translate(i*step_size+x_off, j*step_size+y_off);
       square(0, 0, loop_noise*square_size);
       if(isFill==true){
         stroke(bg_c);
