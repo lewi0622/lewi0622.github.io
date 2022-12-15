0570d01651f4900903a22717135d419cb262af38
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-12-15 09:41:18 -0600

    added paper bouquet and pixel pollock projects

---
+++ projects/Paper_Bouquet/sketch.js
@@ -0,0 +1,81 @@
+'use strict';
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 5;
+const capture = false;
+const capture_time = 2;
+const sixteen_by_nine = false;
+let grid_bg_c;
+suggested_palettes = [SIXTIES];
+
+
+function gui_values(){
+  parameterize("x_padding", 50, 0, 400, 1, true);
+  parameterize("num_shapes", floor(random(2,random(2, 120))), 2, 200, 1, false);
+  parameterize("variation", random(100,200), 0, 400, 1, true);
+  parameterize("num_points", floor(random(1,6)), 1, 20, 1, false);
+  parameterize("damp", random(1,20), 1, 100, 1, false);
+  parameterize("offset", 20, 0, 200, 1, false);
+  parameterize("tightness", random(-1,1), -5, 5, 0.01, false);
+  parameterize("rotation", random(360), 0,360, 1, false);
+}
+
+function setup() {
+  common_setup(gif);
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+  //bleed
+  const bleed_border = apply_bleed();
+
+  refresh_working_palette();
+  //actual drawing stuff
+  push();
+  center_rotate(rotation);
+  noStroke();
+  curveTightness(tightness);
+  background("#fffbf1")
+  const startX = random(x_padding, x_padding*2);
+  const startY = canvas_y/2;
+  const endX = canvas_x-startX;
+  const endY = canvas_y-startY;
+
+  const start = createVector(startX, startY);
+  const end = createVector(endX, endY);
+
+  //less shapes, more alpha
+  let c_alpha = map(num_shapes, 2,200, 175, 100)
+
+  print(start, end);
+  for(let i=0; i<num_shapes; i++){
+    beginShape();
+    const c=color(random(working_palette));
+    //less alpha on top shapes than bottom
+    let loop_alpha = (1-(i/num_shapes))*c_alpha;
+    loop_alpha = constrain(loop_alpha, c_alpha*0.75, c_alpha);
+
+    c.setAlpha(loop_alpha);
+    fill(c);
+    curveVertex(startX, startY);
+    curveVertex(startX, startY);
+    const var_div = 10;
+    for(let j=0; j<num_points; j++){
+      const mid_vec = p5.Vector.lerp(start, end, (j+1)/num_points);
+      const x = mid_vec.x + random(-variation/var_div, variation/var_div);
+      let y = mid_vec.y + map(noise(mid_vec.x+offset, i/damp), 0,1, 0, variation);
+      y = random(y, canvas_y-y);
+      curveVertex(x, y);
+    }
+    curveVertex(endX, endY);
+    endShape();
+  }
+  pop();
+
+  //cutlines
+  apply_cutlines(bleed_border);
+  capture_frame(capture);
+}
+//***************************************************
+//custom funcs
---
+++ projects/Pixel_Pollock/sketch.js
@@ -0,0 +1,71 @@
+'use strict';
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 5;
+const capture = false;
+const capture_time = 2;
+const sixteen_by_nine = false;
+suggested_palettes = [SAGEANDCITRUS, BIRDSOFPARADISE, SUMMERTIME, JAZZCUP];
+
+function gui_values(){
+  parameterize("grid_size", 1, 1, 400, 0.5, true);
+  parameterize("grid_pad_num", 0, 0, 50, 1, false);
+  parameterize("layers", 500, 1, 1000, 1, false);
+  parameterize("steps", 1000, 1, 5000, 1, false);
+  parameterize("cor", 0, 0, 100, 1, true);
+}
+
+function setup() {
+  common_setup(gif);
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+  //bleed
+  const bleed_border = apply_bleed();
+
+  refresh_working_palette();
+  //actual drawing stuff
+  push();
+  background("#f2eadb")
+  noStroke();
+
+  speckles(200, 1);
+
+  pop();
+
+  //cutlines
+  apply_cutlines(bleed_border);
+  capture_frame(capture);
+}
+//***************************************************
+//custom funcs
+function speckles(alpha, size_mult){
+  grid_size = round(grid_size);
+  let cols = floor(canvas_x/grid_size);
+  let rows = floor(canvas_y/grid_size);
+  for(let z=0; z<layers; z++){
+    push();
+    const c = color(random(working_palette));
+    c.setAlpha(alpha);
+    fill(c);
+
+    const start_col = ceil(random(grid_pad_num, cols-grid_pad_num));
+    const start_row = ceil(random(grid_pad_num, rows-grid_pad_num));
+    let x = start_col*grid_size;
+    let y = start_row*grid_size;
+    for(let i=0; i<steps; i++){
+      square(x,y,grid_size*size_mult, cor,cor,cor,cor);
+      let new_x=-1;
+      let new_y=-1;
+      while(new_x<grid_pad_num*grid_size || new_x>canvas_x-grid_pad_num*grid_size || new_y<grid_pad_num*grid_size || new_y>canvas_y-grid_pad_num*grid_size){
+        new_x = x + random([-1,0,1])*grid_size;
+        new_y = y + random([-1,0,1])*grid_size;
+      }
+      x = new_x;
+      y = new_y;
+    }
+    pop();
+  }
+}
