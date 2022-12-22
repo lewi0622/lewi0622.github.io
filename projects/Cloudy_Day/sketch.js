e8ce8fe13edad2dc01eb891858212e02128e733b
Eric Lewiston <eric.lewiston.el@gmail.com>
2022-12-22 11:58:50 -0600

    added constraints on upper circle gen. added wiggle to coords

--- projects/Cloudy_Day/sketch.js
+++ projects/Cloudy_Day/sketch.js
@@ -2,15 +2,15 @@
 //setup variables
 const gif = false;
 const animation = false;
-const fr = 5;
+const fr = 1;
 const capture = false;
-const capture_time = 2;
+const capture_time = 20;
 const sixteen_by_nine = false;
-suggested_palettes = [COTTONCANDY, NURSERY];
+suggested_palettes = [NURSERY, TOYBLOCKS];
 let cloud_color, cloud_back_color;
 function gui_values(){
-  parameterize("cloud_min_radius", random(0,5), 0, 200, 1, true);
-  parameterize("cloud_max_radius", random(5,10), 0, 200, 1, true);
+  parameterize("cloud_min_radius", random(1,5), 0, 200, 1, true);
+  parameterize("cloud_max_radius", random(3,10), 0, 200, 1, true);
 }
 
 function setup() {
@@ -37,16 +37,14 @@ function draw() {
   cloud_color = bg_c;
   cloud_back_color = random([random(working_palette), "BLACK"]);
 
-  translate(canvas_x/2, canvas_y/2);
-  let num_clouds;
-  let clump = random()>0.25;
-  if(clump) num_clouds = round(random(1,10));
-  else num_clouds = round(random(10,30));
+  let num_clouds = round(random(1,20));
+  let circle_rad = random(cloud_min_radius,cloud_max_radius);
   for(let i=0; i<num_clouds; i++){
     push();
-    if(clump) translate(random(-canvas_x/8, canvas_x/8), random(-canvas_y/8,canvas_y/8));
-    else translate(random(-canvas_x/4, canvas_x/4), random(-canvas_y/4,canvas_y/4));
-    cloud(random(10,40)*global_scale, random(10,200)*global_scale, i*50, false, multi_color);
+    let cloud_width = random(10,40)*global_scale;
+    let cloud_height = random(cloud_width, cloud_width*4);
+    translate(random(cloud_width*2.5, canvas_x-cloud_width*2.5), random(cloud_height/2, canvas_y/3));
+    cloud(cloud_width, cloud_height, i*50, false, circle_rad, multi_color);
     pop();
   }
 
@@ -69,27 +67,32 @@ function draw() {
 }
 //***************************************************
 //custom funcs
-function cloud(cloud_width, cloud_height, noise_offset, rain, random_background_color){
-
+function cloud(cloud_width, cloud_height, noise_offset, rain, circle_rad, random_background_color){
   if(rain) make_it_rain();
 
   noStroke();
   //build cloud from mid bottom
   const gaussian_mean = random(-1,1);
-  const gaussian_std = random(0.5,1.5);
-  const lower_limit = -cloud_width*2*gaussian_std;
-  const upper_limit = cloud_width*2*gaussian_std;
+  const gaussian_std = random(0.75,1.5);
+  const x_upper_limit = cloud_width*2*gaussian_std;
   const circle_data = [];
-  let cloud_cirlces = map(cloud_width/global_scale*cloud_height/global_scale, 100, 8000, 1000, 20000)
+  let cloud_cirlces = map((cloud_width/global_scale)/(circle_rad/global_scale), 0.5, 7, 200, 20000);
   for(let i=0; i<cloud_cirlces; i++){
     let x = randomGaussian(gaussian_mean,gaussian_std)*cloud_width;
     //ignore point if too far left/right
-    if(x<lower_limit || x>upper_limit) continue;
+    if(abs(x)>x_upper_limit) continue;
 
     //shfit x by cloud width, as perlin noise is reflected about the 0 axis;
-    const y_max = -abs(map(noise((x+cloud_width+noise_offset)), 0,1, -cloud_height, cloud_height));
-    let y = random(-y_max/8,y_max);
-    let circle_rad = random(cloud_min_radius,cloud_max_radius);
+    const y_max = abs(map(noise((x+cloud_width+noise_offset)), 0,1, -cloud_height, cloud_height));
+    let y = random(-y_max,y_max/8);
+
+    const x_width_pct = abs(x)/x_upper_limit;
+    let y_upper_limit = cloud_height*0.5*(1-x_width_pct);
+    if(x_width_pct<0.5 && x_width_pct>0.2) y_upper_limit *= random(0.75, 0.8);
+    else if(x_width_pct<0.5) y_upper_limit *= random(0.5, 0.75);
+    constrain(y_upper_limit, 0, cloud_height * 0.5);
+    if(abs(y)>y_upper_limit) continue;
+
     x += random(-circle_rad, circle_rad);
     circle_data.push([x,y,circle_rad]);
   }
@@ -103,7 +106,11 @@ function cloud(cloud_width, cloud_height, noise_offset, rain, random_background_
     fill("BLACK");
     // if(random_background_color) fill(random(working_palette));
     // else fill(cloud_back_color);
-    circle(0,0, rad);
+    const x_wiggle = random(-1,1)*(rad)/2;
+    const y_wiggle = random(-1,1)*(rad)/2;
+    circle(x_wiggle, y_wiggle, rad);
+    data[0] += x_wiggle;
+    data[1] += y_wiggle;
     pop();
   });
   circle_data.forEach(data => {
@@ -113,7 +120,7 @@ function cloud(cloud_width, cloud_height, noise_offset, rain, random_background_
     push();
     translate(x,y);
     fill(cloud_color);
-    rad *= random(0.3,random(0.5,0.9));
+    rad *= random(0.1,random(0.5,0.9));
     const x_wiggle = random(-1,1)*(data[2]-rad)/8;
     const y_wiggle = random(-1,1)*(data[2]-rad)/8;
     circle(x_wiggle, y_wiggle, rad);
