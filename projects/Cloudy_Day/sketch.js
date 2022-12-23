fbecbb3c565ae423eaeb00e869b6d1990236ad05
lewi0622 <eric.lewiston.el@gmail.com>
2022-12-22 21:07:18 -0600

    added rain, reordered clouds into creation/drawing funcs

--- projects/Cloudy_Day/sketch.js
+++ projects/Cloudy_Day/sketch.js
@@ -8,6 +8,7 @@ const capture_time = 20;
 const sixteen_by_nine = false;
 suggested_palettes = [NURSERY, TOYBLOCKS];
 let cloud_color, cloud_back_color;
+let rain_dir; 
 function gui_values(){
   parameterize("cloud_min_radius", random(1,5), 0, 200, 1, true);
   parameterize("cloud_max_radius", random(3,10), 0, 200, 1, true);
@@ -37,16 +38,34 @@ function draw() {
   cloud_color = bg_c;
   cloud_back_color = random([random(working_palette), "BLACK"]);
 
-  let num_clouds = round(random(1,20));
+  let num_clouds = round(random(1,10));
+  // num_clouds = 1;
   let circle_rad = random(cloud_min_radius,cloud_max_radius);
+  rain_dir = random([-1,1]);
+  let cloud_info = [];
   for(let i=0; i<num_clouds; i++){
-    push();
     let cloud_width = random(10,40)*global_scale;
-    let cloud_height = random(cloud_width, cloud_width*4);
-    translate(random(cloud_width*2.5, canvas_x-cloud_width*2.5), random(cloud_height/2, canvas_y/3));
-    cloud(cloud_width, cloud_height, i*50, false, circle_rad, multi_color);
-    pop();
+    let cloud_height = random(cloud_width*1.5, cloud_width*4);
+    let starting_x = canvas_x/2 +random(-cloud_width, cloud_width);
+    let starting_y = canvas_y/2 + random(-cloud_height, cloud_height);
+    let coords = (create_cloud_coords(cloud_width, cloud_height, i*50, false, circle_rad, multi_color));
+    coords.push([starting_x, starting_y]);
+    cloud_info.push(coords);
   }
+  cloud_info.forEach(e => {
+    push();
+    let x_upper_limit = e[1];
+    translate(e[2][0], e[2][1]);
+    make_it_rain(x_upper_limit, rain_dir);
+    pop();
+  });
+  cloud_info.forEach(e => {
+    push();
+    let coords = e[0];
+    translate(e[2][0], e[2][1]);
+    draw_clouds(coords);
+    pop();
+  });
 
   //grain
   pop();
@@ -58,7 +77,6 @@ function draw() {
   for(let i=0; i<60000; i++){
     circle(random(-canvas_x/2, canvas_x*1.5), random(-canvas_y/2, canvas_y*1.5), canvas_x/2);
   }
-
   pop();
 
   //cutlines
@@ -67,14 +85,12 @@ function draw() {
 }
 //***************************************************
 //custom funcs
-function cloud(cloud_width, cloud_height, noise_offset, rain, circle_rad, random_background_color){
-  if(rain) make_it_rain();
-
+function create_cloud_coords(cloud_width, cloud_height, noise_offset, rain, circle_rad, random_background_color){
   noStroke();
   //build cloud from mid bottom
   const gaussian_mean = random(-1,1);
   const gaussian_std = random(0.75,1.5);
-  const x_upper_limit = cloud_width*2*gaussian_std;
+  const x_upper_limit = cloud_width*3*gaussian_std;
   const circle_data = [];
   let cloud_cirlces = map((cloud_width/global_scale)/(circle_rad/global_scale), 0.5, 7, 200, 20000);
   for(let i=0; i<cloud_cirlces; i++){
@@ -96,7 +112,11 @@ function cloud(cloud_width, cloud_height, noise_offset, rain, circle_rad, random
     x += random(-circle_rad, circle_rad);
     circle_data.push([x,y,circle_rad]);
   }
-  circle_data.forEach(data => {
+  return [circle_data, x_upper_limit]
+}
+
+function draw_clouds(coords){
+  coords.forEach(data => {
     const x = data[0];
     const y = data[1];
     let rad = data[2];
@@ -113,7 +133,7 @@ function cloud(cloud_width, cloud_height, noise_offset, rain, circle_rad, random
     data[1] += y_wiggle;
     pop();
   });
-  circle_data.forEach(data => {
+  coords.forEach(data => {
     const x = data[0];
     const y = data[1];
     let rad = data[2];
@@ -126,9 +146,23 @@ function cloud(cloud_width, cloud_height, noise_offset, rain, circle_rad, random
     circle(x_wiggle, y_wiggle, rad);
     pop();
   });
-
 }
 
-function make_it_rain(starting_height, max_width){
-  //todo
+function make_it_rain(max_width, dir){
+  push();
+  stroke("BLACK");
+  strokeWeight(1.25*global_scale);
+  max_width *= 0.6;
+  let rain_spacing = 10*global_scale;
+  let number_rain = max_width*2/rain_spacing;
+  let rain_length = canvas_y/4;
+  let rain_direction = dir * tan(30)*rain_length;
+  let min_dash = 5; 
+  let max_dash = 10;
+  translate(-max_width,0);
+  for(let i=0; i<number_rain; i++){
+    drawingContext.setLineDash([random(min_dash,max_dash)*global_scale, random(min_dash,max_dash)*global_scale, random(min_dash,max_dash)*global_scale, random(min_dash,max_dash)*global_scale, random(min_dash,max_dash)*global_scale]);
+    line(i*rain_spacing,0, i*rain_spacing + rain_direction, rain_length);
+  }
+  pop();
 }
