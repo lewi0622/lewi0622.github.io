67bd0878acfd0600568d3236115f88184867cc73
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-02-08 08:38:58 -0600

    added pen utils file to common and added to every index/mass index change

--- mass_index_change.py
+++ mass_index_change.py
@@ -32,6 +32,8 @@ for f in to_replace:
     <script src="'''+rel_path.as_posix()+'''/p5.gui.js"></script>
     <script src="'''+rel_path.as_posix()+'''/quicksettings.js"></script>   
     <script src="https://unpkg.com/p5.js-svg@1.3.3"></script>
+    <!-- Pen Size Files for Plotting -->
+    <script src="'''+rel_path.as_posix()+'''/pen_utils.js"></script>
     <!-- Color Picker lib -->
     <link rel="stylesheet" href="'''+rel_path.as_posix()+'''/Alwan_Color_Picker/css/alwan.min.css">
     <script src="'''+rel_path.as_posix()+'''/Alwan_Color_Picker/js/alwan.min.js"></script>
---
+++ projects/Common/pen_utils.js
@@ -0,0 +1,6 @@
+//Pen Width and Color Settings
+
+//multiply inches by 96 to obtain pixel size, use this for weight
+
+//Sakura Pigma Micron 05, 0.45mm, 0.01771654inches
+const MICRON05 = 0.01771654*96;
