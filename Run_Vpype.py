9326b6a9a222ce7c137021729b8bfb6188dde889
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-02-09 13:19:51 -0600

    Overhauled Run_Vpype to use a full interface to format vpype cli. Added deeper customization options. added show option with vpype viewer

--- Run_Vpype.py
+++ Run_Vpype.py
@@ -1,8 +1,8 @@
 import subprocess, os, glob
-import tkinter as tk
+from tkinter import *
 from tkinter.filedialog import askopenfilename
-
-tk.Tk().withdraw()
+import xml.etree.ElementTree as ET
+import webbrowser
 
 initial_dir = r"C:\Users\lewi0\Downloads"
 list_of_files = glob.glob(initial_dir + r"\*.svg")
@@ -10,56 +10,201 @@ latest_file = max(list_of_files, key=os.path.getctime)
 
 input_file = askopenfilename(initialdir=initial_dir, filetypes=(("SVG files","*.svg*"),("all files","*.*")), initialfile=latest_file)
 
+#get svg size
+tree = ET.parse(input_file)
+root = tree.getroot()
+svg_width = root.attrib["width"] #size in pixels, css units are 96 px = 1 inch 
+svg_height = root.attrib["height"] 
+svg_width_inches = float(svg_width)/96 
+svg_height_inches = float(svg_height)/96
+
 #set output file
 file_parts = os.path.splitext(input_file)
 output_file = file_parts[0] + "_PROCESSED" + file_parts[1]
 
-#options and args
-# crop 0 0 8in 10in can be placed after letter if need be
 
-#read command
-prefix = r"vpype read "
+#Define a callback function
+def callback(url):
+   webbrowser.open_new_tab(url)
 
-# conserve stroke colors
-prefix += r" -a stroke  "
 
-# read flag: doesn't crop incoming picture
-prefix += r" --no-crop "
+def run_vpypeline():
+    window.quit()
+    command = build_vpypeline()
+    print("Running: \n", command)
+    subprocess.run(command)
 
-args = r""
 
-# sclaing argument, uncomment which one is the preferred size
-#standard mat opening for an 8x10 artwork is 7.5x9.5, usually scale to something less
-# 7.3, 9.3 gives a decent space from the sides if desired
-# args += r" scaleto 9.3in 7.3in "
-# args += r" scaleto 9in 6.5in "
+def show_vpypeline():
+    show=True
+    command = build_vpypeline(show)
+    print("Showing: \n", command)
+    subprocess.run(command)
 
-# scaled for 5x7
-# args += r" scaleto 4in 6in "
-# args += r" layout 5inx7in "
 
-#full 8x10 size
-# args += r" scaleto 11in 17in "
-#slightly larger than mat opening, so edges aren't visible
-# args += r" scaleto 8.5in 11in "
-# args += r" scaleto 11in 8.5in "
-# args += r" scaleto 16.5in 12.75in "
-#full sized
-# args += r" scaleto 279.4cm 215.9cm "
-args += r" scaleto 5in 5in "
+def build_vpypeline(show=False):
+    #read command
+    prefix = r"vpype read "
 
-#occult function uses most recently drawn closed shapes to erase lines that are below the shape
-# the flag -i ignores layers and occults everything
-# args += r" occult -i "
+    # conserve stroke colors\
+    if read_stroke.get():
+        prefix += r" -a stroke  "
 
-#layout as letter centers graphics within a letter size, but does not scale it
-args += r" layout -l letter "
-# args += r" layout -l 10.125x6.875in"
-# args += r" layout 11x17in "
-#linemerge connects start/ends of shapes. linesort sorts for plotting pendown time. 
-# reloop randomizes starts ends of closed shapes to reduce alignment of up/down marks. linesimplify changes curves to polygons for better plotter control
-#still need to check on color-mode-layer
-args += r" linemerge linesort reloop linesimplify write "# --color-mode layer "
-print(prefix + '"' + input_file + '"' + args + '"' + output_file + '"')
-full_cmd = prefix + '"' + input_file + '"' + args + '"' + output_file + '"'
-# subprocess.run(full_cmd)
+    # read flag: doesn't crop incoming picture
+    if not crop.get():
+        prefix += r" --no-crop "
+
+    #standard mat opening for an 8x10 artwork is 7.5x9.5, usually scale to something less
+    args = f" scaleto {scale_width_entry.get()}in {scale_height_entry.get()}in "
+    args += f" rotate {rotate_entry.get()} "
+
+    #occult function uses most recently drawn closed shapes to erase lines that are below the shape
+    # the flag -i ignores layers and occults everything
+    if occult.get():
+        args += r" occult "
+        if occult_ignore.get():
+            args += r" -i "
+        elif occult_accross.get():
+            args += r" -a "
+ 
+    #layout as letter centers graphics within given page size
+    args += r" layout "
+    if(layout_landscape.get()):
+        args += r" -l "
+    args += f" {layout_width_entry.get()}x{layout_height_entry.get()}in "
+
+    if linemerge.get():
+        args += f" linemerge -t {linemerge_tolerance.get()} "
+
+    if linesort.get():
+        args += r" linesort "
+
+    if reloop.get():
+        args += r" reloop "  
+
+    if linesimplify.get():
+        args += f" linesimplify -t{linesimplify_tolerance.get()} "
+
+    if show:
+        args += r" show "
+        return prefix + '"' + input_file + '"' + args
+    else:
+        args += r" write "
+        # if color_mode.get():
+        #args += r" --color-mode layer "
+        return prefix + '"' + input_file + '"' + args + '"' + output_file + '"'
+
+
+#tk widgets and window
+window = Tk()
+title = Label(window, text="Vpype Options", fg="blue", cursor="hand2")
+title.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/index.html"))
+title.pack()
+
+dimensions = Label(window, text=f"Input file Width(px): {svg_width}, Height(px): {svg_height}").pack()
+
+read_stroke_label = Label(window, text="Preserve Colors", fg="blue", cursor="hand2")
+read_stroke_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/cookbook.html#preserve-color-or-other-attributes-when-reading-svg"))
+read_stroke_label.pack()
+read_stroke = IntVar(value=1)
+read_stroke_button = Checkbutton(window, text="--attr stroke", variable=read_stroke).pack()
+
+crop_label = Label(window, text="Crop to above dimensions on read", fg="blue", cursor="hand2")
+crop_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#cmdoption-read-c"))
+crop_label.pack()
+crop = IntVar()
+crop_button = Checkbutton(window, text="Crop input", variable=crop).pack()
+
+scale_label = Label(window, text="Scale options (default: input file size)", fg="blue", cursor="hand2")
+scale_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#scaleto"))
+scale_label.pack()
+scale_width_label = Label(window, text="Width Scale to (inches):").pack()
+scale_width_entry = Entry(window)
+scale_width_entry.insert(0,f"{svg_width_inches}")
+scale_width_entry.pack()
+
+scale_height_label = Label(window, text="Height Scale to (inches):").pack()
+scale_height_entry = Entry(window)
+scale_height_entry.insert(0,f"{svg_height_inches}")
+scale_height_entry.pack()
+
+rotate_label = Label(window, text="Rotate Clockwise", fg="blue", cursor="hand2")
+rotate_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#rotate"))
+rotate_label.pack()
+rotate_entry = Entry(window)
+rotate_entry.insert(0, "0")
+rotate_entry.pack()
+
+layout_label = Label(text="Layout(centers scaled design in page size)", fg="blue", cursor="hand2")
+layout_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#layout"))
+layout_label.pack()
+
+layout_width_label = Label(window, text="Page Layout Width(inches):").pack()
+layout_width_entry = Entry(window)
+layout_width_entry.insert(0,f"8.5")
+layout_width_entry.pack()
+
+layout_height_label = Label(window, text="Page Layout Height(inches):").pack()
+layout_height_entry = Entry(window)
+layout_height_entry.insert(0,f"11")
+layout_height_entry.pack()
+
+layout_landscape_label = Label(window, text="By default, the larger layout size is assumed to be the height\n Landscape flips the orientation")
+layout_landscape_label.pack()
+layout_landscape = IntVar(value=1)
+layout_landscape_button = Checkbutton(window, text="Landscape", variable=layout_landscape)
+layout_landscape_button.pack()
+
+
+occult_label = Label(window, text="Remove occluded geometries", fg="blue", cursor="hand2")
+occult_label.bind("<Button-1>", lambda e: callback("https://github.com/LoicGoulefert/occult"))
+occult_label.pack()
+occult = IntVar(value=1)
+occult_button = Checkbutton(window, text="Occult", variable=occult).pack()
+occult_ignore = IntVar(value=1)
+occult_ignore_button = Checkbutton(window, text="Ignore Layers", variable=occult_ignore).pack()
+occult_accross = IntVar(value=0)
+occult_accross_button = Checkbutton(window, text="Occult accross layers, not within", variable=occult_accross).pack()
+
+linemerge_label = Label(window, text="Merge Lines with overlapping line endings", fg="blue", cursor="hand2")
+linemerge_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#linemerge"))
+linemerge_label.pack()
+linemerge = IntVar(value=1)
+linemerge_button = Checkbutton(window, text="linemerge", variable=linemerge).pack()
+linemerge_tolerance_label = Label(window, text="Tolerance (inches) to merge lines").pack()
+linemerge_tolerance = Entry(window)
+linemerge_tolerance.insert(0, "0.001968504")
+linemerge_tolerance.pack()
+
+linesort_label = Label(window, text="Sort Lines", fg="blue", cursor="hand2")
+linesort_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#linesort"))
+linesort_label.pack()
+linesort = IntVar(value=1)
+linesort_button = Checkbutton(window, text="linesort", variable=linesort).pack()
+
+reloop_label = Label(window, text="Randomize seam location on closed paths", fg="blue", cursor="hand2")
+reloop_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#reloop"))
+reloop_label.pack()
+reloop = IntVar(value=1)
+reloop_button = Checkbutton(window, text="reloop", variable=reloop).pack()
+
+linesimplify_label = Label(window, text="Reduce geometry complexity", fg="blue", cursor="hand2")
+linesimplify_label.bind("<Button-1>", lambda e: callback("https://vpype.readthedocs.io/en/latest/reference.html#linesimplify"))
+linesimplify_label.pack()
+linesimplify = IntVar(value=1)
+linesimplify_button = Checkbutton(window, text="linesimplify", variable=linesimplify).pack()
+linesimplify_tolerance_label = Label(window, text="Tolerance (inches) to simplify geometries").pack()
+linesimplify_tolerance = Entry(window)
+linesimplify_tolerance.insert(0, "0.001968504")
+linesimplify_tolerance.pack()
+
+# color_mode = IntVar()
+# color_mode_button = Checkbutton(window, text="Order by color (will overwrite colors)", variable=color_mode).pack()
+
+#OUTPUT FILENAME, EDITABLE
+
+show_button = Button(window, text="Show Output", command=show_vpypeline).pack()
+confirm_button = Button(window, text="Confirm", command=run_vpypeline).pack()
+
+window.mainloop()
+
