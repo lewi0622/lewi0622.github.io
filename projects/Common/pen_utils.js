d756821fc321b01a603ef7d8a4cbec6a682ea67c
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-02-09 13:23:25 -0600

    added pen nib sizing for doing plotting work. Usually the size needs to be divided by 2 to give a decent fill

--- projects/Common/pen_utils.js
+++ projects/Common/pen_utils.js
@@ -2,5 +2,13 @@
 
 //multiply inches by 96 to obtain pixel size, use this for weight
 
-//Sakura Pigma Micron 05, 0.45mm, 0.01771654inches
+//Sakura Pigma Micron 05, 0.45mm, 0.01771654inches,
+//for fill, use this value/2 distance between lines
 const MICRON05 = 0.01771654*96;
+//BIC Cristal 1.6mm, 0.0629921, does not fill at this weight
+const BICCRISTAL = 0.0629921*96/2;
+// const BICCRISTAL = 0.015*96;
+
+// approx copic SuperBrush line size.
+//for fill use this value/2 distance between lines
+const COPICMARKER = 0.1*96;
