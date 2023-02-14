b27286037bc153e59d58e48f0fc3522c433ac90d
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-02-14 10:48:02 -0600

    added copic color codes for all my markers

--- projects/Common/pen_utils.js
+++ projects/Common/pen_utils.js
@@ -12,3 +12,54 @@ const BICCRISTAL = 0.0629921*96/2;
 // approx copic SuperBrush line size.
 //for fill use this value/2 distance between lines
 const COPICMARKER = 0.1*96;
+
+
+//copic marker hex codes
+const COPIC_OPACITY = 0.75;
+const COPIC_B05 = "#4EC1E6"; //process blue
+const COPIC_B06 = "#00A7E3"; //peacock blue
+const COPIC_B14 = "#7FCDEC"; //light blue
+const COPIC_B29 = "#006DBB"; //ultramarine
+const COPIC_B32 = "#EAF4F9"; //pale blue
+const COPIC_B39 = "#255CA7"; //prussian blue
+
+const COPIC_C1 = "#E6EBEE"; //cool gray no. 1
+const COPIC_C3 = "#CED6DB"; //cool gray no. 3
+const COPIC_C5 = "#A3ADB4"; //cool gray no. 5
+const COPIC_C7 = "#6D767C"; //cool gray no. 7
+const COPIC_C9 = "#3E4449"; //cool gray no. 9
+
+const COPIC_E09 = "#DA6744"; //burnt sienna
+const COPIC_E29 = "#843F2D"; //burnt umber
+const COPIC_E33 = "#F5DBB8"; //sand
+
+const COPIC_G07 = "#7FC26B"; //Nile Green
+const COPIC_G17 = "#00AA74"; //forest green
+const COPIC_G21 = "#CFE7D1"; //Lime green
+const COPIC_G28 = "#00925C"; //Ocean Green
+
+const COPIC_R02 = "#FBDACD"; //Rose Salmon (flesh)
+const COPIC_R08 = "#EB6148"; //vermillion
+
+const COPIC_RV11 = "#FBDEE3"; //pink
+const COPIC_RV29 = "#E73D78"; //Crimson
+
+const COPIC_V09 = "#894A99"; //Violet
+
+const COPIC_W1 = "#EFEFE9"; //warm gray no. 1
+const COPIC_W3 = "#DDDCD6"; //warm gray no. 3
+const COPIC_W5 = "#B5B5AF"; //warm gray no. 5
+const COPIC_W7 = "#7D7C77"; //warm gray no. 7
+const COPIC_W9 = "#4C4944"; //warm gray no. 9
+
+const COPIC_Y11 = "#FFFCD1"; //pale yellow
+const COPIC_Y13 = "#FEF8B0"; //lemon yellow
+const COPIC_Y15 = "#FFEB63"; //cadmium yellow
+
+const COPIC_YG03 = "#E6EDAB"; //yellow green
+
+const COPIC_YR04 = "#FBCA5E"; //Chrome orange
+const COPIC_YR24 = "#F4D95B"; //pale sepia
+
+const COPIC_BLACK = "#2F2521"; //Black
+const COPIC_SPECIAL_BLACK = "#231815"; //special black
