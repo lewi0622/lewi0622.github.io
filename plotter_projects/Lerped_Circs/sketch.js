277eca4a2a40d5c98fc3d3cca5b86787fed49ec6
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-05-12 18:37:53 -0500

    added lerped circs, needs wircs

---
+++ plotter_projects/Lerped_Circs/sketch.js
@@ -0,0 +1,63 @@
+'use strict';
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 1;
+const capture = false;
+const capture_time = 10;
+const sixteen_by_nine = false;
+let x_fourth, y_fourth, copic_palette;
+suggested_palettes = []
+
+function gui_values(){
+  parameterize("number_circles", 20, 1, 400, 1, false);
+  parameterize("starting_radius", 50, 0, 200, 1, true);
+  
+} 
+
+function setup() {
+  common_setup(gif, SVG, 6*96, 6*96);
+}
+//***************************************************
+function draw() {
+  capture_start(capture);
+
+  //bleed
+  const bleed_border = apply_bleed();
+
+  refresh_working_palette();
+
+  //actual drawing stuff
+  push();
+  noFill();
+  stroke(0,0,0,150);
+
+  let point_1 = createVector(
+    canvas_x/2 + random(-1,1)*canvas_x/16,
+    canvas_y/2 + random(-1,1)*canvas_y/16)
+  let point_2 = createVector(
+    canvas_x/2 + random(-1,1)*canvas_x/8,
+    canvas_y/2 + random(-1,1)*canvas_y/8)
+  let point_3 = createVector(
+    canvas_x/2 + random(-1,1)*canvas_x/4,
+    canvas_y/2 + random(-1,1)*canvas_y/4)
+  let points = [point_1, point_2, point_3];
+
+  // let radius = starting_radius;
+  for(let j=0; j<points.length-1; j++){
+    let start = points[j];
+    let end = points[j+1];
+    for(let i=0; i<number_circles; i++){
+      let target = p5.Vector.lerp(start,end, i/number_circles);
+      let radius = starting_radius + 2*abs(dist(start.x, start.y, target.x, target.y)); 
+      circle(target.x, target.y, radius);
+    }
+  }
+  pop();
+  //cleanup
+  apply_cutlines(bleed_border);
+
+  capture_frame(capture);
+}
+//***************************************************
+//custom funcs
