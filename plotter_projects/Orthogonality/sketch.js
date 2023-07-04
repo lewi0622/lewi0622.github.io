d90e0d49eeabda9b39d51d576171f9bba3429f4b
lewi0622 <eric.lewiston.el@gmail.com>
2023-07-04 08:19:30 -0500

    added orthogonality, grid working, needs noise

---
+++ plotter_projects/Orthogonality/sketch.js
@@ -0,0 +1,53 @@
+'use strict';
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 1;
+const capture = false;
+const capture_time = 8
+
+
+function gui_values(){
+  parameterize('num_vertical_lines', 25, 1, 200, 1, false);
+  parameterize('num_horizontal_lines', 25, 1, 200, 1, false);
+  parameterize('vertical_points', 10, 1, 200, 1, false);
+  parameterize('horizontal_points', 10, 1, 200, 1, false);
+}
+
+function setup() {
+  common_setup(6*96, 6*96, SVG);
+}
+//***************************************************
+function draw() {
+  global_draw_start();
+  push();
+  let c = color("BLUE");
+  c.setAlpha(100);
+  stroke(c);
+  for(let i=0; i<num_vertical_lines; i++){
+    beginShape();
+    for(let j=0; j<=vertical_points; j++){
+      let x = i*canvas_x/num_vertical_lines;
+      let y = j*canvas_y/vertical_points;
+      vertex(x,y);
+    }
+    endShape();
+  }
+
+  c = color("RED");
+  c.setAlpha(100);
+  stroke(c);
+  for(let i=0; i<num_horizontal_lines; i++){
+    beginShape();
+    for(let j=0; j<=horizontal_points; j++){
+      let x = i*canvas_x/num_horizontal_lines;
+      let y = j*canvas_y/horizontal_points;
+      vertex(y,x);
+    }
+    endShape();
+  }
+  pop();
+  global_draw_end();
+}
+//***************************************************
+//custom funcs
