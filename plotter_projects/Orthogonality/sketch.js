b704fbf196e5036510115723dfcca3e7125bc6c5
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-07-05 08:32:45 -0500

    added params

--- plotter_projects/Orthogonality/sketch.js
+++ plotter_projects/Orthogonality/sketch.js
@@ -12,23 +12,28 @@ function gui_values(){
   parameterize('num_horizontal_lines', 25, 1, 200, 1, false);
   parameterize('vertical_points', 10, 1, 200, 1, false);
   parameterize('horizontal_points', 10, 1, 200, 1, false);
+  parameterize("amp", 10, 0, 100, 0.1, true);
+  parameterize("i_damp", 10, 1, 100, 1, false);
+  parameterize("j_damp", 10, 1, 100, 1, false);
 }
 
 function setup() {
-  common_setup(6*96, 6*96, SVG);
+  common_setup(4*96, 6*96, SVG);
 }
 //***************************************************
 function draw() {
   global_draw_start();
   push();
+  translate(-canvas_x/2, -canvas_y/2);
+  noFill();
   let c = color("BLUE");
   c.setAlpha(100);
   stroke(c);
   for(let i=0; i<num_vertical_lines; i++){
     beginShape();
     for(let j=0; j<=vertical_points; j++){
-      let x = i*canvas_x/num_vertical_lines;
-      let y = j*canvas_y/vertical_points;
+      let x = noise(i/i_damp, j/j_damp)*amp + i*canvas_x*1.5/num_vertical_lines;
+      let y = j*canvas_y*1.5/vertical_points;
       vertex(x,y);
     }
     endShape();
@@ -40,8 +45,8 @@ function draw() {
   for(let i=0; i<num_horizontal_lines; i++){
     beginShape();
     for(let j=0; j<=horizontal_points; j++){
-      let x = i*canvas_x/num_horizontal_lines;
-      let y = j*canvas_y/horizontal_points;
+      let x = noise(i/i_damp, j/j_damp)*amp + i*canvas_y*1.5/num_horizontal_lines;
+      let y = j*canvas_x*1.5/horizontal_points;
       vertex(y,x);
     }
     endShape();
