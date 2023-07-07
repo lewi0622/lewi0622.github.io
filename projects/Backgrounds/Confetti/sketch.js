c2725adf514a055c9e2731b4fa02cf3da4851c07
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-07-07 08:55:11 -0500

    added 90s as confetti and new proj squiggles for backgrounds

---
+++ projects/Backgrounds/Confetti/sketch.js
@@ -0,0 +1,102 @@
+'use strict';
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 5;
+const capture = false;
+const capture_time = 2;
+const sixteen_by_nine = false;
+let grid_bg_c;
+suggested_palettes = [BIRDSOFPARADISE, NURSERY, SIXTIES, JAZZCUP];
+
+
+function gui_values(){
+  parameterize("grid_size", 50, 50, 400, 10, true);
+  parameterize("wobble_x", 50, -400, 400, 5, true);
+  parameterize("wobble_y", 30, -400, 400 ,5, true);
+  parameterize("arc_size", 1, 1, 400, 1, true);
+  parameterize("weight", random(3,30), 1, 100, 1, true);
+}
+
+function setup() {
+  common_setup();
+}
+//***************************************************
+function draw() {
+  global_draw_start();
+
+  refresh_working_palette();
+  //actual drawing stuff
+  push();
+  let num_cols = floor(canvas_y/grid_size);
+  let num_rows = floor(canvas_x/grid_size);
+  num_cols = constrain(num_cols,1,num_cols);
+  num_rows = constrain(num_rows,1,num_cols);
+  if(canvas_y<canvas_x) grid_size = canvas_y/num_cols;
+  else grid_size = canvas_x/num_rows;
+  num_rows = floor(canvas_x/grid_size);
+  num_cols = floor(canvas_y/grid_size);
+  
+  translate((canvas_x-grid_size*num_rows)/2, (canvas_y-grid_size*num_cols)/2);
+  const num_arcs = 2;
+  if(frameCount==1){
+    grid_bg_c = random(working_palette);
+  }
+  background(grid_bg_c);
+  strokeWeight(weight);
+  for(let col=0; col<num_cols; col++){
+    for(let row=0; row<num_rows; row++){
+      push();
+      translate(row*grid_size+grid_size/2, col*grid_size+grid_size/2);
+      rotate(random(360));
+      for(let i=0; i<num_arcs; i++){
+        push();
+        rotate(360/num_arcs*i);
+        translate(random(wobble_x), random(wobble_y));
+        refresh_working_palette();
+        reduce_array(working_palette, grid_bg_c);
+        let dir = "LEFT";
+        if(i!=0){
+          dir = "RIGHT";
+        }
+        slices(arc_size, dir);
+        pop();
+      }
+      pop();
+    }
+  }
+  pop();
+
+  global_draw_end();
+}
+//***************************************************
+//custom funcs
+function slices(rad, dir){
+  const c1 = random(working_palette);
+  reduce_array(working_palette, c1);
+  const c2 = random(working_palette);
+  const clear = color(c1);
+  clear.setAlpha(0);
+  const fill_gradient = drawingContext.createLinearGradient(0,0, rad,0);
+  fill_gradient.addColorStop(0,color(c1));
+  fill_gradient.addColorStop(0.9,color(clear));
+  push();
+  noStroke();
+  drawingContext.fillStyle = fill_gradient;
+  beginShape();
+  vertex(rad,0);
+  bezierVertex(random(-rad,rad),rad, -random(-rad,rad),rad, -rad,0); //first control point, second control point, endpoint
+  endShape();
+  pop();
+
+  noFill();
+  stroke(c2);
+  drawingContext.shadowBlur=2*global_scale;
+  drawingContext.shadowColor = color(c2);
+  translate(0, rad*0.1);
+  rad *=0.8;
+  beginShape();
+  vertex(rad,0);
+  bezierVertex(random(-rad,rad),rad, -random(-rad,rad),rad, -rad,0); //first control point, second control point, endpoint
+  endShape();
+}
---
+++ projects/Backgrounds/Squiggles/sketch.js
@@ -0,0 +1,62 @@
+'use strict';
+
+//setup variables
+const gif = false;
+const animation = false;
+const fr = 1;
+const capture = false;
+const capture_time = 10;
+const sixteen_by_nine = false;
+suggested_palettes = [SAGEANDCITRUS, COTTONCANDY, BUMBLEBEE, BIRDSOFPARADISE, SOUTHWEST, NURSERY];
+
+function gui_values(){
+  parameterize("num_lines", 500, 1, 500, 1, false);
+  parameterize("line_steps", 10, 1, 5000, 1, false);
+  parameterize("line_step_size", 0.2, 0.01, 10, 0.01, false); //step size along noise curve
+  parameterize("amp", random(5,15), 1, 100, 1, true);
+}
+
+function setup() {
+  common_setup();
+}
+//***************************************************
+function draw() {
+  global_draw_start();
+
+  //actual drawing stuff
+  push();
+  center_rotate(random(360));
+  let bg_c = random(working_palette);
+  reduce_array(working_palette, bg_c);
+  background(bg_c);
+
+  strokeWeight(1*global_scale);
+
+  noFill();
+  for(let i=0; i<num_lines; i++){
+    let c = color(random(working_palette));
+    c.setAlpha(180);
+    stroke(c);
+    let start_x = random(canvas_x*1.25);
+    let start_y = random(canvas_y*1.25);
+    const noise_x_offset = random(500);
+    const noise_y_offset = random(500);
+    beginShape();
+    for(let j=0; j<line_steps; j++){
+      const x = map(noise(start_x + noise_x_offset, i, j*line_step_size), 0,1, -amp, amp);
+      const y = map(noise(start_y + noise_y_offset, i, j*line_step_size), 0,1, -amp, amp)
+      start_x += x;
+      start_y += y;
+      curveVertex(start_x, start_y);
+
+      if(j/line_steps>0.5 && (start_x>canvas_x || start_x<0 || start_y>canvas_y || start_y<0)) break;
+    }
+    endShape();
+  }
+
+  pop();
+
+  global_draw_end();
+}
+//***************************************************
+//custom funcs
