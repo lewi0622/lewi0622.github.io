c2cc37736f511e00c76a31e5d997d733745c1134
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-08-19 09:50:38 -0500

    rewrote algo resulting in better lines

--- plotter_projects/Intersecting_Noise/sketch.js
+++ plotter_projects/Intersecting_Noise/sketch.js
@@ -11,17 +11,14 @@ suggested_palettes = [LASER]
 
 function gui_values(){
   parameterize("points_per_line", 500, 1, 500, 1, false);
-  parameterize("amp", 100, 0, 1000, 1, true);
-  parameterize("i_damp", random(200, 1000), 1, 1000, 1, false);
+  parameterize("amp", random(20,500), 0, 1000, 1, true);
+  parameterize("i_damp", random(300, 1000), 1, 1000, 1, false);
   parameterize("j_boost", random(1,100), 1, 100, 1, false);
-  parameterize("number_of_lines", floor(random(20, 100)), 1, 500, 1, false);
-  parameterize("offset", 0, 0, 50, 0.1, true);
-  parameterize("z_iterations", 1, 1, 7, 1, false);
-  parameterize("z_damp", 1, 1, 100, 1, false);
+  parameterize("number_of_lines", 150, 3, 500, 1, false);
 }
 
 function setup() {
-  common_setup(11*96, 8.5*96, SVG);
+  common_setup(8*96, 10*96, SVG);
 }
 //***************************************************
 function draw() {
@@ -31,95 +28,89 @@ function draw() {
   push();
   noFill();
   translate(canvas_x/2, 0);
-  const i_offset = random(-200, 500);
-  for(let z=0; z<z_iterations; z++){
-    let c = color(working_palette[z]);
-    c.setAlpha(150);
-    stroke(c);
-    const x_min = [];
-    const x_max = [];
-    const y_step_size = canvas_y/points_per_line;
 
-    
-    let x_offset = 0;
-    let dir, rev;
-    for(let j=0; j<number_of_lines; j++){
-      if(j != 0){
-        if((j-1)%4==0 || (j-2)%4==0) dir = 1;
-        else dir = -1;
-
-        if((j-2)%4==0 || (j-3)%4==0) rev = 1;
-        else rev = 0;
-      }
-      else{ // if first loop, special case
-        dir = 1;
-        rev = 0;
-      }
-      beginShape();
-      if(rev == 0){
-        if(j != 0){
-          if(dir == -1) x_offset = x_min[0] - offset;
-          else x_offset = x_max[0] + offset;
-        }
-
-        for(let i=0; i<points_per_line; i++){
-          const x = x_offset + map(noise((i + i_offset)/i_damp, j*j_boost, (z+1)/z_damp), 0,1, -amp, amp);
-          const y = i*y_step_size;
-          if(j==0){
-            x_min[i] = x;
-            x_max[i] = x;
-          }
-          else if(dir == 1 && x<x_max[i]){
-            vertex(x_max[i], y);
-            break;
-          }
-          else if(dir == -1 && x>x_min[i]){
-            vertex(x_min[i], y);
-            break;
-          }
-    
-          vertex(x,y);
-    
-          if(x>x_max[i]) x_max[i] = x;
-          if(x<x_min[i]) x_min[i] = x;
-    
-        }
-      }
-      else{
-        x_min.reverse();
-        x_max.reverse();
-        if(dir == -1) x_offset = x_min[0] - offset;
-        else x_offset = x_max[0] + offset;
-
-        for(let i=0; i<points_per_line; i++){
-          const x = x_offset + map(noise((i + i_offset)/i_damp, j*j_boost, (z+1)/z_damp), 0,1, -amp, amp);
-          const y = canvas_y - i*y_step_size;
-          if(j==0){
-            x_min[i] = x;
-            x_max[i] = x;
-          }
-          else if(dir == 1 && x<x_max[i]){
-            vertex(x_max[i], y);
-            break;
-          }
-          else if(dir == -1 && x>x_min[i]){
-            vertex(x_min[i], y);
-            break;
-          }
-    
-          vertex(x,y);
-    
-          if(x>x_max[i]) x_max[i] = x;
-          if(x<x_min[i]) x_min[i] = x;
-    
-        }
-        x_min.reverse();
-        x_max.reverse();
-      }
-      
-      endShape();
-    }
+  let lines_pts = [];
+  for(let i=0; i<points_per_line; i++){
+    lines_pts.push([]);
   }
+  let line_id = 0;
+  const y_step_size = canvas_y/points_per_line;
+
+  //draw outer lines
+  const starting_min_x = random(-canvas_x/3, -canvas_x/4);
+  let ending_min_x;
+  beginShape();
+  for(let i=0; i<points_per_line; i++){
+    const x = starting_min_x + map(noise((i+starting_min_x)/i_damp), 0,1, -amp, amp);
+    const y = i*y_step_size;
+    vertex(x,y);
+    lines_pts[i].push({id:line_id, x:x});
+
+    if(i+1 == points_per_line) ending_min_x = x;
+  }
+  endShape();
+  line_id++;
+
+  const starting_max_x = random(canvas_x/4, canvas_x/3);
+  let ending_max_x;
+  beginShape();
+  for(let i=0; i<points_per_line; i++){
+    const x = starting_max_x + map(noise((i+starting_max_x)/i_damp), 0,1, -amp, amp);
+    const y = i*y_step_size;
+    vertex(x,y);
+    lines_pts[i].push({id:line_id, x:x});
+
+    if(i+1 == points_per_line) ending_max_x = x;
+  }
+  endShape();
+  line_id++;
+
+  for(let j=0; j<number_of_lines-2; j++){
+    let reverse = j%2 != 0;
+    let starting_x = random(starting_min_x, starting_max_x);
+    if(reverse){
+      starting_x = random(ending_min_x, ending_max_x);
+      lines_pts.reverse();
+    }
+
+    //determine which IDs are to the left, and which are to the right of this line
+    const left_ids = [];
+    const right_ids = [];
+
+    beginShape();
+    for(let i=0; i<points_per_line; i++){
+      const x = starting_x + map(noise((i+starting_x)/i_damp), 0,1, -amp, amp);
+      let y = i*y_step_size;
+      if(reverse) y = canvas_y - i*y_step_size;
+
+      //collision check
+      let collision = false;
+      for(let z=0; z<lines_pts[i].length; z++){
+        const prev_id = lines_pts[i][z].id;
+        const prev_x = lines_pts[i][z].x;
+
+        //check if in neither column, and add it to one
+        if(!left_ids.includes(prev_id) && !right_ids.includes(prev_id)){
+          if(prev_x<=x) left_ids.push(prev_id);
+          else right_ids.push(prev_id);
+        }
+
+        if((prev_x<=x && !left_ids.includes(prev_id)) || (prev_x>x && !right_ids.includes(prev_id))){
+          vertex(prev_x,y);
+          collision = true;
+          print(lines_pts)
+        }
+      }
+
+      if(collision) break;
+      vertex(x,y);
+      lines_pts[i].push({id:line_id, x:x});
+    }
+    endShape();
+    if(reverse) lines_pts.reverse();
+    line_id++;
+  }
+
   pop();
 
   global_draw_end();
