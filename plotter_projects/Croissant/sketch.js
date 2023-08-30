36d82b59b30d096f5ce9290925a1754634f3ed57
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-08-30 14:14:18 -0500

    added randomization of params and better rotation

--- plotter_projects/Croissant/sketch.js
+++ plotter_projects/Croissant/sketch.js
@@ -11,23 +11,24 @@ suggested_palettes = []
 
 function gui_values(){
   parameterize("number_of_circles", 100, 1, 1000, 1, false);
-  parameterize("n_points", 3, 3, 300, 1, false);
-  parameterize("shape_rotation", 0, 0, 360, 15, false);
-  parameterize("point_1_x", 50, -500, 1500, 1, true);
-  parameterize("point_2_x", 150, -500, 1500, 1, true);
-  parameterize("point_3_x", 300, -500, 1500, 1, true);
-  parameterize("point_4_x", 450, -500, 1500, 1, true);
-  parameterize("point_5_x", 600, -500, 1500, 1, true);
-  parameterize("point_1_y", 0, -500, 1500, 1, true);
-  parameterize("point_2_y", 0, -500, 1500, 1, true);
-  parameterize("point_3_y", 0, -500, 1500, 1, true);
-  parameterize("point_4_y", 0, -500, 1500, 1, true);
-  parameterize("point_5_y", 0, -500, 1500, 1, true);
-  parameterize("radius_1", 0, 0, 500, 1, true);
-  parameterize("radius_2", random(300), 0, 500, 1, true);
-  parameterize("radius_3", random(300), 0, 500, 1, true);
-  parameterize("radius_4", random(300), 0, 500, 1, true);
-  parameterize("radius_5", 0, 0, 500, 1, true);
+  parameterize("n_points", random([3,4,5,6,7,8,9,10,11,12,100]), 3, 300, 1, false);
+  parameterize("overall_rotation", random(360), 0, 360, 15, false);
+  parameterize("rotation_per_shape", random(-5,5), -10, 10, 0.1, false);
+  parameterize("point_1_x", random(50, 650), -500, 1500, 1, true);
+  parameterize("point_2_x", random(50, 650), -500, 1500, 1, true);
+  parameterize("point_3_x", random(50, 650), -500, 1500, 1, true);
+  parameterize("point_4_x", random(50, 650), -500, 1500, 1, true);
+  parameterize("point_5_x", random(50, 650), -500, 1500, 1, true);
+  parameterize("point_1_y", random(50, 450), -500, 1500, 1, true);
+  parameterize("point_2_y", random(50, 450), -500, 1500, 1, true);
+  parameterize("point_3_y", random(50, 450), -500, 1500, 1, true);
+  parameterize("point_4_y", random(50, 450), -500, 1500, 1, true);
+  parameterize("point_5_y", random(50, 450), -500, 1500, 1, true);
+  parameterize("radius_1", random(50), 0, 500, 1, true);
+  parameterize("radius_2", random(250), 0, 500, 1, true);
+  parameterize("radius_3", random(250), 0, 500, 1, true);
+  parameterize("radius_4", random(250), 0, 500, 1, true);
+  parameterize("radius_5", random(50), 0, 500, 1, true);
 }
 
 function setup() {
@@ -72,7 +73,9 @@ function draw() {
     const rad = lerp(rad_1, rad_2, 4*i/number_of_circles%1);
     push();
     translate(x,y);
-    rotate(shape_rotation);
+    rotate(overall_rotation);
+    rotate(i*rotation_per_shape);
+    //add options for RECT with/out rounded corners and ellipses
     polygon(0,0,rad, n_points);
     pop();
   }
