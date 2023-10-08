f4ba1f1a1c326a84df03a5e3dc8bed1415918ef8
Eric Lewiston <eric.lewiston.el@gmail.com>
2023-10-08 06:39:42 -0500

    changed size to be 8x10

--- plotter_projects/Rays/sketch.js
+++ plotter_projects/Rays/sketch.js
@@ -23,7 +23,7 @@ function gui_values(){
 } 
 
 function setup() {
-  common_setup(6*96, 6*96, SVG);
+  common_setup(10*96, 8*96, SVG);
 }
 //***************************************************
 function draw() {
