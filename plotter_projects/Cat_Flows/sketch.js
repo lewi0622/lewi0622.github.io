997b1d28056339d0398bd0ac8dc66cc4dd03fe19
lewi0622 <eric.lewiston.el@gmail.com>
2023-11-05 17:17:46 -0600

    added whiskers

--- plotter_projects/Cat_Flows/sketch.js
+++ plotter_projects/Cat_Flows/sketch.js
@@ -11,21 +11,20 @@ suggested_palettes = [BIRDSOFPARADISE, SOUTHWEST, NURSERY, SIXTIES]
 let shape_points;
 
 function gui_values(){
-  parameterize('inc', 0.1, 0.01, 10, 0.01, false);
-  parameterize("zinc", 0.1, 0, 1, 0.01, false);
-  parameterize("scl", 20, 1, 100, 1, true);
-  parameterize("z_iterations", 1, 1, 100, 1, false);
+  parameterize('inc', random(0.01,0.25), 0.01, 10, 0.01, false);
+  parameterize("scl", random(20,50), 1, 100, 1, true);
 
   parameterize("debug", 0, 0, 1, 1, false);
-  parameterize("density_multiplier", 0.26, 0.001, 1, 0.01, false);
+  parameterize("density_multiplier", random(0.07,0.25), 0.001, 1, 0.01, false);
   parameterize("radius", 45, 1, 100, 1, true);
   parameterize("radius_variation", 0, 0, 1, 0.1, false);
-  parameterize('number_of_shapes', 1, 1, 100, 1, false);
   parameterize("point_skip_chance", 0, 0, 1, 0.01, false);
+  parameterize("num_inline", 50, 1, 100, 1, false);
 }
 
 function setup() {
-  common_setup(6*96, 8*96, SVG);
+  // common_setup(6*96, 8*96, SVG);
+  common_setup();
 }
 //***************************************************
 function draw() {
@@ -36,50 +35,85 @@ function draw() {
   translate(-canvas_x/2, -canvas_y/2);
   //actual drawing stuff
 
-  push();
-  let zoff=0;
-  for(let z=0; z<z_iterations; z++){
-    if(z==1)stroke(c2);
-    let yoff=0;
-    for(let y=0; y<rows; y++){
-      let xoff=0;
-      for(let x=0; x<cols; x++){
-        stroke(random(working_palette));
-        let index = x + y * cols;
-        let angle = noise(xoff, yoff, zoff) * 360;
-        let v = p5.Vector.fromAngle(radians(angle));
-        xoff += inc; 
-        push();
-        translate(x*scl, y*scl);
-        rotate(degrees(v.heading()));
+  //todo try adding pt in middle to constrain width
 
-        shape_points = [];
-        const pt1 = {x:0, y:0};
-        const pt2 = {x:scl, y:0};
-        out_or_back(pt1, pt2, 1);
-        out_or_back(pt1, pt2, -1);
-        if(shape_points.length>2){
-          fill("WHITE")
-          strokeWeight(0.3*global_scale);
-          if(debug){
-            noFill();
-            stroke("BLACK");
-          }
+  push();
+  let yoff=0;
+  for(let y=0; y<rows; y++){
+    let xoff=0;
+    for(let x=0; x<cols; x++){
+      stroke(random(working_palette));
+      let index = x + y * cols;
+      let angle = noise(xoff, yoff) * 360;
+      let v = p5.Vector.fromAngle(radians(angle));
+      xoff += inc; 
+      push();
+      translate(x*scl, y*scl);
+      rotate(degrees(v.heading()));
+
+      shape_points = [];
+      const pt1 = {x:0, y:0};
+      const pt2 = {x:scl, y:0};
+      out_or_back(pt1, pt2, 1);
+      out_or_back(pt1, pt2, -1);
+      if(shape_points.length>2){
+        fill("WHITE")
+        strokeWeight(0.3*global_scale);
+        if(debug){
+          noFill();
+          stroke("BLACK");
+        }
+        let center_x = Math.max(...shape_points.map(o => o.x)) / 2;
+        let center_y = 0;
+        for(let j=0; j<num_inline; j++){
+          if(j/num_inline > 0.05) curveTightness(1);
+          else curveTightness(0);
+          //full outline plus ears
           beginShape();
           for(let i=0; i<shape_points.length+3; i++){
-            const ptx = shape_points[i%shape_points.length].x;
-            const pty = shape_points[i%shape_points.length].y;
+            let ptx = shape_points[i%shape_points.length].x;
+            let pty = shape_points[i%shape_points.length].y;
+            pty = lerp(pty, center_y, j/num_inline);
+            if(i%shape_points.length==0) pty *= 0.8;
+            if((i+1)%shape_points.length>0 && i%shape_points.length<0) pty *= 0.8;
             curveVertex(ptx, pty);
+            //trying to remove feet, tbd
 
+            // if(i>(shape_points.length+3)/2) circle(ptx,pty,5);
+            // if(i==8)circle(ptx,pty,10);
           }
           endShape();
+          // else{
+          //   //just fill lines
+          //   let pty = shape_points[0].y;
+          //   pty = lerp(pty, center_y, j/num_inline);
+          //   line(0, pty, center_x*2, pty);
+          //   line(0, -pty, center_x*2, -pty);
+          // }
         }
-        // line(0,0,scl,0);
+        line(0,0,center_x*2, center_y);
+
+        //whiskers
+        push();
+        translate(center_x, radius * random(0.6, 0.7));
+        stroke("BLACK");
+        let num_whiskers = floor(random(3,5));
+        let whisker_offset = scl * random(0.03, 0.07);
+        let whisker_length = scl * random(0.3,0.7);
+        let starting_rot = -20;
+        for(let i=0; i<num_whiskers; i++){
+          push();
+          rotate(lerp(starting_rot, 20, (i+0.5)/num_whiskers));
+          line(whisker_offset, 0, whisker_length, 0);
+          line(-whisker_offset, 0, -whisker_length, 0);
+          pop();
+        }
         pop();
       }
-      yoff += inc;
+
+      pop();
     }
-    zoff += zinc;
+    yoff += inc;
   }
   pop();
   
