f6a352392f06ab3037380d78f03024e57583a8a5
lewi0622 <eric.lewiston.el@gmail.com>
2023-11-29 19:56:00 -0600

    added paramater randomizations

--- plotter_projects/Hex_Gone/sketch.js
+++ plotter_projects/Hex_Gone/sketch.js
@@ -8,21 +8,22 @@ const capture_time = 8
 
 
 function gui_values(){
-  parameterize("start_rad", 5, 1, 100, 1, true, grid_slider_1);
-  parameterize("rad_inc", 1, 1, 10, 1, true);
+  parameterize("start_rad", 1, 1, 100, 1, true, grid_slider_1);
+  parameterize("rad_inc", random(2,10), 1, 10, 1, true);
   parameterize("thic", 1, 1, 10, 1, false, grid_slider_2);
-  parameterize("rot", 10, 0, 360, 1, false, grid_slider_3);
-  parameterize("num_hexes", 10, 1, 100, 1, false, grid_slider_4);
+  parameterize("rot", random(360), 0, 360, 1, false, grid_slider_3);
+  parameterize("num_hexes", floor(random(5,50)), 1, 100, 1, false, grid_slider_4);
+  parameterize("num_colors", 2, 1, 5, 1, false);
 }
 
 function setup() {
-  common_setup(3.5*96, 3.5*96, SVG);
+  common_setup(6*96, 6*96);
 }
 //***************************************************
 function draw() {
   global_draw_start();
 
-  const colors = gen_n_colors(2);
+  const colors = gen_n_colors(num_colors);
   //actual drawing stuff
   push();
   let hex_rad = start_rad + rad_inc*num_hexes;
