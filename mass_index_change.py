879c6b9b5b549e4784b9de3117ceb8aad38caccb
lewi0622 <eric.lewiston.el@gmail.com>
2024-01-01 17:38:41 -0600

    changed mass_index to point to the minified version of p5js, v 1.6.0. updated all index files

--- mass_index_change.py
+++ mass_index_change.py
@@ -21,7 +21,7 @@ for f in to_replace:
     rel_path = pathlib.Path(rel_path)
     
     file_contents = '''<!DOCTYPE html><html lang="en"><head>
-    <script src="'''+rel_path.as_posix()+'''/p5.js"></script>
+    <script src="'''+rel_path.as_posix()+'''/p5.min.js"></script>
     <!-- Custom scripts -->
     <script src="'''+rel_path.as_posix()+'''/palettes.js"></script>
     <script src="'''+rel_path.as_posix()+'''/utils.js"></script>
