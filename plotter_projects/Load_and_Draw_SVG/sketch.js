38265a1b6efc406e7245b974e031003930947c1f
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-01-05 14:36:28 -0600

    added load svg attributes func

--- plotter_projects/Load_and_Draw_SVG/sketch.js
+++ plotter_projects/Load_and_Draw_SVG/sketch.js
@@ -6,17 +6,24 @@ const fr = 1;
 const capture = false;
 const capture_time = 10;
 
-suggested_palettes = []
+const suggested_palettes = []
 let my_svg;
+let svg_width, svg_height, svg_viewbox;
 function preload(){
-  my_svg = loadSVG('Demo.svg');
+  my_svg = loadSVG('Demo.svg', load_svg_attributes);
+}
+
+function load_svg_attributes(){
+  svg_width = my_svg.elt.attributes.width.value;
+  svg_height = my_svg.elt.attributes.height.value;
+  svg_viewbox = my_svg.elt.attributes.viewBox.value.split(" ");
 }
 
 function gui_values(){
 }
 
 function setup() {
-  common_setup(6*96, 8*96, SVG);
+  common_setup(svg_viewbox[2], svg_viewbox[3], SVG);
 }
 //***************************************************
 function draw() {
@@ -25,7 +32,7 @@ function draw() {
   //actual drawing stuff
   push();
 
-  image(my_svg, 0,0, width, height);
+  image(my_svg, 0, 0, width, height);
 
 
   pop();
