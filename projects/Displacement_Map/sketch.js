4220607ca027c7e2948c040e3372ade774cfb7c6
lewi0622 <eric.lewiston.el@gmail.com>
2024-01-17 21:21:35 -0600

    changed design to draw little by little so the page doesn't hang

--- projects/Displacement_Map/sketch.js
+++ projects/Displacement_Map/sketch.js
@@ -2,16 +2,21 @@
 //setup variables
 const gif = false;
 const animation = false;
-const fr = 30;
+const fr = 120;
 const capture = false;
 const capture_time = 10;
 
-const suggested_palettes = [BEACHDAY, COTTONCANDY, BIRDSOFPARADISE, SOUTHWEST]
+let k;
+let y_offset;
+let noise_map;
+let total_loops;
+
+const suggested_palettes = [BEACHDAY, COTTONCANDY, SOUTHWEST, SIXTIES];
 
 function gui_values(){
   parameterize("num_rows", 200, 1, 1000, 1, false);
   parameterize("num_cols", 200, 1, 1000, 1, false);
-  parameterize("amp_noise", random(0.5,1.5), 0, 5, 0.1, false);
+  parameterize("amp", random(0.5,1.5), 0, 5, 0.1, false);
   parameterize("num_warps", 50, 0, 100, 1, false);
   parameterize("x_damp", random(15,40), 1, 100, 1, false);
   parameterize("y_damp", random(15,40), 1, 100, 1, false);
@@ -22,13 +27,11 @@ function gui_values(){
 
 function setup() {
   common_setup();
-}
-//***************************************************
-function draw() {
-  global_draw_start();
+
+  k=0;
+  noise_map = [];
 
   refresh_working_palette();
-
   //apply background
   noStroke();
   working_palette = controlled_shuffle(working_palette, true);
@@ -39,41 +42,55 @@ function draw() {
   working_palette = [];
   for(let i=0; i<num_colors; i++) working_palette.push(palette_placeholder[i]);
   
-  const y_offset = random(500, 2000);
+  y_offset = random(500, 2000);
+
+  for(let i=0; i<num_rows; i++){
+    for(let j=0; j<num_cols; j++){
+      let x = j;
+      let y = i;
+      const c_index = map(pnoise.simplex2(x / x_color_damp, y / y_color_damp), -1,1, 0,1);
+      noise_map.push({
+        x:x,
+        y:y,
+        c:color_map(c_index)
+      })
+    }
+  }
+
+  total_loops = num_warps * noise_map.length;
+
+  loop();
+}
+//***************************************************
+function draw() {
+  global_draw_start(false);
+
+  if(k+2 == total_loops) noLoop();
+  
   //actual drawing stuff
   push();
   translate(canvas_x / 8, canvas_y / 8);
 
   const row_step = canvas_y * 3/4 / num_rows;
   const col_step = canvas_x * 3/4 / num_cols;
-  for(let i=0; i<num_rows; i++){
-    for(let j=0; j<num_cols; j++){
-      push();
-      let x = j;
-      let y = i;
-      let amp = amp_noise;
-      const c_index = map(pnoise.simplex2(x / x_color_damp, y / y_color_damp), -1,1, 0,1);
-      const c_fill = color_map(c_index);
+  for(let i=k; i<k+1000; i++){
+    const actual_k = floor(k/noise_map.length);
+    let e = noise_map[i%noise_map.length];
+    e.x += amp * pnoise.simplex2(e.x / x_damp, e.y / y_damp);
+    e.y += amp * pnoise.simplex2(y_offset + e.x / x_damp, y_offset + e.y / y_damp);
 
-      for(let k=0; k<num_warps; k++){
-        x += amp * pnoise.simplex2(x / x_damp, y / y_damp);
-        y += amp * pnoise.simplex2(y_offset + x / x_damp, y_offset + y / y_damp);
+    e.c.setAlpha(lerp(100, 10, actual_k / num_warps));
 
-        c_fill.setAlpha(lerp(100, 10, k / num_warps));
+    fill(e.c);
 
-        fill(c_fill);
+    //values past this point are scaled
+    let size_x = lerp(col_step*4, 0, actual_k / num_warps);
+    let size_y = lerp(row_step*4, 0, actual_k / num_warps);
 
-        //values past this point are scaled
-        const size_x = lerp(col_step*4, 0, k / num_warps);
-        const size_y = lerp(row_step*4, 0, k / num_warps);
-        
-        ellipse(x * col_step, y * row_step, size_x, size_y);
-
-        amp *= falloff;
-      }
-      pop();
-    }
+    ellipse(e.x * col_step, e.y * row_step, size_x, size_y);
   }
+
+  k += 1000;
 
   pop();
   
