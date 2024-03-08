01486290415e06f57fcfe364a1802c5b09024f7f
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-08 17:00:23 -0600

    added single line mode as well as larger x_move and y_move options

--- plotter_projects/Billow/sketch.js
+++ plotter_projects/Billow/sketch.js
@@ -21,8 +21,9 @@ function gui_values(){
   parameterize("y_sin_amp", 200, 0, 400, 1, true);
   parameterize("sin_range", random(180), 0, 720, 1, false);
   parameterize("rotate_per_line", 0, -5, 5, 0.1, false);
-  parameterize("x_move", 0, -100, 100, 1, true);
-  parameterize("y_move", 0, -100, 100, 1, true);
+  parameterize("x_move", 0, -400, 400, 1, true);
+  parameterize("y_move", 0, -400, 400, 1, true);
+  parameterize("single_line", 0, 0, 1, 1, false);
 } 
 
 function setup() {
@@ -42,20 +43,46 @@ function draw() {
   const segment_step_size = canvas_x*2/line_segments;
   const line_step_size = canvas_y*2/number_of_lines;
   // curveTightness(1);
+  const lines = [];
   for(let j=0; j<number_of_lines; j++){
     rotate(rotate_per_line);
     // stroke(random(working_palette));
-    beginShape();
+    const pts = [];
     const y_base_loc = j*line_step_size;
     for(let i=0; i<line_segments; i++){
       const x_loc = i*segment_step_size;
       const y_loc = y_base_loc + y_sin_amp * sin(y_theta_offset + map(i/line_segments, 0,1, 0, sin_range));
       const x = x_loc + map(noise(x_loc/x_i_damp, y_loc/x_j_damp), 0,1, -x_amp, x_amp);
       const y = y_loc + map(noise(x_loc/y_i_damp, y_loc/y_j_damp), 0,1, -y_amp, y_amp);
-      curveVertex(x,y);
+      pts.push([x,y]);
     }
-    endShape();
+    lines.push(pts);
   }
+
+  let shape_began = false;
+  lines.forEach((l,index) => {
+    if(!shape_began || !single_line){
+      beginShape();
+      shape_began = true;
+    }
+
+    if(index%2==0){
+      for(let i=0; i<l.length; i++){
+        const pt = l[i];
+        curveVertex(pt[0], pt[1]);
+      }
+    }
+    else{
+      for(let i=l.length-1; i>=0; i--){
+        const pt = l[i];
+        curveVertex(pt[0], pt[1]);
+      }
+    }
+    if(!single_line) endShape();
+  });
+
+  if(single_line) endShape();
+
   pop();
   global_draw_end();
 }
