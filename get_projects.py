fcc8068d4ddcfdd73aa590c9c288ef11266f4163
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-19 16:27:20 -0500

    changed get projects to create a list of dicts containing paths to iframes and thumbnails

--- get_projects.py
+++ get_projects.py
@@ -1,15 +1,22 @@
-import os
+import os, json
 
 def find_folders_with_files(directory):
     folders_with_files = []
 
     def search_folders(current_path):
         files = os.listdir(current_path)
-
-        if 'index.html' in files and 'sketch.js' in files:
+        thumbnail = False
+        thumbnail_name = ""
+        for file in files:
+            if ".gif" in file or ".png" in file or ".mp4" in file:
+                thumbnail = True
+                thumbnail_name = file
+        if thumbnail and 'index.html' in files and 'sketch.js' in files:
             rel_path = os.path.relpath(current_path, directory)
-            path = os.path.join(rel_path, "index.html?controls=true")
-            folders_with_files.append(path)
+            iframe_path = os.path.join(rel_path, "index.html?controls=true")
+            thumbnail_path = os.path.join(rel_path, thumbnail_name)
+            folder_dict = {"iframe":iframe_path, "thumbnail": thumbnail_path}
+            folders_with_files.append(folder_dict)
 
         for file in files:
             file_path = os.path.join(current_path, file)
@@ -22,7 +29,7 @@ def find_folders_with_files(directory):
 def write_js_array(file_path, py_list):
     try:
         with open(file_path, 'w') as js_file:
-            js_file.write('const all_projects_rel_paths = ' + str(py_list) + ';')
+            js_file.write('const all_projects_rel_paths = ' + json.dumps(py_list) + ';')
         print(f"JavaScript file '{file_path}' has been successfully written.")
     except Exception as e:
         print(f"Error occurred while writing JavaScript file: {e}")
