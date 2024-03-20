105e36b4bb54691b09a336fd02d7a3604d90031f
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-20 09:44:59 -0500

    updated mass_index_change to use os.path.join at all times, added print statements

--- mass_index_change.py
+++ mass_index_change.py
@@ -3,8 +3,12 @@ import pathlib
 
 path = os.getcwd()
 
+print(path)
+
 #absolute paths
-common = path + r"\projects\Common"
+common = os.path.join(path, "projects", "Common")
+
+print(common)
 
 to_replace = []
 
@@ -21,31 +25,31 @@ for f in to_replace:
     rel_path = pathlib.Path(rel_path)
     
     file_contents = '''<!DOCTYPE html><html lang="en"><head>
-    <script src="'''+rel_path.as_posix()+'''/p5.min.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "p5.min.js") +'''"></script>
     <!-- Perlin and Simplex Noise functions -->
-    <script src="'''+rel_path.as_posix()+'''/perlin.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "perlin.js") +'''"></script>
     <!-- Custom scripts -->
-    <script src="'''+rel_path.as_posix()+'''/palettes.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "palettes.js") +'''"></script>
     <script src="sketch.js"></script>
-    <script src="'''+rel_path.as_posix()+'''/utils.js"></script>
-    <script src="'''+rel_path.as_posix()+'''/cutouts.js"></script>    
-    <script src="'''+rel_path.as_posix()+'''/shapes.js"></script>
-    <script src="'''+rel_path.as_posix()+'''/midi.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "utils.js") +'''"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "cutouts.js") +'''"></script>    
+    <script src="'''+os.path.join(rel_path.as_posix(), "shapes.js") +'''"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "midi.js")+'''"></script>
     <!-- Frame capture lib for gifs/vids -->
-    <script src="'''+rel_path.as_posix()+'''/CCapture.all.min.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "CCapture.all.min.js") +'''"></script>
     <!-- p5.gui lib -->
-    <script src="'''+rel_path.as_posix()+'''/p5.gui.js"></script>
-    <script src="'''+rel_path.as_posix()+'''/quicksettings.js"></script>   
-    <script src="'''+rel_path.as_posix()+'''/p5.svg.js"></script>   
+    <script src="'''+os.path.join(rel_path.as_posix(), "p5.gui.js") +'''"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "quicksettings.js") +'''"></script>   
+    <script src="'''+os.path.join(rel_path.as_posix(), "p5.svg.js") +'''"></script>   
     <!-- Pen Size Files for Plotting -->
-    <script src="'''+rel_path.as_posix()+'''/pen_utils.js"></script>
+    <script src="'''+os.path.join(rel_path.as_posix(), "pen_utils.js") +'''"></script>
     <!-- Color Picker lib -->
-    <link rel="stylesheet" href="'''+rel_path.as_posix()+'''/Alwan_Color_Picker/css/alwan.min.css">
-    <script src="'''+rel_path.as_posix()+'''/Alwan_Color_Picker/js/alwan.min.js"></script>
+    <link rel="stylesheet" href="'''+os.path.join(rel_path.as_posix(), "Alwan_Color_Picker","css" ,"alwan.min.css") +'''">
+    <script src="'''+os.path.join(rel_path.as_posix(), "Alwan_Color_Picker", "js", "alwan.min.js")+'''"></script>
     <!-- Open Type js library for svg handling of text -->
     <script src="https://cdn.jsdelivr.net/npm/opentype.js@latest/dist/opentype.min.js"></script>
     <!-- Global Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="'''+rel_path.as_posix()+'''/style.css">
+    <link rel="stylesheet" type="text/css" href="'''+os.path.join(rel_path.as_posix(), "style.css") +'''">
     <meta charset="utf-8">
 
     </head>
