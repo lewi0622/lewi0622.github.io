c8173f3eaf014b25bfb928ed0d2950a546619428
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-20 10:07:31 -0500

    updated and improved arced_swirl, added new png

--- plotter_projects/Arced_Swirl/sketch.js
+++ plotter_projects/Arced_Swirl/sketch.js
@@ -6,11 +6,14 @@ const fr = 1;
 const capture = false;
 const capture_time = 10;
 
-const suggested_palettes = []
+const suggested_palettes = [BUMBLEBEE];
 
 function gui_values(){
-  parameterize("num_circles", 100, 1, 500, 1, false);
-  parameterize("circle_steps", 100, 3, 300, 1, false);
+  parameterize("num_circles", random(30,300), 1, 500, 1, false);
+  parameterize("circle_steps", random(50,300), 3, 300, 1, false);
+  parameterize("radius_start", random(50), 0, 200, 1, true);
+  parameterize("circle_radius_inc", random(-5,5), -10, 10, 0.1, true);
+  parameterize("step_radius_inc", random(-2,2), -10, 10, 0.1, true);
 } 
 
 function setup() {
@@ -24,17 +27,22 @@ function draw() {
 
   //actual drawing stuff
   push();
+  const bg_c = bg(true);
+  if(type == "png") {
+    background(bg_c);
+    stroke(random(working_palette));
+  }
   noFill();
   const ang_step = 360/circle_steps;
   translate(canvas_x/2, canvas_y/2);
   for(let i=0; i<num_circles; i++){
     // rotate(map(noise(i/1000), 0,1, 0,360));
     beginShape();
-    let radius = (i/10 + 50)*global_scale;
+    let radius =  i * circle_radius_inc + radius_start;
     let actual_steps = random(circle_steps/2, circle_steps);
     for(let j=0; j<actual_steps; j++){
       const theta = j*ang_step;
-      radius += 0.1 + (i+j)/100;
+      radius += step_radius_inc;
       let x = radius*cos(theta);
       let y = radius*sin(theta);
 
