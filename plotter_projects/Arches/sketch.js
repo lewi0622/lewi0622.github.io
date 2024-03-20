0dbe42cc950d8cf9a73cda005271fa3a98e1ad20
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-20 11:06:22 -0500

    rewrote arches, added parameters, added new thumbnail

--- plotter_projects/Arches/sketch.js
+++ plotter_projects/Arches/sketch.js
@@ -6,15 +6,16 @@ const fr = 1;
 const capture = false;
 const capture_time = 10;
 
-const suggested_palettes = [COTTONCANDY, SIXTIES, SUPPERWARE]
+const suggested_palettes = [];
 
 
 function gui_values(){
-  parameterize("cols", 10, 1, 50, 1, false, grid_slider_1);
-  parameterize("rows", 20, 1, 50, 1, false, grid_slider_2);
-  parameterize("starting_arch_width", 100, 1, 1000, 1, true, grid_slider_3);
-  parameterize("starting_arch_height", 200, 1, 1000, 1, true, grid_slider_4);
-  parameterize("arch_step_size", random(5,20), 1, 100, 1, true, grid_dial_1);
+  parameterize("cols", round(random(5,20)), 1, 50, 1, false, grid_slider_1);
+  parameterize("rows", round(random(5,20)), 1, 50, 1, false, grid_slider_2);
+  parameterize("starting_arch_width", random(15, 100), 1, 1000, 1, true, grid_slider_3);
+  parameterize("starting_arch_height", random(1,100), 1, 1000, 1, true, grid_slider_4);
+  parameterize("num_arches", round(random(5, 20)), 1, 50, 1, false);
+  parameterize("filled_shapes", 1, 0, 1, 1, false);
 }
 
 function setup() {
@@ -24,37 +25,39 @@ function setup() {
 function draw() {
   global_draw_start();
 
+  refresh_working_palette();
+
   //actual drawing stuff
   push();
-  const colors = [color("RED"), color("BLUE")];
+  const bg_c = random(working_palette);
+  reduce_array(working_palette, bg_c);
+  if(type == "png") background(bg_c);
+  const c1 = random(working_palette);
+  reduce_array(working_palette, c1);
+  const c2 = random(working_palette);
 
   const x_step_size = canvas_x*1.5/cols;
   const y_step_size = canvas_y*1.5/rows;
+  translate(-starting_arch_width/2, starting_arch_height/2);
 
-  for(let i=0; i<=cols; i++){
-    for(let j=0; j<=rows; j++){
+  for(let j=0; j<rows; j++){
+    for(let i=0; i<cols; i+=2){ //evens
       push();
-      let arch_x = starting_arch_width;
-      let arch_y = starting_arch_height;
-
-      // arches in order of 0,2,1,4,3,6,5 so every other is on top
-      let actual_i;
-      if(i==0) actual_i = i;
-      else if(i%2 == 1) actual_i = i+1;
-      else if(i%2 == 0) actual_i = i-1;
-
-      //y offset for every other arch
-      let y_offset = 0;
-      if(i%2 == 0 && i != 0) y_offset = arch_y/2; //why is i=0 a special case for the yoffset????
-      translate(actual_i*x_step_size, j*y_step_size + y_offset);
-
-      while(arch_x>arch_step_size && arch_y>arch_step_size){
-        stroke(colors[i%colors.length]);
-        if(i==0) stroke(colors[1]);
-        arches(arch_x,arch_y);
-        arch_x -= arch_step_size;
-        arch_y -= arch_step_size;
-      } 
+      stroke(c1);
+      if(filled_shapes)fill(c2);
+      else noFill();
+      translate(i*x_step_size, j*y_step_size);
+      arch_column();
+      pop();
+    }
+    for(let i=1; i<=cols; i+=2){ //odds
+      push();
+      stroke(c2);
+      if(filled_shapes) fill(c1);
+      else noFill();
+      const y_offset = starting_arch_height/2;
+      translate(i*x_step_size, j*y_step_size + y_offset);
+      arch_column();
       pop();
     }
   }
@@ -65,6 +68,12 @@ function draw() {
 }
 //***************************************************
 //custom funcs
+function arch_column(){
+  for(let i=0; i<num_arches; i++){
+    arches(lerp(starting_arch_width, 0, i/num_arches), lerp(starting_arch_height, 0, i/num_arches));
+  }
+}
+
 function arches(arch_width, arch_height){
   beginShape();
 
