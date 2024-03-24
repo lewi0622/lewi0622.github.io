ae67730df0c75fda0cae4958862caa3d6ce09c3e
lewi0622 <eric.lewiston.el@gmail.com>
2024-03-23 21:53:55 -0500

    added colors and new thumbnail

--- plotter_projects/Concentric_Noise/sketch.js
+++ plotter_projects/Concentric_Noise/sketch.js
@@ -6,8 +6,10 @@ const fr = 1;
 const capture = false;
 const capture_time = 8;
 
+const suggested_palettes = [SUPPERWARE];
+
 function gui_values(){
-  parameterize("starting_radius", 100, 10, 4000, 1, true);
+  parameterize("starting_radius", random(smaller_dim*3/4, smaller_dim/4), 10, 1000, 1, true);
   parameterize("number_of_points", 100, 4, 1000, 1, false);
   parameterize("number_of_rings", 20, 1, 200, 1, false);
   parameterize("max_noise", 5, 0, 1000, 1, false);
@@ -27,6 +29,13 @@ function setup() {
 function draw() {
   global_draw_start();
 
+  noFill();
+  const bg_c = random(working_palette);
+  reduce_array(working_palette, bg_c);
+  if(type == "png") background(bg_c);
+  const c = color(random(working_palette));
+  c.setAlpha(150);
+  stroke(c);
   //actual drawing stuff
   push();
   translate(canvas_x/2, canvas_y/2);
@@ -34,7 +43,6 @@ function draw() {
   rotate(rotation);
   // strokeWeight(0.04*96);
   strokeWeight(COPICMARKER*1/2);
-  stroke(0,0,0,150);
   //strokejoin, strokecap, etc
   for(let i=0; i<number_of_rings; i++){
     beginShape();
