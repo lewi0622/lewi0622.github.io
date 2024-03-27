92acf7392502fe8ef811ccc08436d96d7fe8b26b
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-03-27 15:50:48 -0500

    added bettter params, made it scale nicely

--- projects/Letter_to_the_Editor/sketch.js
+++ projects/Letter_to_the_Editor/sketch.js
@@ -5,13 +5,21 @@ const animation = false;
 const fr = 1;
 const capture = false;
 const capture_time = 10;
-
 const suggested_palettes = [BIRDSOFPARADISE, SIXTIES, SUPPERWARE]
 
 function gui_values(){
-  parameterize("tile_div", random(70, 300), 1, 400, 1, false);
-  parameterize("amp_start", random(15, 20), 10, 50, 0.1, true);
+  parameterize("letter_step_size", random(1,5), 1, 10, 0.1, true);
+  parameterize("letter_amplitude", random(15,20), 1, base_y/4, 0.1, true);
   parameterize("tightness", random(4,5), -5, 5, 0.1, false);
+
+  if(random()>0.75){
+    parameterize("margin_x", -base_x/4, -base_x/4, base_x/4, 1, true);
+    parameterize("margin_y", -base_y/4, -base_y/4, base_y/4, 1, true);
+  }
+  else{
+    parameterize("margin_x", base_x/random(5,15), -base_x/4, base_x/4, 1, true);
+    parameterize("margin_y", 0, -base_y/4, base_y/4, 1, true);
+  }
 }
 
 function setup() {
@@ -34,30 +42,30 @@ function draw() {
   //actual drawing stuff
   push();
   noFill();
-  const num_lines = floor(canvas_y/amp_start/2)-2;
-  const tile_x = canvas_x/tile_div;
+  const num_lines = floor((canvas_y - margin_y*2)/letter_amplitude/2)-2;
+  const tile_x = letter_step_size;
   strokeWeight(1.25*global_scale);
-  const margin_y = random(30, 60)*global_scale;
-  stroke(random(working_palette));
+  const stroke_c = random(working_palette);
+  if(type == "png") stroke(stroke_c);
 
   //space between words
-  const space_size = amp_start/4;
+  const space_size = letter_amplitude/4;
 
-  //reset y position
-  let current_y = margin_y;
+  let current_y = letter_amplitude*2 + margin_y/2;
   let word_count = 0;
   for(let j=0; j<num_lines; j++){
     push();
-    const margin_x = 60*global_scale;
-
     //reset x position
     let current_x = margin_x;
     while(current_x<canvas_x-margin_x*2){
       push();
       translate(current_x, current_y);
       const word_len = random(5, 20);
-      if(j ==0) translate(0, -amp_start/2);
-      if(j == num_lines-1) translate(random(canvas_x-margin_x*2), amp_start);
+
+      //top line gets more space from body
+      if(j ==0) translate(0, -letter_amplitude/2);
+      //signature line gets random palcement;
+      if(j == num_lines-1) translate(random(canvas_x-margin_x*2), letter_amplitude);
 
       //average sentence is 15-20 words
       //maybe use 1-2 commas between punctuation
@@ -71,7 +79,7 @@ function draw() {
       let x_movement;
       for(let i = 0;  i<word_len; i++){
         const l_r = random([-1,1,1])*tile_x*3;
-        const u_d = random([-1,1])*random([amp_start*3/4, amp_start*3/4, amp_start]);
+        const u_d = random([-1,1])*random([letter_amplitude*3/4, letter_amplitude*3/4, letter_amplitude]);
         x_movement = i*tile_x+noise(i+j)*l_r;
         curveVertex(x_movement, noise(i+j)*u_d);
       }
@@ -87,7 +95,7 @@ function draw() {
       if((j == num_lines -2 && current_x>canvas_x-margin_x*2)  || word_count>8 && random()>map(word_count, 0,20, 1, 0.8)){
         push();
         //period
-        translate(current_x - space_size, current_y + amp_start/2);
+        translate(current_x - space_size, current_y + letter_amplitude/2);
         beginShape();
         curveVertex(0, 0);
         curveVertex(0, 0);
@@ -99,28 +107,28 @@ function draw() {
         if(random()>0.75){
           //exlcamation
           push();
-          translate(current_x - space_size, current_y + amp_start/2-4*global_scale);
+          translate(current_x - space_size, current_y + letter_amplitude/2-4*global_scale);
           beginShape();
           curveVertex(0,0);
           curveVertex(0,0);
-          curveVertex(random(0,4*global_scale), random(0.5, 1)*(-amp_start));
-          curveVertex(4*global_scale, -amp_start);
+          curveVertex(random(0,4*global_scale), random(0.5, 1)*(-letter_amplitude));
+          curveVertex(4*global_scale, -letter_amplitude);
           endShape();
           pop();
         }
         else if(random()>0.75){
           //question mark
           push();
-          translate(current_x - space_size, current_y + amp_start/2-4*global_scale);
+          translate(current_x - space_size, current_y + letter_amplitude/2-4*global_scale);
           curveTightness(random(-1, 0.5));
           rotate(random(-5,5));
           beginShape();
           curveVertex(0,0);
           curveVertex(0,0);
-          curveVertex(0, random(0.9,1.1)*(-amp_start/3));
-          curveVertex(random(0.9,1.1)*6*global_scale, -amp_start*3/4);
-          curveVertex(0, random(0.9,1.1)*(-amp_start));
-          curveVertex(0, random(0.9,1.1)*(-amp_start/2));
+          curveVertex(0, random(0.9,1.1)*(-letter_amplitude/3));
+          curveVertex(random(0.9,1.1)*6*global_scale, -letter_amplitude*3/4);
+          curveVertex(0, random(0.9,1.1)*(-letter_amplitude));
+          curveVertex(0, random(0.9,1.1)*(-letter_amplitude/2));
           endShape();
           pop();
         }
@@ -132,19 +140,19 @@ function draw() {
         //underline
         push();
         curveTightness(0);
-        translate(current_x - x_movement + space_size, current_y + amp_start*3/4);
+        translate(current_x - x_movement + space_size, current_y + letter_amplitude*3/4);
         beginShape();
-        const y_start = random(-1,1)*amp_start/4;
+        const y_start = random(-1,1)*letter_amplitude/4;
         curveVertex(0,y_start);
         curveVertex(0,y_start);
         for(let i=0; i<5; i++){
-          curveVertex(x_movement*i/5, random(-1,1)*amp_start/16 + y_start)
+          curveVertex(x_movement*i/5, random(-1,1)*letter_amplitude/16 + y_start)
         }
         endShape();
         pop();
       }
     }
-    current_y += amp_start*2;
+    current_y += letter_amplitude*2;
     pop();
   }
   //grain
