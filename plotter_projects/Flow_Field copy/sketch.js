0f50ed8aaaf0193a7fe0552dc0efe6c09921ff3f
lewi0622 <eric.lewiston.el@gmail.com>
2024-04-01 22:39:55 -0500

    added rows and columns, tweaked params

--- plotter_projects/Flow_Field copy/sketch.js
+++ plotter_projects/Flow_Field copy/sketch.js
@@ -6,16 +6,21 @@ const fr = 30;
 const capture = false;
 const capture_time = 10;
 
-const suggested_palettes = [];
-
+const suggested_palettes = [SUMMERTIME];
 
 function gui_values(){
-  parameterize("iterations", 50, 1, 500, 1, false);
+  const col_division = random(4,10);
+  const row_division = random([col_division/2, col_division, col_division, col_division]);
+  parameterize("cols", floor(base_x/col_division), 1, base_x, 1, false);
+  parameterize("rows", floor(base_y/row_division), 1, base_y, 1, false);
+  parameterize("x_margin", -base_x/16, -base_x/2, base_x/2, 1, true);
+  parameterize("y_margin", -base_y/16, -base_y/2, base_y/2, 1, true);
+  parameterize("iterations", floor(random(10,100)), 1, 500, 1, false);
   parameterize("step_size", 5, 1, 50, 1, true);
-  parameterize("num_lines", 50, 1, 5000, 1, false);
-  parameterize("x_damp", 1, 1, 1000, 1, false);
-  parameterize("y_damp", 1, 1, 1000, 1, false);
-  parameterize("i_damp", 1, 1, 1000, 1, false);
+  const damp = random(200, 2000);
+  parameterize("x_damp", damp, 1, 10000, 1, false);
+  parameterize("y_damp", damp, 1, 10000, 1, false);
+  parameterize("i_damp", 1000, 1, 10000, 1, false);
   parameterize("num_colors", round(random(1, working_palette.length-1)), 1, working_palette.length-1, 1, false);
 }
 
@@ -29,27 +34,40 @@ function draw() {
 
   push();
   noFill();
+  strokeWeight(1*global_scale);
   png_bg(true);
   const colors = [];
   for(let i=0; i<num_colors; i++){
     colors.push(color(working_palette[i]));
+    colors[i].setAlpha(200);
   }
-  for(let j=0; j<num_lines; j++){
-    stroke(random(colors));
-    const pts = [{x:random(-canvas_x/2, canvas_x*1.5), y:random(-canvas_y/2, canvas_y*1.5)}];
-    beginShape();
-    for(let i=0; i<iterations; i++){
-      const starting_pt = pts[i];
-  
-      curveVertex(starting_pt.x, starting_pt.y);
-  
-      let angle = noise(starting_pt.x/global_scale/x_damp, starting_pt.y/global_scale/y_damp, i/i_damp) * 360*5;
-      pts.push({
-        x: starting_pt.x + step_size * cos(angle),
-        y: starting_pt.y + step_size * sin(angle)
-      });
+  const col_step = (canvas_x-x_margin*2)/cols;
+  const row_step = (canvas_y-y_margin*2)/rows;
+  translate(x_margin, y_margin);
+  for(let k=0; k<cols; k++){
+    for(let l=0; l<rows; l++){
+      const stroke_c = random(colors);
+      stroke(stroke_c);
+      if(type == "png"){
+        line_blur(stroke_c, 2*global_scale);
+      }
+      const pts = [{
+        x:k*col_step, 
+        y:l*row_step}];
+      beginShape();
+      for(let i=0; i<iterations; i++){
+        const starting_pt = pts[i];
+    
+        curveVertex(starting_pt.x, starting_pt.y);
+    
+        let angle = noise(starting_pt.x/global_scale/x_damp, starting_pt.y/global_scale/y_damp, i/i_damp) * 360*5;
+        pts.push({
+          x: starting_pt.x + step_size * cos(angle),
+          y: starting_pt.y + step_size * sin(angle)
+        });
+      }
+      endShape();
     }
-    endShape();
   }
 
   pop();
