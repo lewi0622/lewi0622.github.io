f1f5d06afc5c20219e976ebbb3a2d1b01a37bd6a
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-04-08 10:22:59 -0500

    added back and front versions of business card desgins, and for both detailed and generic versions. added gridded svgs as well

--- plotter_projects/Business_Cards/Detail_Back/sketch.js
+++ plotter_projects/Business_Cards/Detail_Back/sketch.js
@@ -10,11 +10,13 @@ let font;
 
 function gui_values(){
   parameterize("guide_lines", 0, 0, 1, 1, false);
-  parameterize("text_size", 96, 1, 500, 1, true);
-  parameterize("x_loc", 16, -base_x, base_x, 1, true);
-  parameterize("y_loc", 112, -base_y, base_y, 1, true);
-  parameterize("face_x_loc", 228, -base_x, base_x, 1, true);
-  parameterize("face_y_loc", -49, -base_y, base_y, 1, true);
+  parameterize("text_size", 42, 1, 500, 1, true);
+  parameterize("x_loc", 13, -base_x, base_x, 1, true);
+  parameterize("y_loc", 43, -base_y, base_y, 1, true);
+  parameterize("x2_loc", 79, -base_x, base_x, 1, true);
+  parameterize("y2_loc", 64, -base_y, base_y, 1, true);
+  parameterize("x3_loc", -17, -base_x, base_x, 1, true);
+  parameterize("y3_loc", 65, -base_y, base_y, 1, true);
 }
 
 function setup() {
@@ -37,29 +39,24 @@ function draw() {
   if(!font) return;
   global_draw_start();
   push();
-
   if(guide_lines){
     rect(0,0,canvas_x, canvas_y);
     line(canvas_x/2, 0, canvas_x/2, canvas_y);
     line(0, canvas_y/2, canvas_x, canvas_y/2);
+    line(0, canvas_y/4, canvas_x, canvas_y/4);
+    line(0, canvas_y*3/4, canvas_x, canvas_y*3/4);
   }
 
   noFill();
+  const line1 = font.getPath("Northrup King Bldg. #406", 0,0, text_size);
+  const line2 = font.getPath("@LewistonFace", 0,0, text_size);
+  const line3 = font.getPath("LewistonFace.com", 0,0, text_size);
   translate(x_loc, y_loc);
-
-  const lewiston_path = font.getPath("Lewiston", 0,0, text_size);
-  const face_path = font.getPath(":D", 0,0, text_size);
-  draw_open_type_js_path_p5_commands(lewiston_path);
-  translate(face_x_loc, face_y_loc);
-  rotate(80);
-  draw_open_type_js_path_p5_commands(face_path);
-  // textSize(text_size);
-  // translate(0*global_scale, canvas_y/2)
-  // text("Lewiston", 0,0);
-  // translate(text_size*2.4, -text_size/2);
-  // rotate(80);
-  // text(":D", 0, 0)
-
+  draw_open_type_js_path_p5_commands(line1);
+  translate(x2_loc, y2_loc);
+  draw_open_type_js_path_p5_commands(line2);
+  translate(x3_loc, y3_loc);
+  draw_open_type_js_path_p5_commands(line3);
   pop();
   global_draw_end();
 }
--- plotter_projects/Business_Cards/Detail_Front/sketch.js
+++ plotter_projects/Business_Cards/Detail_Front/sketch.js
@@ -10,11 +10,14 @@ let font;
 
 function gui_values(){
   parameterize("guide_lines", 0, 0, 1, 1, false);
-  parameterize("text_size", 96, 1, 500, 1, true);
-  parameterize("x_loc", 16, -base_x, base_x, 1, true);
-  parameterize("y_loc", 112, -base_y, base_y, 1, true);
+  parameterize("logo_text_size", 96, 1, 500, 1, true);
+  parameterize("sub_text_size", 51, 1, 500, 1, true);
+  parameterize("logo_x_loc", 16, -base_x, base_x, 1, true);
+  parameterize("logo_y_loc", 76, -base_y, base_y, 1, true);
   parameterize("face_x_loc", 228, -base_x, base_x, 1, true);
   parameterize("face_y_loc", -49, -base_y, base_y, 1, true);
+  parameterize("sub_x_loc", 13, -base_x, base_x, 1, true);
+  parameterize("sub_y_loc", 152, -base_y, base_y, 1, true);
 }
 
 function setup() {
@@ -45,20 +48,20 @@ function draw() {
   }
 
   noFill();
-  translate(x_loc, y_loc);
 
-  const lewiston_path = font.getPath("Lewiston", 0,0, text_size);
-  const face_path = font.getPath(":D", 0,0, text_size);
+  const lewiston_path = font.getPath("Lewiston", 0,0, logo_text_size);
+  const face_path = font.getPath(":D", 0,0, logo_text_size);
+  const sub_path = font.getPath("Digital, Pen-Plotted Art", 0,0, sub_text_size);
+  push();
+  translate(logo_x_loc, logo_y_loc);
   draw_open_type_js_path_p5_commands(lewiston_path);
   translate(face_x_loc, face_y_loc);
   rotate(80);
   draw_open_type_js_path_p5_commands(face_path);
-  // textSize(text_size);
-  // translate(0*global_scale, canvas_y/2)
-  // text("Lewiston", 0,0);
-  // translate(text_size*2.4, -text_size/2);
-  // rotate(80);
-  // text(":D", 0, 0)
+  pop();
+
+  translate(sub_x_loc, sub_y_loc);
+  draw_open_type_js_path_p5_commands(sub_path);
 
   pop();
   global_draw_end();
