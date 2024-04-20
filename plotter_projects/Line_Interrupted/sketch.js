c81f83c3b4302d23125572b0abb08f7e5fd59b30
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-04-20 14:42:31 -0500

    added colors, need to tweak params tho

--- plotter_projects/Line_Interrupted/sketch.js
+++ plotter_projects/Line_Interrupted/sketch.js
@@ -7,15 +7,17 @@ const fr = 1;
 const capture = false;
 const capture_time = 10;
 
+const suggested_palettes = [];
 
 function gui_values(){
   parameterize("step_size", 5, 1, 20, 1, true);
-  parameterize("line_spacing", 10, 1, 50, 1, true);
-  parameterize("xnoise_div", 100, 10, 1000, 10,false);
+  parameterize("line_spacing", 2, 0.01, 10, 0.01, true);
+  parameterize("xnoise_div", 100, 1, 1000, 10,false);
   parameterize("xnoise_off", 0, -50, 50, 1, false);
-  parameterize("ynoise_div", 100, 10, 1000, 10,false);
+  parameterize("ynoise_div", 100, 1, 1000, 10,false);
   parameterize("ynoise_off", 0, -50, 50, 1, false);
   parameterize("noise_size", 3, 0.1, 500, 1, true);
+  parameterize("num_colors", round(random(1, working_palette.length-1)), 1, working_palette.length-1, 1, false);
 }
 
 function setup() {
@@ -28,47 +30,36 @@ function draw() {
 
   //actual drawing stuff
   push();
-  noFill();
-  const lines = [];
-  let line_pos = 0;
-  let line_max = canvas_x;
-  //line_background'
-  let line_skips = 0;
-  let skip = false;
-  let line_skip_chance = 0.01;
-  while(line_pos<line_max){
-    if(line_skips <= 0){
-      line_skips = floor(random(10,20));
-      skip = false;
-    }
-    const line_points = [];
-    for(let i=0; i<canvas_y; i+=step_size){
-      line_points.push([map(noise((i + xnoise_off*i)/xnoise_div, (line_pos+ynoise_off)/ynoise_div), 0,1, -noise_size, noise_size), i]);
-    }
-    line_points.push([map(noise((canvas_y + xnoise_off*canvas_y)/xnoise_div, (line_pos+ynoise_off)/ynoise_div), 0,1, -noise_size, noise_size), canvas_y]);
-    if(random()>(line_skip_chance) && !skip) lines.push(line_points);
-    else {
-      lines.push([])
-      line_skips--;
-      skip = true;
-    }
-    line_pos += line_spacing;
+  strokeWeight(0.5*global_scale);
+  working_palette = controlled_shuffle(working_palette, true);
+  png_bg(true);
+  const colors = [];
+  for(let i=0; i<num_colors; i++){
+    colors.push(color(working_palette[i]));
+    colors[i].setAlpha(200);
   }
-  let colors = [color(COPIC_R08), color(COPIC_B06)];
-  colors.forEach(c => {
-    c.setAlpha(150);
-  });
+  noFill();
 
-  for(let i=0; i<1; i++){
-    center_rotate(i*90);
-    stroke(colors[i]);
+  for(let j=0; j<num_colors; j++){
+    const lines = [];
+    let line_pos = 0;
+    let line_max = canvas_x;
+    while(line_pos<line_max){
+      const line_points = [];
+      for(let i=0; i<canvas_y; i+=step_size){
+        line_points.push([map(noise((i + xnoise_off*i)/xnoise_div, (line_pos+ynoise_off)/ynoise_div, j), 0,1, -noise_size, noise_size), i]);
+      }
+      line_points.push([map(noise((canvas_y + xnoise_off*canvas_y)/xnoise_div, (line_pos+ynoise_off)/ynoise_div, j), 0,1, -noise_size, noise_size), canvas_y]);
+      lines.push(line_points);
+      line_pos += line_spacing;
+    }
+    stroke(colors[j]);
     lines.forEach((line_points, idx) => {
       push();
       translate(idx*line_spacing, 0);
 
       beginShape();
       line_points.forEach(points => vertex(...points));
-
       endShape();
       pop();
     });
