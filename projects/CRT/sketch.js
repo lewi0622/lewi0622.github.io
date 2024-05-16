dd0e639ee5bd03342821b63cc65c947ababb291f
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-05-16 11:16:55 -0500

    added CRT

---
+++ projects/CRT/sketch.js
@@ -0,0 +1,58 @@
+'use strict';
+
+//setup variables
+const gif = true;
+const animation = true;
+const fr = 30;
+const capture = false;
+const capture_time = 10;
+
+const suggested_palettes = [];
+
+let z;
+let z_inc = 1;
+let ang;
+
+function gui_values(){
+  parameterize("iterations", 10, 1, 50, 1, false);
+  // parameterize("rot_per_iteration", random(-0.5, 0.6), -180, 180, 0.1, false);
+  parameterize("wobble", random(100), 0, 100, 0.1, true);
+  parameterize("margin_x", -base_x/2, -base_x/2, base_x/2, 1, true);
+  parameterize("margin_y", -base_y/2, -base_y/2, base_y/2, 1, true);
+}
+
+function setup() {
+  common_setup();
+  gui_values();
+  z = random(360);
+  ang = random(360);
+}
+//***************************************************
+function draw() {
+  global_draw_start();
+
+  //actual drawing stuff
+  push();
+  background("WHITE");
+  
+  const num_lines = floor(map(sin(z), -1,1, 300, 500));
+  if(frameCount%30 == 0) ang = random(360);
+  const rot_per_iteration = sin(z) * ang;
+  strokeWeight(0.5*global_scale);
+  for(let j=0; j<iterations; j++){
+    push();
+    center_rotate(j*rot_per_iteration);
+    translate(random(wobble), random(wobble));
+    for(let i=0; i<num_lines; i++){
+      const x = lerp(margin_x, canvas_x-margin_x, i/num_lines);
+      line(x, margin_y, x, canvas_y-margin_y);
+    }
+    pop();
+  }
+  z += z_inc;
+  pop();
+
+  global_draw_end();
+}
+//***************************************************
+//custom funcs
