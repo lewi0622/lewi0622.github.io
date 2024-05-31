90bcaec48566d963d01bf526b820bf303d2f20b3
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-05-31 16:58:53 -0500

    added micron005 and signo broad nib sizes

--- projects/Common/pen_utils.js
+++ projects/Common/pen_utils.js
@@ -5,11 +5,13 @@
 //Sakura Pigma Micron 05, 0.45mm, 0.01771654inches,
 //for fill, use this value/2 distance between lines
 const MICRON05 = 0.01771654*96;
+const MICRON005 = 0.00787402*96;
 //BIC Cristal 1.6mm, 0.0629921, does not fill at this weight
 const BICCRISTAL = 0.0629921*96/2;
 // const BICCRISTAL = 0.015*96;
 const STABILO88 = 0.015748*96;
 
+const SIGNOBROAD = 0.0393701*96;
 // approx copic SuperBrush line size.
 //for fill use this value/2 distance between lines
 const COPICMARKER = 0.1*96;
