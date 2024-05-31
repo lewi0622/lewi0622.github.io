8963c2333003151f351866ed6b40a9342fdfdd27
Eric Lewiston <eric.lewiston.el@gmail.com>
2024-05-31 17:01:42 -0500

    added eyes and further params

--- plotter_projects/chUck/sketch.js
+++ plotter_projects/chUck/sketch.js
@@ -9,21 +9,24 @@ const capture_time = 10;
 const suggested_palettes = [BIRDSOFPARADISE, SUMMERTIME];
 
 function gui_values(){
-  const division = random(6,16);
+  const division = random(8, 16);
   const col_division = random([division*2, division, division, division]);
   const row_division = random([division*2, division, division, division]);
   parameterize("cols", floor(base_x/col_division), 1, base_x, 1, false);
   parameterize("rows", floor(base_y/row_division), 1, base_y, 1, false);
-  parameterize("hair_width", smaller_base/100, 1, smaller_base/4, 1, true);
+  parameterize("hair_width", smaller_base/random(60,100), 1, smaller_base/4, 1, true);
   parameterize("x_margin", -base_x/2, -base_x/2, base_x/2, 1, true);
   parameterize("y_margin", -base_y/2, -base_y/2, base_y/2, 1, true);
-  parameterize("iterations", floor(random(15,50)), 1, 500, 1, false);
-  parameterize("step_size", random(1,10), 1, 50, 1, true);
-  const damp = random(100, 500);
-  parameterize("x_damp", damp, 1, 1000, 1, false);
-  parameterize("y_damp", damp, 1, 1000, 1, false);
+  parameterize("iterations", floor(random(10,25)), 1, 500, 1, false);
+  parameterize("step_size", random(1,5), 1, 50, 1, true);
+  const damp = random(700,1000);
+  parameterize("x_damp", damp, 1, 1500, 1, false);
+  parameterize("y_damp", damp, 1, 1500, 1, false);
   parameterize("num_colors", round(random(1, working_palette.length-1)), 1, working_palette.length-1, 1, false);
   parameterize("simplex", round(random()), 0, 1, 1, false);
+  parameterize("eye_size", smaller_base/3, 1, larger_base, 1, true);
+  parameterize("eye_x_displacement", 0, -smaller_base/2, smaller_base/2, 1, true);
+  parameterize("eye_y_displacement", 0, -smaller_base/2, smaller_base/2, 1, true);
 }
 
 function setup() {
@@ -35,10 +38,11 @@ function draw() {
   global_draw_start();
 
   push();
-  center_rotate(random([0,90,180,270]));
+  center_rotate(-90);
   noFill();
   strokeWeight(1*global_scale);
   const bg_c = png_bg(true);
+  // background("WHITE")
   const colors = [];
   for(let i=0; i<num_colors; i++){
     colors.push(color(working_palette[i]));
@@ -86,8 +90,23 @@ function draw() {
   }
 
   fill(bg_c);
+  const eye_iteration = round(random(0.9,1.1) * lines.length/2);
+  const pupil_pt = {
+    x: random(-0.1, 0.1)*eye_size,
+    y: random(-0.1, 0.1)*eye_size,
+    size: random(eye_size/3, eye_size/2)};
   for(let j=0; j<lines.length; j++){
     const all_pts = lines[j];
+    if(j == eye_iteration || j == eye_iteration + 5){
+      push();
+      translate(all_pts[0].x*0.95 + eye_x_displacement, all_pts[0].y*0.95 + eye_y_displacement);
+      noStroke();
+      stroke("BLACK");
+      circle(0,0, eye_size);
+      fill("BLACK");
+      circle(pupil_pt.x, pupil_pt.y, pupil_pt.size);
+      pop();
+    }
     let average_angle = 0;
     for(let i=0; i<all_pts.length; i++){
       if(isNaN(all_pts[i].ang)) continue;
@@ -111,18 +130,27 @@ function draw() {
     const offset_y = hair_width * sin(average_angle + 90);
     const offset_vector = createVector(offset_x, offset_y);
 
+    let lerp_amt = 0;
     //reverse array
     for(let i=all_pts.length-1; i>=0; i--){
       const pt = all_pts[i];
       //create vector for each point and create vector combined with offset
       const pt_vector = createVector(pt.x, pt.y);
       const offset_pt = p5.Vector.add(offset_vector, pt_vector);
-      pt_vector.lerp(offset_pt, i/all_pts.length);
+      if(i/all_pts.length>0.5){
+        lerp_amt = constrain(map(i/all_pts.length, 1,0.5, lerp_amt, 1), 0, 1);
+      }
+      else{
+        // lerp_amt = constrain(map(i/all_pts.length, 0., 1,lerp_amt), 0, 1);
+        lerp_amt = constrain(i/all_pts.length * map(noise(i/100), 0,1, 1.8, 2.2), 0, 1);
+      }
+
+      pt_vector.lerp(offset_pt, lerp_amt);
       vertex(pt_vector.x, pt_vector.y);
     }
 
 
-    endShape();
+    endShape(CLOSE);
   }
 
 
