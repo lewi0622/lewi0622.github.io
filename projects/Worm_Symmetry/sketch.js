f55abcf65e9ad647fd93a5871682bf32dc39fcce
lewi0622 <eric.lewiston.el@gmail.com>
2024-11-09 19:33:45 -0600

    reorganized and applied params

--- projects/Worm_Symmetry/sketch.js
+++ projects/Worm_Symmetry/sketch.js
@@ -9,61 +9,50 @@ const capture_time = 10;
 const suggested_palettes = []
 
 //project variables
-const noise_off = 20;
-const inc = 0.1*60/fr;
-let xoff, sym_angs, line_segs, line_color;
-
+const inc = 0.15*60/fr;
+let xoff;
 
 function gui_values(){
-
+  parameterize("sym_angs", floor(random(6,49)), 1, 100, 1, false);
+  parameterize("line_segs", floor(random(20,51)), 5, 100, 1, false);
+  parameterize("dampening", random(30,80), 1, 500, 1, false);
+  parameterize("noise_off", random(-20,20), -100, 100, 1, false);
+  parameterize("weight", random(1,3), 1, 10, 0.1, true);
 }
 
 function setup() {
   common_setup();
   gui_values();
-  
-  xoff = 0;
-  sym_angs = floor(random(6,49));
+  dampening = 50;
+  xoff = 0
   line_segs = floor(random(20,51));
-  line_color = color(random(palette));
+  const line_color = color(random(palette));
   //shadow/glow
   drawingContext.shadowBlur=3*global_scale;
   drawingContext.shadowColor = line_color;
   stroke(line_color);
+  noFill();
 }
 //***************************************************
 function draw() {
   global_draw_start();
-
+  strokeWeight(weight);
   background("BLACK");
+ 
+  translate(canvas_x/2, canvas_y/2);
 
-
-  //actual drawing stuff
-  push();
   for(let i=0; i<sym_angs; i++){
-    push();
-      translate(canvas_x/2, canvas_y/2);
-      noFill();
-      strokeWeight(1.5*global_scale);
-
       beginShape();
       for(let j=0; j<line_segs; j++){
-        const dampening = 50;
         const x = floor(map(noise((j + xoff)/dampening), 0, 1, -canvas_x*.75, canvas_x*.75));
         const y = floor(map(noise((j + xoff + noise_off)/dampening), 0,1, -canvas_y*.75, canvas_y*.75));
-        if(j == 0){
-          curveVertex(x, y);
-        }
         curveVertex(x, y);
       }
       endShape();
-    pop();
-    center_rotate(360/sym_angs);
+    rotate(360/sym_angs);
   }
 
   xoff+= inc;
-
-  pop();
   
   global_draw_end();
 }
