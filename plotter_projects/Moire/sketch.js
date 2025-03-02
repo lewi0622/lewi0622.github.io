28f665f5fdfe72ceaa95e765b0f17a1efbdd855a
Eric Lewiston <eric.lewiston.el@gmail.com>
2025-03-01 18:02:06 -0600

    added minnesota cutout

--- plotter_projects/Moire/sketch.js
+++ plotter_projects/Moire/sketch.js
@@ -9,6 +9,10 @@ const capture_time = 10;
 const suggested_palettes = [COTTONCANDY, BUMBLEBEE, SUMMERTIME];
 
 let porcine;
+let minnesota;
+function preload(){
+  minnesota = loadJSON("..\\..\\projects\\WIP\\Minnesota\\minnesota.json");
+}
 
 function gui_values(){
   parameterize("num_shapes", floor(random(2,5)), 1, 10, 1, false);
@@ -18,6 +22,7 @@ function gui_values(){
   // parameterize("font_size", 100, 0, smaller_base*2, 1, true);
   parameterize("num_cols", 10, 1, 100, 1, false);
   parameterize("num_rows", 10, 1, 100, 1, false);
+  parameterize("grid_margin", 10, 0, 100, 1, true);
 } 
 
 function setup() {
@@ -44,13 +49,13 @@ function draw() {
   push();
   refresh_working_palette();
   noFill();
-  strokeWeight(0.5*global_scale);
+  strokeWeight(2*global_scale);
   working_palette = controlled_shuffle(working_palette, true);
   const bg_c = png_bg(true);
   const colors = [];
   for(let i=0; i<num_colors; i++){
     colors.push(color(working_palette[i]));
-    // colors[i].setAlpha(200);
+    colors[i].setAlpha(200);
   }
   
   translate(canvas_x/2, canvas_y/2);
@@ -61,32 +66,39 @@ function draw() {
     translate(random(-1,1)*max_rad/8, random(-1,1)*max_rad/8);
     for(let j=0; j<circles_per_shape; j++){
       const rad = lerp(0, max_rad, j/circles_per_shape);
+      rotate(random(360));
       circle(0,0, rad);
     }
     pop();
   }
   pop();
   push();
-  //rectangular grid with certain boxes filled and others see thru. If SVG each see thru rectangle should have a different stroke line to make separation easy
-  // const col_step = canvas_x/num_cols;
-  // const row_step = canvas_y/num_rows;
-  // let ctr = 10;
-  // for(let i=0; i<num_cols; i++){
-  //   for(let j=0; j<num_rows; j++){
-  //     push();
-  //     translate(i*col_step, j*row_step);
-  //     stroke(ctr);
-  //     noFill();
-  //     if(random()>0.8){
-  //       fill(bg_c);
-  //       stroke(bg_c);
-  //     }
-  //     rect(0,0,col_step,row_step);
-  //     // ctr++;
-  //     pop();
-  //   }
-  // }
+  //MINNESOTA CUTOUT
+  // draw_minnesota(); 
 
+  //GRID CUTOUTS
+  // const grid_layers = 2;
+  // let ctr = 10;
+  // for(let k=0; k<grid_layers; k++){
+  //   center_rotate(45);
+  //   push();
+  //   translate(-canvas_x/2, -canvas_y/2);
+  //   const col_step = canvas_x*2/num_cols;
+  //   const row_step = canvas_y*2/num_rows;
+
+  //   for(let i=0; i<num_cols; i++){
+  //     for(let j=0; j<num_rows; j++){
+  //       push();
+  //       translate(i*(col_step + grid_margin), j*(row_step + grid_margin));
+  //       stroke(ctr);
+  //       noFill();
+  //       rect(0,0,col_step,row_step);
+  //       pop();
+  //     }
+  //   }
+  //   ctr++;
+  //   pop();
+  // }
 
   pop();
 
@@ -104,3 +116,35 @@ function draw() {
 }
 //***************************************************
 //custom funcs
+function draw_minnesota(){
+  push();
+  stroke("BLACK");
+  noFill();
+  let coords = minnesota.geometry.coordinates[0];
+
+  let min_lon, max_lon, min_lat, max_lat;
+  min_lon = 181;
+  max_lon = -181;
+  min_lat = 91;
+  max_lat = -91;
+
+  for(let i=0; i<coords.length; i++){
+    if(coords[i][0]<min_lon) min_lon = coords[i][0];
+    if(coords[i][0]>max_lon) max_lon = coords[i][0];
+    if(coords[i][1]<min_lat) min_lat = coords[i][1];
+    if(coords[i][1]>max_lat) max_lat = coords[i][1];
+  }
+
+  let padding = 20*global_scale;
+  beginShape();
+  for (let i = 0; i < coords.length; i++) {
+    let lon = coords[i][0];
+    let lat = coords[i][1];
+    let x = map(lon, min_lon, max_lon, 0+padding, canvas_x-padding)-padding/2;
+    let y = map(lat, min_lat, max_lat, canvas_y-padding, 0+padding);
+
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+  pop();
+}
