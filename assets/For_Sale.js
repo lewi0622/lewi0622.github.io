40fd05c0428fcc1462151e4f2712adaa03db110b
Eric Lewiston <eric.lewiston.el@gmail.com>
2025-06-29 17:27:51 -0500

    added for sale js file and accompanying script in index for parsing it

---
+++ assets/For_Sale.js
@@ -0,0 +1,30 @@
+const for_sale = [
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/271-scaled.jpg",
+        "url": "https://nemaa.org/shop/ace-of-diamonds-2/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/349-scaled.jpg",
+        "url": "https://nemaa.org/shop/a-disorganized-farmer/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/292-scaled.jpg",
+        "url": "https://nemaa.org/shop/dipped-flow-2/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/284-scaled.jpg",
+        "url": "https://nemaa.org/shop/eddy/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/297-scaled.jpg",
+        "url": "hhttps://nemaa.org/shop/flow-1/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/285-scaled.jpg",
+        "url": "https://nemaa.org/shop/flow-2/"
+    },
+    {
+        "photo_url": "https://nemaa.org/wp-content/uploads/272-scaled.jpg",
+        "url": "https://nemaa.org/shop/landscapes/"
+    }
+]
