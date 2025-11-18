8c5e8705f6072a83a36d507ca8c9227bc5c13b2e
Eric Lewiston <eric.lewiston.el@gmail.com>
2025-11-17 22:24:07 -0600

    added hand overhead and hand on hip

--- plotter_projects/Folks/sketch.js
+++ plotter_projects/Folks/sketch.js
@@ -19,7 +19,7 @@ function gui_values(){
   parameterize("cols", 5, 1, 100, 1, false);
   parameterize("rows", 5, 1, 100, 1, false);
   parameterize("limb_thickness", 10, 1, 50, 1, false);
-  parameterize("limb_mult", 0.005, 0.001, 0.5, 0.001, false);
+  parameterize("limb_mult", 0.025, 0.001, 0.5, 0.001, false);
 } 
 
 function setup() {
@@ -44,13 +44,10 @@ function draw() {
 
   translate(col_step/2, row_step/2);
 
-
   for(let i=0; i<cols; i++){
     for(let j=0; j<rows; j++){
       push();
       translate(i * col_step, j* row_step); //move to grid upper left corner
-
-
       translate(col_step/2, row_step/2); //move to center of grid
       rotate(random(-10,10));
       translate(-col_step/2, -row_step/2);//move back to upper left corner
@@ -74,24 +71,6 @@ function folk(w,h, weight){
   dress(w,h, weight);
   head(w,h,weight);
 }
-
-function head(w,h, interval){
-  let head_w = random(0.7, 0.9)*w/8;
-  let head_h = random(1,1.5)*head_w;
-  let head_size = min(head_w, head_h);
-  translate(head_size*random(-0.5,0.5), head_size*random(-0.25, 0.25));//random offset
-  translate(0, -h*0.4); //normal head position
-  rotate(random(-10,10));
-  while(head_size>0){
-    push();
-    ellipse(0,0, head_w, head_h);
-    head_w -= interval;
-    head_h -= interval;
-    head_size -= interval;
-    pop();
-  }
-}
-
 
 function dress(w, h, weight){
   //consider extracting individual bezier curves out from the dress fill for detail work on top
@@ -152,95 +131,88 @@ function dress(w, h, weight){
 }
 
 function arms(w, h, num_lines){
-//arms and legs should use beizer curves with slightly varying control points to go from thin to thick to thin
+  //arms and legs should use beizer curves with slightly varying control points to go from thin to thick to thin
 
-//left arm, start on the outer edge
-const left_x = -w*0.4;
-const left_y = random(-h/2, h/4);
-let control_y = left_y + random(-h/4,h/4);
-const left_body_x = -w/8;
-const left_body_y = -h/4;
+  //left arm, start on the outer edge
+  let control_x, left_x;
+  const left_y = random(-h*3/5, h/8);
+  let stance;
+  if(left_y > 0) stance = "on_hip";
+  else if(left_y <-h/2) stance = "in_air";
+  else stance = "out"
+  if(stance == "on_hip"){ //arm on hip
+    left_x = -w/8
+    control_x = -h/2;
+  } else if(stance == "in_air"){ //arm waving
+    left_x = -w/8
+    control_x = -h/2;
+  } else if(stance == "out"){
+    left_x = -w*0.4;
+    control_x = left_x;
+  }
+  let control_y = left_y + random(-h/4,h/8);
+  const left_body_x = -w/8;
+  const left_body_y = -h/4;
 
-//flair code
-push();
-noFill();
-// get angle from shoulder to tip of hand
-// rotate in accordance to the angle
-// bezier segments beyond the tip of the hand
-// look at keith harring
+  for(let i=0; i<num_lines; i++){
+    bezier(left_x, left_y, control_x, control_y, left_body_x, left_body_y, left_body_x, left_body_y);
+    control_y += limb_mult*h;
+  }
 
-pop();
-//flair code end
+  //right arm, start on the outer edge
+  let right_x;
+  const right_y = random(-h*3/5, h/8);
+  if(left_y > 0) stance = "on_hip";
+  else if(left_y <-h/2) stance = "in_air";
+  else stance = "out"
+  if(stance == "on_hip"){ //arm on hip
+    right_x = w/8
+    control_x = h/2;
+  } else if(stance == "in_air"){ //arm waving
+    right_x = w/8
+    control_x = h/2;
+  } else if(stance == "out"){
+    right_x = w*0.4;
+    control_x = right_x;
+  }
 
-// beginShape();
-// vertex(left_x, left_y);
+  control_y = right_y + random(-h/4,h/8);
+  const right_body_x = w/8;
+  const right_body_y = -h/4;
 
-for(let i=0; i<num_lines; i++){ //TODO rework for pen size
-  bezier(left_x, left_y, left_x, control_y, left_body_x, left_body_y, left_body_x, left_body_y);
-  control_y += limb_mult*h;
-  // bezierVertex(left_body_x, left_body_y, left_x, control_y, left_x, left_y);
-}
-
-// endShape();
-
-//right arm, start on the outer edge
-const right_x = w*0.4;
-const right_y = random(-h/2, h/4);
-control_y = right_y + random(-h/4,h/4);
-const right_body_x = w/8;
-const right_body_y = -h/4;
-
-// beginShape();
-// vertex(right_x, right_y);
-
-for(let i=0; i<num_lines; i++){ //TODO rework for pen size
-  bezier(right_x, right_y, right_x, control_y, right_body_x, right_body_y, right_body_x, right_body_y);
-  control_y += limb_mult*h;
-  // bezierVertex(right_body_x, right_body_y, right_x, control_y, right_x, right_y);
-}
-
-// endShape();
+  for(let i=0; i<num_lines; i++){
+    bezier(right_x, right_y, control_x, control_y, right_body_x, right_body_y, right_body_x, right_body_y);
+    control_y += limb_mult*h;
+  }
 
 }
 
 function legs(w, h, num_lines){
-//arms and legs should use beizer curves with slightly varying control points to go from thin to thick to thin
+  //arms and legs should use beizer curves with slightly varying control points to go from thin to thick to thin
 
-//left leg, start on the outer edge
-let leg_x = random(-w/2,w/8);
-let leg_y = h/2;
-let control_x = leg_x + random(-w/4,w/4);
-let body_x = -w/16;
-let body_y = h/8;
+  //left leg, start on the outer edge
+  let leg_x = random(-w/2,w/8);
+  let leg_y = h/2;
+  let control_x = leg_x + random(-w/4,w/4);
+  let body_x = -w/16;
+  let body_y = h/8;
 
-// beginShape();
-// vertex(leg_x, leg_y);
+  for(let i=0; i<num_lines; i++){
+    bezier(leg_x, leg_y, control_x, leg_y, body_x, body_y, body_x, body_y);
+    control_x += limb_mult*h;
+  }
 
-for(let i=0; i<num_lines; i++){ //TODO rework for pen size
-  bezier(leg_x, leg_y, control_x, leg_y, body_x, body_y, body_x, body_y);
-  control_x += limb_mult*h;
-  // bezierVertex(body_x, body_y, control_x, leg_y, leg_x, leg_y);
-}
+  //right leg, start on the outer edge
+  leg_x = random(-w/8,w/2);
+  leg_y = h/2;
+  control_x = leg_x + random(-w/4,w/4);
+  body_x = w/16;
+  body_y = h/8;
 
-// endShape();
-
-//right leg, start on the outer edge
-leg_x = random(-w/8,w/2);
-leg_y = h/2;
-control_x = leg_x + random(-w/4,w/4);
-body_x = w/16;
-body_y = h/8;
-
-// beginShape();
-// vertex(leg_x, leg_y);
-
-for(let i=0; i<num_lines; i++){ //TODO rework for pen size
-  bezier(leg_x, leg_y, control_x, leg_y, body_x, body_y, body_x, body_y);
-  control_x += limb_mult*h;
-  // bezierVertex(body_x, body_y, control_x, leg_y, leg_x, leg_y);
-}
-
-// endShape();
+  for(let i=0; i<num_lines; i++){
+    bezier(leg_x, leg_y, control_x, leg_y, body_x, body_y, body_x, body_y);
+    control_x += limb_mult*h;
+  }
 
 }
 
@@ -249,5 +221,27 @@ function perimeter_flair(){ //keith harring style shooting lines around person, 
 }
 
 function curved_flair(x, y, w, h){ //keith harring style movement lines
+
+}
+
+// HEAD --------------------------------------
+function head(w,h, interval){
+  let head_w = random(0.7, 0.9)*w/8;
+  let head_h = random(1,1.5)*head_w;
+  let head_size = min(head_w, head_h);
+  translate(head_size*random(-0.5,0.5), head_size*random(-0.25, 0.25));//random offset
+  translate(0, -h*0.4); //normal head position
+  rotate(random(-10,10));
+  while(head_size>0){
+    push();
+    ellipse(0,0, head_w, head_h);
+    head_w -= interval;
+    head_h -= interval;
+    head_size -= interval;
+    pop();
+  }
+}
+
+function barbershop_hat(w,h){
 
 }
