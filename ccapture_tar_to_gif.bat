f8bdfb4ce2735a82c6044d5d077d7d84e3dfc9ae
lewi0622 <eric.lewiston.el@gmail.com>
2026-01-20 09:25:05 -0600

    added creation and deletion of temp folder so it doesn't accidentally nuke every file if something goes wrong

--- ccapture_tar_to_gif.bat
+++ ccapture_tar_to_gif.bat
@@ -1,5 +1,8 @@
-cd "C:\Users\lewi0\Desktop\Generative Art\to_gif"
-set FILENAME=C:\Users\lewi0\Downloads\?*.tar
+cd "C:\Dev"
+mkdir "animation_temp"
+cd "animation_temp"  || GOTO :Failure
+
+set FILENAME=C:\Users\ericl\Downloads\?*.tar
 "C:\Program Files\7-Zip\7z" e %FILENAME% -y 
 
 for %%i in (%FILENAME%) do echo %%~ni > fr.txt
@@ -10,12 +13,19 @@ set /p WIDTH=<width.txt
 magick identify -format "%%h" 0000000.png > height.txt
 set /p HEIGHT=<height.txt
 
-"C:\Users\lewi0\Desktop\Generative Art\ffmpeg-5.0.1-full_build\ffmpeg-5.0.1-full_build\bin\ffmpeg.exe" -y -r %FR% -f image2 -i "%%07d.png" -vcodec libx264 -crf 17 -pix_fmt yuv420p C:\Users\lewi0\Downloads\output.mp4
+"ffmpeg" -y -r %FR% -f image2 -i "%%07d.png" -vcodec libx264 -crf 17 -pix_fmt yuv420p C:\Users\ericl\Downloads\output.mp4
 
-"C:\Users\lewi0\Desktop\Generative Art\gifski-1.6.4\win\gifski.exe" --fps %FR% --width %WIDTH% --height %HEIGHT% -o C:\Users\lewi0\Downloads\output.gif *.png
+"C:\Dev\gifski\win\gifski.exe" --fps %FR% --width %WIDTH% --height %HEIGHT% -o C:\Users\ericl\Downloads\output.gif *.png
 
-del "*.*" /s /f /q
+cd ..\
+
+rd "animation_temp" /s /q
 
 del %FILENAME%
 
+EXIT
+
+:Failure
+echo Unable to go to animation_temp folder, exiting...
+pause >nul
 EXIT
