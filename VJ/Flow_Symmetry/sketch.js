e0c05deeee89c8dc7652a67e18e885e250cc98eb
lewi0622 <eric.lewiston.el@gmail.com>
2026-01-24 11:25:27 -0600

    added flow symmetry and swimmers

--- VJ/Flow_Symmetry/sketch.js
+++ VJ/Flow_Symmetry/sketch.js
@@ -4,12 +4,13 @@ const gif = true;
 const animation = true;
 const fr = 30;
 const capture = false;
-const capture_time = 10;
+const capture_time = 5;
 
 const suggested_palettes = []
 
 //project variables
-let pts;
+const inc = 0.15*60/fr;
+let xoff;
 
 function gui_values(){
   parameterize("sym_angs", floor(random(6,49)), 1, 100, 1, false);
@@ -17,28 +18,20 @@ function gui_values(){
   parameterize("dampening", random(30,80), 1, 500, 1, false);
   parameterize("noise_off", random(-20,20), -100, 100, 1, false);
   parameterize("weight", random(1,3), 1, 10, 0.1, true);
-  parameterize("radius", smaller_base/2, 0, larger_base, 1, true);
 }
 
 function setup() {
   common_setup();
   gui_values();
-
+  dampening = 50;
+  xoff = 0
+  line_segs = floor(random(20,51));
   const line_color = color("WHITE");
   //shadow/glow
   drawingContext.shadowBlur=3*global_scale;
   drawingContext.shadowColor = line_color;
   stroke(line_color);
   noFill();
-
-  pts = [];
-  for(let i=0; i<line_segs; i++){
-    frameCount = fr * capture_time - (line_segs-i);
-    const [x,y] = create_pt();
-    pts.push({x:x, y:y});
-  }
-  frameCount = 0;
-
   document.body.style.background = "BLACK";
   pixelDensity(2);
 }
@@ -46,10 +39,17 @@ function setup() {
 function draw() {
   global_draw_start();
   strokeWeight(weight);
-  background("BLACK")
+ 
   translate(canvas_x/2, canvas_y/2);
 
-  rotate(angle_loop(fr, capture_time, 2));
+  const [x_off, y_off] = noise_loop_2d(fr, capture_time, 1);
+
+  const pts = [];
+  for(let j=0; j<line_segs; j++){
+    const x = floor(map(noise((j )/dampening, x_off, y_off), 0, 1, -canvas_x*.75, canvas_x*.75));
+    const y = floor(map(noise((j + noise_off)/dampening, x_off, y_off), 0,1, -canvas_y*.75, canvas_y*.75));
+    pts.push({x:x, y:y});
+  }
 
   for(let i=0; i<sym_angs; i++){
       beginShape();
@@ -60,16 +60,7 @@ function draw() {
     rotate(360/sym_angs);
   }
 
-  const [x,y] = create_pt();
-  pts.push({x:x, y:y});
-  pts.shift();
+  xoff+= inc;
   
   global_draw_end();
-}
-
-function create_pt(){
-    const [x_off, y_off] = noise_loop_2d(fr, capture_time, 0.8);
-    const x = floor(map(noise(1/dampening, x_off, y_off), 0, 1, -radius, radius));
-    const y = floor(map(noise(noise_off/dampening, x_off, y_off), 0,1, -radius, radius));
-    return [x,y];
 }
---
+++ VJ/Swimmers/sketch.js
@@ -0,0 +1,101 @@
+'use strict';
+//setup variables
+const gif = true;
+const animation = true;
+const fr = 30;
+const capture = false;
+const capture_time = 10;
+let capture_delay_seconds = 3;
+
+let grid_bg_c;
+const suggested_palettes = [GAMEDAY, BIRDSOFPARADISE, NURSERY, SUPPERWARE];
+
+let swimmers, bg_c;
+
+function gui_values(){
+  parameterize("number_of_swimmers", floor(random(50,120)), 1, 1000, 1, false);
+  parameterize("heading_change", random(20,60), 0, 180, 1, false);
+  parameterize("swimmer_starting_rad", random(10,30), 1, 100, 1, true);
+  parameterize("swimmer_tail_length", floor(random(10, 40)), 1, 100, 1, false);
+  parameterize("swimmer_movement_speed", random(1,10), 0.1, 10, 0.1, true);
+}
+
+function setup() {
+  common_setup();
+  gui_values();
+
+  swimmers = [];
+  bg_c = random(working_palette);
+  reduce_array(working_palette, bg_c);
+
+  noStroke();
+  pixelDensity(2);
+  document.body.style.background = "BLACK";
+}
+//***************************************************
+function draw() {
+  global_draw_start(true, 3);
+
+  //actual drawing stuff
+  push();
+  let swimmers_to_remove = [];
+
+  // background(bg_c);
+  background("WHITE")
+
+  erase();
+  //check to see which swimmers are fully offscreen
+  swimmers.forEach((swimmer, swimmer_index) => {
+    let tail_in_screen = false;
+    swimmer.tail.forEach(tail => {
+      if((tail.x>0 && tail.x<canvas_x) || (tail.y>0 && tail.y<canvas_y)) tail_in_screen = true;
+    });
+    if(!tail_in_screen) swimmers_to_remove.push(swimmer_index);
+  });
+
+  //remove swimmers offscreen
+  swimmers_to_remove.forEach(swimmer_index => {
+    swimmers.splice(swimmer_index,1);
+  });
+
+  //if less than n are on screen, spawn as many as necessary
+  if(swimmers.length<number_of_swimmers) spawn_swimmers(swimmer_tail_length);
+
+  //move swimmers
+  for(let j=0; j<swimmers.length; j++){
+    fill(swimmers[j].color);
+
+    //allow movement within +/-45 degrees from heading.
+    const theta = random(swimmers[j].heading-heading_change, swimmers[j].heading+heading_change);
+    swimmers[j].heading = theta;
+    const radius = random(swimmer_movement_speed/2, swimmer_movement_speed);
+
+    const new_x = swimmers[j].tail[0].x + radius*cos(theta);
+    const new_y = swimmers[j].tail[0].y + radius*sin(theta);
+    //rotate tail array
+    const new_tail = arrayRotate(swimmers[j].tail,1);
+    new_tail[0] = {x:new_x, y:new_y};
+  
+    new_tail.forEach((tail_coords, tail_index) => {
+      circle(tail_coords.x, tail_coords.y, lerp(0,swimmer_starting_rad, tail_index/swimmer_tail_length));
+    });
+
+  }
+  noErase();
+  pop();
+
+  global_draw_end();
+}
+//***************************************************
+//custom funcs
+
+function spawn_swimmers(tail_legnth){
+  const x = random(canvas_x/4, canvas_x*3/4);
+  const y = random(canvas_y/4, canvas_y*3/4);
+
+  let swimmer = {};
+  swimmer.heading = random(360);
+  swimmer.color = color("WHITE")//random(working_palette);
+  swimmer.tail = new Array(tail_legnth).fill({x:x, y:y});
+  swimmers.push(swimmer);
+}
