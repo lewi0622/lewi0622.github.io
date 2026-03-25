65261436f901c304242c2e3a32f7f201c3c44f60
Eric Lewiston <eric.lewiston.el@gmail.com>
2026-03-24 20:11:10 -0500

    tweaked qr code to use lines instead of pts. added processed version

--- plotter_projects/Business_Cards/QR_Back/sketch.js
+++ plotter_projects/Business_Cards/QR_Back/sketch.js
@@ -10,13 +10,17 @@ let font, svg_width, svg_height, svg_viewbox, my_svg;
 
 function gui_values(){
   parameterize("guide_lines", 0, 0, 1, 1, false);
-  parameterize("text_size", 45, 1, 500, 1, true);
-  parameterize("x_loc", 53, -base_x, base_x, 1, true);
-  parameterize("y_loc", 44, -base_y, base_y, 1, true);
-  parameterize("x2_loc", 130, -base_x, base_x, 1, true);
-  parameterize("y2_loc", 65, -base_y, base_y, 1, true);
-  parameterize("x3_loc", -118, -base_x, base_x, 1, true);
-  parameterize("y3_loc", 72, -base_y, base_y, 1, true);
+  parameterize("text_size", 31, 1, 500, 1, true);
+  parameterize("qr_x_loc", 16, -base_x, base_x, 1, true);
+  parameterize("qr_y_loc", 28, -base_y, base_y, 1, true);
+  parameterize("x_loc", 112, -base_x, base_x, 1, true);
+  parameterize("y_loc", 55, -base_y, base_y, 1, true);
+  parameterize("x2_loc", 170, -base_x, base_x, 1, true);
+  parameterize("y2_loc", 0, -base_y, base_y, 1, true);
+  parameterize("x3_loc", -150, -base_x, base_x, 1, true);
+  parameterize("y3_loc", 50, -base_y, base_y, 1, true);
+  parameterize("web_x_loc", -104, -base_x, base_x, 1, true);
+  parameterize("web_y_loc", 65, -base_y, base_y, 1, true);
 }
 
 function preload(){
@@ -24,12 +28,12 @@ function preload(){
 }
 
 function setup() {
-  common_setup(3.5*96, 2*96, SVG);
+  common_setup(3.25*96, 2*96, SVG);
   gui_values();
 
   if(!redraw){
     // opentype.load('..\\..\\..\\fonts\\SquarePeg-Regular.ttf', function (err, f) {
-    opentype.load('..\\..\\..\\fonts\\Porcine-Heavy.ttf', function (err, f) {
+    opentype.load('..\\..\\..\\fonts\\Roboto-Black.ttf', function (err, f) {
       if (err) {
         alert('Font could not be loaded: ' + err);
       } else {
@@ -45,7 +49,7 @@ function draw() {
   global_draw_start();
   push();
   // background("TEAL")
-  strokeWeight(GELLYROLLMOONLIGHT10);
+  strokeWeight(LEPEN * global_scale);
 
   if(guide_lines){
     rect(0,0,canvas_x, canvas_y);
@@ -54,18 +58,21 @@ function draw() {
     line(0, canvas_y/4, canvas_x, canvas_y/4);
     line(0, canvas_y*3/4, canvas_x, canvas_y*3/4);
   }
-  image(my_svg, canvas_x/2-80, canvas_y/2 - 70/2, 70, 70);
+  image(my_svg, qr_x_loc, qr_y_loc, 80, 80);
 
   noFill();
   const line1 = font.getPath("Pen Plotter", 0,0, text_size);
-  const line2 = font.getPath("and", 0,0, text_size);
+  const line2 = font.getPath("&", 0,0, text_size);
   const line3 = font.getPath("Digital Art", 0,0, text_size);
+  const website = font.getPath("LewistonFace.com", 0,0, text_size)
   translate(x_loc, y_loc);
   draw_open_type_js_path_p5_commands(line1);
   translate(x2_loc, y2_loc);
   draw_open_type_js_path_p5_commands(line2);
   translate(x3_loc, y3_loc);
   draw_open_type_js_path_p5_commands(line3);
+  translate(web_x_loc, web_y_loc);
+  draw_open_type_js_path_p5_commands(website);
   pop();
   global_draw_end();
 }
