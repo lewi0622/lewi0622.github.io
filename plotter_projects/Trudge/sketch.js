8639354b97b5aecf4c50a6c5b1d06bcf011ae415
lewi0622 <eric.lewiston.el@gmail.com>
2026-05-22 07:33:17 -0500

    reworked trudge, esp tree sizing and footprint spacing

--- plotter_projects/Trudge/sketch.js
+++ plotter_projects/Trudge/sketch.js
@@ -8,15 +8,14 @@ const capture_time = 10;
 
 const suggested_palettes = [];
 
-//ideas:
-//house in clearing? find noise val furthest from tree range
-//road, river, ski trail, boot trail (ovals)
+// TODO don't draw items that go outside the canvas boundary
+  // to reduce chopped off trees/shadows
 
-let house_w, house_h, col_step, row_step, boot_map, last_boot_tile;
+let col_step, row_step, last_boot_pt, last_boot_tile, boot_spacing, boot_count, weight;
 
 function gui_values(){
-  parameterize("cols", floor(base_x/2), 1, base_x, 1, false);
-  parameterize("rows", floor(base_y/2), 1, base_y, 1, false);
+  parameterize("cols", floor(base_x * random(1/8, 1/2)), 1, base_x, 1, false);
+  parameterize("rows", floor(base_y * random(1/8, 1/2)), 1, base_y, 1, false);
   parameterize("shadow_angle", random(-45,0) + random([0,-135]), -360, 0, 1, false);
   parameterize("shadow_len_pct", random(2,5), 0, 10, 0.1, false);
 }
@@ -24,33 +23,27 @@ function gui_values(){
 function setup() {
   common_setup();
   gui_values();
-  boot_map = [];
-  for(let i=0; i<cols; i++){
-    const column = [];
-    for(let j=0; j<rows; j++){
-      column.push(0);
-    }
-    boot_map.push(column);
-  }
-  last_boot_tile = {col: -1, row: -1, angle:0};
+  // pixelDensity(10);
+
+  boot_count = 0;
+  last_boot_pt = {x: 0, y:0};
 }
 //***************************************************
 function draw() {
   global_draw_start();
-
   //actual drawing stuff
   push();
   png_bg(false, color("WHITE"));
-  strokeWeight(LEPEN * global_scale);
+  weight = LEPEN * global_scale;
+  strokeWeight(weight);
 
   col_step = width/cols;
   row_step = height/rows;
+
+  boot_spacing = 0.9 * col_step;
   
   const tree_w = col_step;
-  const tree_h = row_step * 3;
-
-  house_w = tree_w * 8;
-  house_h = tree_h * 2.5;
+  const tree_h = col_step * 3;
 
   const pct_move = 1;
   const min_w = 0.5;
@@ -108,148 +101,107 @@ function shadow(w,h, pct, num_shadows = floor(random(2,10)), angle_variation=5){
   }
 }
 
-
 function lumpy_tree(w, h){
   //start in lower left
 
-  const control_variation = w/2 * random(-1,1);
+  let control_variation = w/4;
+  let variation_dir = random(-1,1);
   
-  stroke("#132e20");
-  fill("#132e20");
-  beginShape();
-  vertex(0,0);
-  bezierVertex(
-    0,0, 
-    control_variation,-h, 
-    w/2,-h
-  );
-  bezierVertex(
-    w/2,-h, 
-    w + control_variation, -h, 
-    w, 0
-  );
-  bezierVertex(
-    w,0,
-    w/2, h/8,
-    0,0
-  );
-  endShape(CLOSE);
+  control_variation *= variation_dir;
   
-  // boughs(w,h);
+  const tree_c = color("#132e20");
+  tree_c.setAlpha(200);
+  stroke(tree_c);
+  fill("WHITE");
+
+  translate(w/2, -h/2);  
+  while(w>0 && h>0){
+    tree_outline(w, h, control_variation);
+    control_variation = control_variation + variation_dir * -1 * weight;
+    if(variation_dir < 0) control_variation = constrain(control_variation, -w, 0);
+    else control_variation = constrain(control_variation, 0, w);
+    w -= weight;
+    h -= weight;
+  }
 }
 
-function boughs(w,h){
-  push();
-  translate(w/2,0);
-  const num_steps = 40;
-  const step_size = -h/num_steps;
-  const noise_offset = random(100);
-  for(let i=0; i<num_steps; i++){
-    for(let j=0; j<2; j++){
-      push();
-      rotate(random(-5,5));
-      stroke(random(["#132e20", "#1db638"]));
-      translate(0, i * step_size);
-      const bough_w = lerp(w, w/4, i/num_steps) * map(noise(noise_offset + i/10, j), 0,1, 0.5,1.5);
-      if(j==0) line(0,0, bough_w,0);
-      else line(-bough_w, 0, 0, 0);
-      pop();
-    }
-  }
-  pop();
+function tree_outline(w,h,variation){
+  beginShape();
+  vertex(-w/2, h/2);
+  bezierVertex(
+    -w/2, h/2,
+    variation,-h/2, 
+    0,-h/2
+  );
+  bezierVertex(
+    0,-h/2, 
+    w/2 + variation, -h/2, 
+    w/2, h/2
+  );
+  bezierVertex(
+    w/2, h/2,
+    0, h/2 + h/8,
+    -w/2, h/2
+  );
+  endShape(CLOSE);
 }
 
 function mouseDragged(){
-  const col = floor(mouseX / col_step);
-  const row = floor(mouseY / row_step);
-  boots(col, row);
+  boots(mouseX, mouseY);
 }
 
-function boots(c, r){
+function boots(x,y){
   //user-made boot trails
-  
+  const boot_c = color("BLACK");
+  boot_c.setAlpha(150);
+  stroke(boot_c);
+  fill(boot_c);
   // on page
-  if(c < 0 && c >= cols) return;
-  if(r < 0 && r >= rows) return;
-
-  //don't place in the same tile twice in a row
-  if(last_boot_tile.col == c && last_boot_tile.row == r) return;
+  if(x < 0 || x > canvas_x) return;
+  if(y < 0 || y > canvas_y) return;
 
   //initial_click
-  if(last_boot_tile.col == -1 && last_boot_tile.row == -1){
-    last_boot_tile.col = c;
-    last_boot_tile.row = r;
+  if(last_boot_pt.x == 0 && last_boot_pt.y == 0){
+    last_boot_pt.x = x;
+    last_boot_pt.y = y;
     return;
   }
+  
+  if(abs(dist(last_boot_pt.x, last_boot_pt.y, x, y)) < boot_spacing) return;
 
   //get angle from last tile to this one
-  const prev = createVector(last_boot_tile.col * col_step, last_boot_tile.row * row_step);
-  const next = createVector(c * col_step, r * row_step);
-  angleMode(DEGREES);
+  const prev = createVector(last_boot_pt.x, last_boot_pt.y);
+  const next = createVector(x, y);
+
   const v  = p5.Vector.sub(next, prev);
-  const boot_angle = lerp(last_boot_tile.angle, v.heading(), 0.25);
+  const boot_angle = v.heading();
 
-  //prolly need to lerp angle
-
+  const boot_variation = 0.5;
+  
+  let w = col_step/3;
+  let h = row_step/3;  
+  
   push();
-  translate(c * col_step, r * row_step);
+
+  x += w * boot_variation * random(-1,1);
+  y += h * boot_variation * random(-1,1);
+
+  translate(x, y);
+  
+  //center rotate about the gait
+  translate(0, row_step/2);
   rotate(boot_angle);
-  ellipse(col_step/4, row_step/4, col_step/4, row_step/4);
-  ellipse(col_step*3/4, row_step*3/4, col_step/4, row_step/4);
+  translate(0, -row_step/2);
+  
+  if(boot_count %2 == 0) translate(0, row_step);
+  while(w > 0 && h > 0){
+    ellipse(0, 0, w, h);
+    w -= weight;
+    h -= weight;
+  }
   pop();
 
-  last_boot_tile.col = c;
-  last_boot_tile.row = r;
-  last_boot_tile.angle = boot_angle;
+  last_boot_pt.x = x;
+  last_boot_pt.y = y;
+  boot_count++;
 }
-
-// function mouseClicked(){
-//   house(mouseX, mouseY);
-// }
-
-// function house(x,y){
-//   //starting bottom left
-//   const w = house_w;
-//   const h = house_h;
-//   stroke("BLACK");
-//   fill("WHITE");
-//   push();
-//   translate(x,y);
-
-//   //front wall
-//   beginShape();
-//   vertex(0, -h * 1/5); //lower left
-//   vertex(0, -h* 3/5); //upper left
-//   vertex(w * 2/3, -h * 2/5); //upper right
-//   vertex(w * 2/3, 0); //lower right
-//   endShape(CLOSE);
-
-//   //side wall
-//   beginShape();
-//   vertex(w * 2/3, 0); //lower left
-//   vertex(w * 2/3, -h * 2/5); //upper left
-//   vertex(w * 4/5, -h * 4/5); //roof tip
-//   vertex(w, -h * 3/5); //upper right
-//   vertex(w, -h * 1/5); //lower right
-//   endShape(CLOSE);
-
-//   //roof
-//   beginShape();
-//   vertex(0, -h* 3/5); //lower left
-//   vertex(w * 1/3, -h * 9/10); //upper left
-//   vertex(w * 4/5, -h * 4/5); //upper right
-//   vertex(w * 2/3, -h * 2/5); //lower right
-//   endShape(CLOSE);
-
-//   const roof_hatch = 10;
-//   for(let i=0; i<roof_hatch-1; i++){
-//     const pct = (i+1)/roof_hatch;
-//     const start_x = lerp(0, w * 2/3, pct);
-//     const ending_x = lerp(w*1/3, w *4/5, pct);
-//     const start_y = lerp(-h*3/5, -h*2/5, pct);
-//     const ending_y = lerp(-h *9/10, -h* 4/5, pct);
-//     line(start_x, start_y, ending_x, ending_y)
-//   }
-
-//   pop();
-// }
