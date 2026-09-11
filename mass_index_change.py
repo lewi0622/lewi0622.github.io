3bb9d219eaa8188bc564b122ef041b6e714361cb
lewi0622 <eric.lewiston.el@gmail.com>
2026-09-10 21:16:24 -0500

    changed ccapture library reference

--- mass_index_change.py
+++ mass_index_change.py
@@ -3,15 +3,15 @@ import pathlib
 
 path = os.getcwd()
 
-#absolute paths
+# absolute paths
 common = os.path.join(path, "projects", "Common")
 
 to_replace = []
 
-#find all index.html files that exist in folder
+# find all index.html files that exist in folder
 for root, dirs, files in os.walk(path):
     for f in files:
-        #verify they reside along with a sketch.js
+        # verify they reside along with a sketch.js
         if f == "index.html" and os.path.exists(os.path.join(root, "sketch.js")):
             to_replace.append(os.path.join(root, f))
 
@@ -19,39 +19,75 @@ for root, dirs, files in os.walk(path):
 for f in to_replace:
     rel_path = os.path.relpath(common, os.path.dirname(f))
     rel_path = pathlib.Path(rel_path)
-    
-    file_contents = '''<!DOCTYPE html><html lang="en"><head>
-    <script src="'''+os.path.join(rel_path.as_posix(), "p5.min.js") +'''"></script>
+
+    file_contents = (
+        '''<!DOCTYPE html><html lang="en"><head>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "p5.min.js")
+        + '''"></script>
     <!-- Perlin and Simplex Noise functions -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "perlin.js") +'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "perlin.js")
+        + '''"></script>
     <!-- Custom scripts -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "palettes.js") +'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "palettes.js")
+        + '''"></script>
     <script src="sketch.js"></script>
-    <script src="'''+os.path.join(rel_path.as_posix(), "utils.js") +'''"></script>
-    <script src="'''+os.path.join(rel_path.as_posix(), "cutouts.js") +'''"></script>    
-    <script src="'''+os.path.join(rel_path.as_posix(), "shapes.js") +'''"></script>
-    <script src="'''+os.path.join(rel_path.as_posix(), "midi.js")+'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "utils.js")
+        + '''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "cutouts.js")
+        + '''"></script>    
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "shapes.js")
+        + '''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "midi.js")
+        + '''"></script>
     <!-- Frame capture lib for gifs/vids -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "CCapture.all.min.js") +'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "ccapture.umd.min.js")
+        + '''"></script>
     <!-- p5.gui lib -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "p5.gui.js") +'''"></script>
-    <script src="'''+os.path.join(rel_path.as_posix(), "quicksettings.js") +'''"></script>   
-    <script src="'''+os.path.join(rel_path.as_posix(), "p5.svg.js") +'''"></script>   
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "p5.gui.js")
+        + '''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "quicksettings.js")
+        + '''"></script>   
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "p5.svg.js")
+        + '''"></script>   
     <!-- Pen Size Files for Plotting -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "pen_utils.js") +'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "pen_utils.js")
+        + '''"></script>
     <!-- Color Picker lib -->
-    <link rel="stylesheet" href="'''+os.path.join(rel_path.as_posix(), "Alwan_Color_Picker","css" ,"alwan.min.css") +'''">
-    <script src="'''+os.path.join(rel_path.as_posix(), "Alwan_Color_Picker", "js", "alwan.min.js")+'''"></script>
+    <link rel="stylesheet" href="'''
+        + os.path.join(
+            rel_path.as_posix(), "Alwan_Color_Picker", "css", "alwan.min.css"
+        )
+        + '''">
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "Alwan_Color_Picker", "js", "alwan.min.js")
+        + '''"></script>
     <!-- Open Type js library for svg handling of text -->
-    <script src="'''+os.path.join(rel_path.as_posix(), "opentype.min.js") +'''"></script>
+    <script src="'''
+        + os.path.join(rel_path.as_posix(), "opentype.min.js")
+        + '''"></script>
     <!-- Global Stylesheet -->
-    <link rel="stylesheet" type="text/css" href="'''+os.path.join(rel_path.as_posix(), "style.css") +'''">
+    <link rel="stylesheet" type="text/css" href="'''
+        + os.path.join(rel_path.as_posix(), "style.css")
+        + """">
     <meta charset="utf-8">
 
     </head>
     <body>
     </body>
-    </html>'''
+    </html>"""
+    )
 
     print("Overwriting" + f)
     with open(f, "w") as myfile:
